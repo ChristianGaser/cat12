42da4af6329648fa849f21465140ba5f02361b2c
Christian Gaser <christian.gaser@uni-jena.de>
2008-12-11 15:38:55 +0000

    Update KmeansMex.m to provide automatical compilation.
    git-svn-id: svn://141.35.69.214/cat12@19 9f44b166-36c4-483c-8295-4af200601734

--- KmeansMex.m
+++ KmeansMex.m
@@ -1,7 +1,13 @@
 function label = KmeansMex(src, mask, separations, iters_nu)
 
 disp('Compiling KmeansMex.c')
+
+pth = fileparts(which(mfilename));
+p_path = pwd;
+cd(pth);
 mex KmeansMex.c Kmeans.c splineSmooth.cc -lebtks
+cd(p_path);
+
 % -L/usr/local/lib -I/usr/local/include
 label = KmeansMex(src, mask, separations, iters_nu);
 
