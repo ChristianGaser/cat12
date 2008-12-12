b53ff6b77ce295eae88dbf7813f3a3e278d2fe54
Christian Gaser <christian.gaser@uni-jena.de>
2008-12-12 10:58:37 +0000

    Added Bayes* Kmeans* Pve5*.
    git-svn-id: svn://141.35.69.214/cat12@21 9f44b166-36c4-483c-8295-4af200601734

---
+++ Pve5Mex.c
@@ -0,0 +1,43 @@
+/*
+ * Christian Gaser
+ * $Id: PveMex.c 8 2008-12-10 20:21:27Z gaser $ 
+ *
+ */
+
+#include "mex.h"
+#include "matrix.h"
+#include "math.h"
+#include "stdio.h"
+
+void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
+{
+  unsigned char *label;
+  unsigned char *prob;
+  double *src, *mean;
+  const int *dims;
+  int nc;
+  int BG;
+    
+  if (nrhs!=5)
+    mexErrMsgTxt("5 inputs required.");
+  else if (nlhs>0)
+    mexErrMsgTxt("Too many output arguments.");
+  
+  if (!mxIsUint8(prhs[1]))
+	mexErrMsgTxt("Second argument must be uint8.");
+
+  if (!mxIsUint8(prhs[2]))
+	mexErrMsgTxt("Second argument must be uint8.");
+
+  src   = (double*)mxGetPr(prhs[0]);
+  prob  = (unsigned char*)mxGetPr(prhs[1]);
+  label = (unsigned char*)mxGetPr(prhs[2]);
+  mean  = (double*)mxGetPr(prhs[3]);
+  BG    = (int)mxGetScalar(prhs[4]);
+
+  dims = mxGetDimensions(prhs[0]);
+
+  Pve5(src, prob, label, mean, BG, dims);
+
+}
+
---
+++ Pve5Mex.m
@@ -0,0 +1,16 @@
+function Pve5Mex(src, prob, label, mean, BG);
+%
+% Christian Gaser
+% $Id: Pve5Mex.m 8 2008-12-10 20:21:27Z gaser $
+
+disp('Compiling Pve5Mex.c')
+
+pth = fileparts(which(mfilename));
+p_path = pwd;
+cd(pth);
+mex -O Pve5Mex.c Pve5.c 
+cd(p_path);
+
+Pve5Mex(src, prob, label, mean, BG);
+
+return
