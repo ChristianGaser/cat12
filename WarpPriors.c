38e892e1dc80c025dafaa9ef048eca32e03b0cdb
Christian Gaser <christian.gaser@uni-jena.de>
2009-01-04 22:38:29 +0000

    (1) Added test_vbm8.m for testing. (2) First successful warping of priors implemented.
    git-svn-id: svn://141.35.69.214/cat12@34 9f44b166-36c4-483c-8295-4af200601734

--- WarpPriors.c
+++ WarpPriors.c
@@ -9,44 +9,69 @@
 #include <math.h>
 #include "optimizer3d.h"
 #include "diffeo3d.h"
+#include "Amap.h"
 
 struct dartel_prm {
-  int rform;         // regularization form: 0 - linear elastic energy; 1 - membrane energy; 2 - bending energy
-  double rparam[6];  // regularization parameters
-  double lmreg;      // LM regularization
-  int cycles;        // number of cycles for full multi grid (FMG)
-  int its;           // Number of relaxation iterations in each multigrid cycle
-  int k;             // time steps for solving the PDE
-  int code;          // objective function: 0 - sum of squares; 1 - symmetric sum of squares; 2 - multinomial
+  int rform;         /* regularization form: 0 - linear elastic energy; 1 - membrane energy; 2 - bending energy */
+  double rparam[6];  /* regularization parameters */
+  double lmreg;      /* LM regularization */
+  int cycles;        /* number of cycles for full multi grid (FMG) */
+  int its;           /* Number of relaxation iterations in each multigrid cycle */
+  int k;             /* time steps for solving the PDE */
+  int code;          /* objective function: 0 - sum of squares; 1 - symmetric sum of squares; 2 - multinomial */
 };
 
 
 void WarpPriors(unsigned char *prob, unsigned char *priors, unsigned char *mask, float *flow, int *dims, int loop)
 {
-  int vol, vol2, vol3, i, j;
-  
-  vol  = dims[0]*dims[1]*dims[2];
-  vol2 = 2*vol;
-  vol3 = 3*vol;
-
-  float *f     = (float *)malloc(sizeof(float)*vol3);
-  float *g     = (float *)malloc(sizeof(float)*vol3);
-  float *v     = (float *)malloc(sizeof(float)*vol3);
-  float *flow1 = (float *)malloc(sizeof(float)*vol3);
-
-  for (i = 0; i < vol3; i++) v[i] = 0.0;
-
-  int code = 2;    // multinomial
-  int rform = 0;   // linear energy
+  int vol, vol0, vol2, vol3, i, j, k, l, m, is, size[4], samp;
+  double buf[3], ll[3], sum_buf, samp_1; 
+  float *f, *g, *v, *flow1, *scratch, max;
+  int it, it0, it1, it_scratch, ndims4, dims_samp[3]; 
+  int x, y, z, index_samp, xsamp, ysamp, ysamp2, zsamp, zsamp2;
+  int yoffset, area_samp, zoffset, area, index;
+  int z_area, y_dims, ix, iy, iz;
+  unsigned char *mask_samp;
+   
+  int code = 2;    /* multinomial */
+  int rform = 0;   /* linear energy */
   double lmreg = 0.01;
   static double param[3] = {1.0, 1.0, 1.0};
 
+  samp = 3;
+  
+  /* only use gm/wm */
+  ndims4 = 2;
+
+  /* define grid dimensions */
+  for(j=0; j<3; j++) dims_samp[j] = (int) ceil((dims[j]-1)/((double) samp))+1;
+  
+  /* find grid point conversion factor */
+  samp_1 = 1.0/((double) samp);
+
+  area = dims[0]*dims[1];
+  vol0 = dims[0]*dims[1]*dims[2];
+  area_samp = dims_samp[0]*dims_samp[1];
+  vol  = dims_samp[0]*dims_samp[1]*dims_samp[2];
+  vol2 = 2*vol;
+  vol3 = 3*vol;
+  
+  f     = (float *)malloc(sizeof(float)*vol3);
+  g     = (float *)malloc(sizeof(float)*vol3);
+  v     = (float *)malloc(sizeof(float)*vol3);
+  flow1 = (float *)malloc(sizeof(float)*vol3);
+  mask_samp = (unsigned char*)malloc(sizeof(unsigned char)*vol);
+
+  /* initialize flow field */
+  for (i = 0; i < vol3; i++) v[i] = flow[i];
+
   struct dartel_prm* prm = (struct dartel_prm*)malloc(sizeof(struct dartel_prm)*10);
-  // first three entrys of param are equal
+  
+  /* first three entries of param are equal */
   for (j = 0; j < loop; j++)
     for (i = 0; i < 3; i++)  prm[j].rparam[i] = param[i];
 
-  // some entry are equal
+  /* some entries are equal */
   for (j = 0; j < loop; j++) {
     prm[j].rform = rform;
     prm[j].cycles = 3;
@@ -62,53 +87,137 @@ void WarpPriors(unsigned char *prob, unsigned char *priors, unsigned char *mask,
   prm[4].rparam[3] = 0.25;  prm[4].rparam[4] = 0.125;  prm[4].rparam[5] = 1e-6; prm[4].k = 4; 
   prm[5].rparam[3] = 0.125; prm[5].rparam[4] = 0.0625; prm[5].rparam[5] = 1e-6; prm[5].k = 6; 
 
-  // change order to gm/wm/csf
-  for (i = 0; i < vol; i++) {
-    f[i     ] = ((float)priors[i+vol ])/255.0;
-    f[i+vol ] = ((float)priors[i+vol2])/255.0;
-    f[i+vol2] = ((float)priors[i     ])/255.0;
-    g[i     ] = ((float)  prob[i+vol ])/255.0;
-    g[i+vol ] = ((float)  prob[i+vol2])/255.0;
-    g[i+vol2] = ((float)  prob[i     ])/255.0;
+  for (i = 0; i < vol3; i++) f[i] = 0.0;
+
+  /* loop over neighborhoods of the grid points */
+  for (k=-samp; k<=samp; k++) {
+    for (l=-samp; l<=samp; l++) {
+      for (m=-samp; m<=samp; m++) {
+        for (z = 0; z < dims_samp[2]; z++) {
+          zsamp = z*samp + k;
+          if ((zsamp>=0) & (zsamp<dims[2])) {
+            zsamp2 = zsamp*area;
+            zoffset = z*area_samp;
+            for (y=0; y<dims_samp[1]; y++) {
+              ysamp = y*samp + l;
+              if ((ysamp>=0) & (ysamp<dims[1])) {
+                ysamp2 = ysamp*dims[0];
+                yoffset = zoffset + y*dims_samp[0];
+                for (x=0; x<dims_samp[0]; x++) {
+                  xsamp = x*samp + m;
+                  if ((xsamp>=0) & (xsamp<dims[0])) {
+                    index_samp = yoffset + x;
+                    index = zsamp2 + ysamp2 + xsamp;
+                    f[index_samp     ] += ((float)priors[index         ]);
+                    f[index_samp+vol ] += ((float)priors[index + vol0  ]);
+                    f[index_samp+vol2] += ((float)priors[index + vol0*2]);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
-  int size[4];
-  for(i=0; i < 3; i++) size[i] = dims[i];
+
+
+  max = -1e15;
+  for (i=0; i<vol3; i++) max = MAX(f[i], max);
+  for (i=0; i<vol3; i++) f[i] /= max;
+
+  for(i=0; i < 3; i++) size[i] = dims_samp[i];
     
-  // only use gm/wm
-  size[3] = 2;
-  int it = 0, it0, it1; 
-  double ll[3]; 
+  size[3] = ndims4;
+  it = 0;
   for (it0 = 0; it0 < loop; it0++) {
-    int it_scratch = iteration_scratchsize((int *)size, prm[it0].code, prm[it0].k);
-    float *scratch = (float *)malloc(sizeof(float)*it_scratch);
+    it_scratch = iteration_scratchsize((int *)size, prm[it0].code, prm[it0].k);
+    scratch = (float *)malloc(sizeof(float)*it_scratch);
+
+    for (i = 0; i < vol3; i++) g[i] = 0.0;
+
+    /* load probabilities and use updated mask */
+    /* loop over neighborhoods of the grid points */
+    for (k=-samp; k<=samp; k++) {
+      for (l=-samp; l<=samp; l++) {
+        for (m=-samp; m<=samp; m++) {
+          for (z = 0; z < dims_samp[2]; z++) {
+            zsamp = z*samp + k;
+            if ((zsamp>=0) & (zsamp<dims[2])) {
+              zsamp2 = zsamp*area;
+              zoffset = z*area_samp;
+              for (y=0; y<dims_samp[1]; y++) {
+                ysamp = y*samp + l;
+                if ((ysamp>=0) & (ysamp<dims[1])) {
+                  ysamp2 = ysamp*dims[0];
+                  yoffset = zoffset + y*dims_samp[0];
+                  for (x=0; x<dims_samp[0]; x++) {
+                    xsamp = x*samp + m;
+                    if ((xsamp>=0) & (xsamp<dims[0])) {
+                      index_samp = yoffset + x;
+                      index = zsamp2 + ysamp2 + xsamp;
+                      g[index_samp     ] += ((float)prob[index + vol0  ])*((float)mask[index]);
+                      g[index_samp+vol ] += ((float)prob[index + vol0*2])*((float)mask[index]);
+                      g[index_samp+vol2] += ((float)prob[index         ])*((float)mask[index]);
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    max = -1e15;
+    for (i=0; i<vol3; i++) max = MAX(g[i], max);
+    for (i=0; i<vol3; i++) g[i] /= max;
+
     for (it1 = 0; it1 < prm[it0].its; it1++) {
       it++;
       iteration(size, prm[it0].k, v, f, g, (float *)0, prm[it0].rform, prm[it0].rparam, prm[it0].lmreg, 
         prm[it0].cycles, prm[it0].its, prm[it0].code, flow, ll, scratch);              
-      printf("%02d:\t%6g\t%6g\t%6g\t%6g\n", it, ll[0], ll[1], ll[0]+ll[1], ll[2]);
+      printf("%02d:\t%.2f\t%6.2f\t%6.2f\t%6.2f\n", it, ll[0], ll[1], ll[0]+ll[1], ll[2]);
       fflush(stdout);
       for (i = 0; i < vol3; i++) v[i] = flow[i];
     }
     free(scratch);
-  }
 
-  expdef(size, 6, -1, v, flow, flow1, (float *)0, (float *)0); 
-  for (i = 0; i < vol; i++) {
-    double buf[3], sum_buf = 0.0; 
-    int j;
-    sampn(size, f, 3, vol,
-            (double)flow[i]-1.0, (double)flow[vol+i]-1.0, (double)flow[vol2+i]-1.0, buf);
-    for(j=0; j<3; j++) sum_buf += buf[j];
-    // set result to zero if sum of probabilities is too low
-    if (sum_buf < 0.25) {
-      prob[i] = 0;
-      prob[i+vol] = 0;
-      prob[i+vol2] = 0;
+    /* warp mask */
+    expdef(size, 6, -1, v, flow, flow1, (float *)0, (float *)0); 
+    for (i = 0; i < vol; i++) {
+      sampn(size, f, 3, vol, (double)flow[i]-1.0, (double)flow[vol+i]-1.0, (double)flow[vol2+i]-1.0, buf);
+      sum_buf = 0.0;
+      for(j=0; j<3; j++) sum_buf += buf[j];
+      mask_samp[i] = (unsigned char) MIN(ROUND(255.0*sum_buf), 255);
+    }
+
+    /* loop over image points */
+    for (z = 0; z < dims[2]; z++) {
+      z_area=z*area;
+      for (y = 0; y < dims[1]; y++) {
+        y_dims=y*dims[0];
+        for (x = 0; x < dims[0]; x++)  {
+	  
+          i = x + y_dims + z_area;
+          
+          /* find the interpolation factors */
+          ix = (int)(samp_1*x);
+          iy = (int)(samp_1*y);
+          iz = (int)(samp_1*z);
+          index_samp = iz*area_samp + iy*dims_samp[0] + ix;
+          
+          mask[i] = mask_samp[index_samp];
+        }
+      }
     }
   }
 
+  for (i = 0; i < vol3; i++) flow[i] = v[i];
+
   free(flow1);
+  free(mask_samp);
   free(v);
   free(f);
   free(g);
---
+++ test_vbm8.m
@@ -0,0 +1,33 @@
+function test_vbm8
+
+if 0
+V = spm_vol('data/t1_icbm_normal_1mm_pn3_rf20.img');
+vol = spm_read_vols(V);
+
+Vpriors = spm_vol(str2mat('/Users/gaser/spm/spm8b/toolbox/Seg/TPM.nii,1',...
+    '/Users/gaser/spm/spm8b/toolbox/Seg/TPM.nii,2',...
+    '/Users/gaser/spm/spm8b/toolbox/Seg/TPM.nii,3'));
+priors = zeros([V(1).dim(1:3) 3],'uint8');
+
+for j=1:V(1).dim(3),
+
+    Mi  = spm_matrix([0 0 j]);
+
+    % Load slice j from all images
+    M1  = V(1).mat\Vpriors(1).mat\Mi;
+    for i=1:3
+        priors(:,:,j,i) = uint8(round(255*spm_slice_vol(Vpriors(i),M1,V(1).dim(1:2),[1 0])));
+    end
+end
+
+mask = zeros(V(1).dim(1:3),'uint8');
+vx = sqrt(sum(V(1).mat(1:3,1:3).^2));
+
+save all
+else
+
+!rm *.mexmaci
+load all
+tic;prob = PveAmapMex(vol, priors, mask, vx);toc
+
+end
