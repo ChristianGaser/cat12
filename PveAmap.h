9d3fa8acc97bc661b680c94742f7f1b654b45947
Christian Gaser <christian.gaser@uni-jena.de>
2009-01-07 13:40:48 +0000

    Updated again resampling in WarpPriors.c
    git-svn-id: svn://141.35.69.214/cat12@44 9f44b166-36c4-483c-8295-4af200601734

--- PveAmap.h
+++ PveAmap.h
@@ -13,6 +13,6 @@ int write_fuzzy = FALSE;
 int write_nu = FALSE;
 int write_label = TRUE;
 int warp_priors = TRUE;
-int samp = 3;
+int subsample_warp = 3;
 double thresh_brainmask = 0.05;
 double thresh_kmeans = 0.5;
