cbbc3d8998fd18c234a28ebe42346493cdeea204
Christian Gaser <christian.gaser@uni-jena.de>
2009-01-28 18:49:28 +0000

    Changed some default values in cg_vbm8_default.m.
    git-svn-id: svn://141.35.69.214/cat12@89 9f44b166-36c4-483c-8295-4af200601734

--- cg_vbm8_defaults.m
+++ cg_vbm8_defaults.m
@@ -34,21 +34,23 @@ defaults.vbm8.output.bias.warped  = 1;
 defaults.vbm8.output.label.native = 0;
 defaults.vbm8.output.label.warped = 0;
 
-defaults.vbm8.output.grey.native = 0;
-defaults.vbm8.output.grey.warped = 1;
-defaults.vbm8.output.grey.mod    = 1;
-defaults.vbm8.output.grey.dartel = 0;
+% order is [native normalised modulated dartel]
+defaults.vbm8.output.grey.native = 0;	% GM
+defaults.vbm8.output.grey.warped = 1;	% GM
+defaults.vbm8.output.grey.mod    = 2;	% GM
+defaults.vbm8.output.grey.dartel = 0;	% GM
 
-defaults.vbm8.output.white.native = 0;
-defaults.vbm8.output.white.warped = 1;
-defaults.vbm8.output.white.mod    = 1;
-defaults.vbm8.output.white.dartel = 0;
+defaults.vbm8.output.white.native = 0;	% WM
+defaults.vbm8.output.white.warped = 1;	% WM
+defaults.vbm8.output.white.mod    = 2;	% WM
+defaults.vbm8.output.white.dartel = 0;	% WM
 
-defaults.vbm8.output.csf.native = 0;
-defaults.vbm8.output.csf.warped = 0;
-defaults.vbm8.output.csf.mod    = 0;
-defaults.vbm8.output.csf.dartel = 0;
+defaults.vbm8.output.csf.native = 0;	% CSF
+defaults.vbm8.output.csf.warped = 1;	% CSF
+defaults.vbm8.output.csf.mod    = 2;	% CSF
+defaults.vbm8.output.csf.dartel = 0;	% CSF
 
+% order is [inverse forward]
 defaults.vbm8.output.warps = [0 0];
 
 % Extended writing options
@@ -57,5 +59,5 @@ defaults.vbm8.extopts.cleanup      = 0;	% Cleanup: 1 - light; 2 -thorough
 defaults.vbm8.extopts.vox          = 1;	% Voxel size to write
 defaults.vbm8.extopts.bb           = [[-78 78]' [-112 76]' [-70 85]'];	% bounding box
 defaults.vbm8.extopts.print        = 1;	% Display and print results
-defaults.vbm8.extopts.brainmask_th = 0.5;	% Brainmask threshold
-defaults.vbm8.extopts.brainmask    = cellstr(fullfile(fileparts(which(mfilename)),'TPM.nii'));;	% Brainmask
+defaults.vbm8.extopts.brainmask_th = 0.25;	% Brainmask threshold
+defaults.vbm8.extopts.brainmask    = cellstr(fullfile(fileparts(which(mfilename)),'brainmask_LPBA40.nii'));;	% Brainmask
