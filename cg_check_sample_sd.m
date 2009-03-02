219a2e2a65ed0ba36b3c402c0ba4751358f114cf
Christian Gaser <christian.gaser@uni-jena.de>
2009-03-02 15:27:51 +0000

    (1) Added output of covariance matrix to cg_check_sample_sd.m (2) Added option to remove nuisance parameter from datain cg_check_sample_sd.m
    git-svn-id: svn://141.35.69.214/cat12@111 9f44b166-36c4-483c-8295-4af200601734

--- cg_check_sample_sd.m
+++ cg_check_sample_sd.m
@@ -1,5 +1,5 @@
 function cg_check_sample_sd(vargin)
-%cg_check_sample_sd	to check standard deviation across sample
+%cg_check_sample_sd to check standard deviation across sample
 %
 % Images have to be in the same orientation with same voxel size
 % and dimension (e.g. normalized images)
@@ -11,107 +11,151 @@ function cg_check_sample_sd(vargin)
 rev = '$Rev$';
 
 if nargin == 1
-	P = [];
-	for i=1:numel(vargin.data)
-		P = strvcat(P,deblank(vargin.data{i}));
-	end
-	norm = vargin.scale;
-	sd_name = vargin.sd_name;
-	mean_name = vargin.mean_name;
+  P = [];
+  for i=1:numel(vargin.data)
+    P = strvcat(P,deblank(vargin.data{i}));
+  end
+  norm = vargin.scale;
+  sd_name = vargin.sd_name;
+  mean_name = vargin.mean_name;
+  if isempty(vargin.nuisance)
+    nuisance = [];
+  else
+    nuisance = vargin.nuisance.c;
+  end
 end
 
 if nargin < 1
-	P = spm_select(Inf,'image','Select images');
+  P = spm_select(Inf,'image','Select images');
 end
 
 V = spm_vol(deblank(P));
 n = size(P,1);
 
 if length(V)>1 & any(any(diff(cat(1,V.dim),1,1),1))
-	error('images don''t all have same dimensions'), end
+  error('images don''t all have same dimensions'), end
 if any(any(any(diff(cat(3,V.mat),1,3),3)))
-	error('images don''t all have same orientation & voxel size'), end
+  error('images don''t all have same orientation & voxel size'), end
 
 if nargin < 1
-	norm = spm_input('Prop. scaling (e.g. for T1- or modulated images)?',1,'yes|no',[1 0],2);
-	sd_name = spm_input('Output SD filename',1,'s','SD.img');
-	mean_name = spm_input('Output Mean filename',1,'s','Mean.img');
+  norm = spm_input('Prop. scaling (e.g. for T1- or modulated images)?',1,'yes|no',[1 0],2);
+  sd_name = spm_input('Output SD filename','+1','s','SD.nii');
+  mean_name = spm_input('Output Mean filename','+1','s','Mean.nii');
+  def_nuis = spm_input('Variable to covariate out (nuisance parameter)?','+1','yes|no',[1 0],2);
+  if def_nuis
+    nuisance = spm_input('Nuisance parameter:','+1','r',[],n);
+  end
+end
+
+if ~isempty(nuisance)
+  if size(nuisance,2) ~= n
+    nuisance = nuisance';
+  end
 end
 
 if ~isempty(sd_name)
-	Vo1 = V(1);
-    Vo1.dt = [spm_type('int16') spm_platform('bigend')];
-	Vo1.fname = sd_name;
+  Vo1 = V(1);
+  Vo1.dt = [spm_type('int16') spm_platform('bigend')];
+  Vo1.fname = sd_name;
 
-	Vo1.descrip = 'std';
+  Vo1.descrip = 'std';
 end
 
 if ~isempty(mean_name)
-	Vo2 = V(1);
-	Vo2.dt = [spm_type('int16') spm_platform('bigend')];
-	Vo2.fname = mean_name;
+  Vo2 = V(1);
+  Vo2.dt = [spm_type('int16') spm_platform('bigend')];
+  Vo2.fname = mean_name;
 
-	Vo.descrip = 'mean';
+  Vo.descrip = 'mean';
 end
 
 
 % global scaling
 if norm
-	gm=zeros(size(V,1),1);
-	disp('Calculating globals...');
-	for i=1:size(V,1), gm(i) = spm_global(V(i)); end
-	gm_all = mean(gm);
-	for i=1:n
-		V(i).pinfo(1:2,:) = gm_all*V(i).pinfo(1:2,:)/gm(i);
-	end
+  gm=zeros(size(V,1),1);
+  disp('Calculating globals...');
+  for i=1:size(V,1), gm(i) = spm_global(V(i)); end
+  gm_all = mean(gm);
+  for i=1:n
+    V(i).pinfo(1:2,:) = gm_all*V(i).pinfo(1:2,:)/gm(i);
+  end
 end
 
 
 %-Start progress plot
 %-----------------------------------------------------------------------
-mean_data = zeros(V(1).dim(1:3));
-std_data  = zeros(V(1).dim(1:3));
+mean_vol = zeros(V(1).dim(1:3));
+std_vol  = zeros(V(1).dim(1:3));
 squared_distance = zeros(n,1);
+vol = zeros(prod(V(1).dim(1:2)), n);
+YpY = zeros(n);
 
-spm_progress_bar('Init',2*n,'volumes completed');
-for i=1:n
-    vol = spm_read_vols(V(i));
-    mean_data = mean_data + vol/n;
-	spm_progress_bar('Set',i);    
+spm_progress_bar('Init',V(1).dim(3),'Check homogeneity','planes completed')
+for j=1:V(1).dim(3),
+
+  dist2_slice = zeros(V(1).dim(1:2));
+  M  = spm_matrix([0 0 j]);
+
+  for i = 1:n
+    img = spm_slice_vol(V(i),M,V(1).dim(1:2),[1 0]);
+    vol(:,i) = img(:);
+  end
+  mean_slice = mean(reshape(vol,[V(i).dim(1:2) n]),3);
+  mask = find(mean_slice ~= 0);
+  % remove nuisance and calculate again mean
+  if ~isempty(nuisance) 
+    vol(mask,:) = vol(mask,:) - vol(mask,:)*pinv(nuisance)*nuisance;
+    mean_slice = mean(reshape(vol,[V(i).dim(1:2) n]),3);
+  end
+  mean_vol(:,:,j) = mean_slice;
+  if ~isempty(mask)
+    for i = 1:n
+      dist2  = (vol(mask,i) - mean_slice(mask)).^2; 
+      dist2_slice(mask) = dist2;
+      std_vol(:,:,j) = std_vol(:,:,j) + dist2_slice;
+      squared_distance(i) = squared_distance(i) + sum(dist2(~isnan(dist2)));
+    end
+    % make sure data is zero mean
+    tmp_vol = vol(mask,:);
+    tmp_vol = tmp_vol - repmat(mean(tmp_vol,1), [length(mask) 1]);
+	  YpY = YpY + (tmp_vol'*tmp_vol)/n;
+  end 
+  spm_progress_bar('Set',j);  
 end
 
-mask = (mean_data ~= 0);
-sz_mask = sum(mask(:));
-
-for i=1:n
-    vol = spm_read_vols(V(i));
-    dist2 = (mean_data(mask) - vol(mask)).^2;
-    squared_distance(i) = sum(dist2(~isnan(dist2)));
-    std_data(mask) = std_data(mask) + dist2;
-	spm_progress_bar('Set',i+n);    
-end
-
-std_data = (std_data/(n-1)).^0.5;
+std_vol = (std_vol/(n-1)).^0.5;
 
 spm_progress_bar('Clear');
+
+% normalize YpY
+d = sqrt(diag(YpY)); % sqrt first to avoid under/overflow
+dd = d*d';
+YpY = YpY ./ (dd+eps);
+t = find(abs(YpY) > 1); YpY(t) = YpY(t)./abs(YpY(t));
+YpY(1:n+1:end) = sign(diag(YpY));
+
+YpYsum = sum(YpY,1);
+[i, j] = sort(YpYsum, 2, 'descend');
+YpYsorted = YpY(j,j);
+Nsorted = P(j,:);
 
 % sort files
 fprintf('Squared distance to mean:\n');
 [squared_distance_sorted, ind] = sort(squared_distance);
 for i=1:n
-    fprintf('%s: %3.2f\n',V(ind(i)).fname,squared_distance_sorted(i));
+  fprintf('%s: %3.2f\n',V(ind(i)).fname,squared_distance_sorted(i));
 end
 
 Fgraph = spm_figure('GetWin','Graphics');
 spm_figure('Clear',Fgraph);
-FS      = spm('FontSizes');
+FS    = spm('FontSizes');
 
 [tmp fname] = spm_str_manip(char(V.fname),'C');
-fprintf('Compressed filenames: %s    \n',tmp);
+fprintf('Compressed filenames: %s  \n',tmp);
 
 xpos = 2*(0:n-1)/(n-1);
 for i=1:n
-    text(xpos(i),squared_distance(i),fname.m{i},'FontSize',FS(8),'HorizontalAlignment','center')
+  text(xpos(i),squared_distance(i),fname.m{i},'FontSize',FS(8),'HorizontalAlignment','center')
 end
 
 hold on
@@ -120,31 +164,66 @@ set(gca,'XTick',[],'XLim',[-.25 2.25]);
 if max(squared_distance) > min(squared_distance)
   set(gca,'YLim',[0.9*min(squared_distance) 1.1*max(squared_distance)]);
 end
-title(sprintf('Boxplot: squared distance to mean    \nCommon filename: %s*%s',fname.s,fname.e),'FontSize',FS(12),'FontWeight','Bold');
-ylabel('<----- low (good quality) --- squared distance from mean --- large (poor quality)------>    ','FontSize',FS(10),'FontWeight','Bold');
-xlabel('<----- first --- file order --- last ------>    ','FontSize',FS(10),'FontWeight','Bold');
+title(sprintf('Boxplot: squared distance to mean  \nCommon filename: %s*%s',fname.s,fname.e),'FontSize',FS(12),'FontWeight','Bold');
+ylabel('<----- low (good quality) --- squared distance from mean --- large (poor quality)------>  ','FontSize',FS(10),'FontWeight','Bold');
+xlabel('<----- first --- file order --- last ------>  ','FontSize',FS(10),'FontWeight','Bold');
 hold off
+
+f = figure(4);
+% allow maximal 50 ticklabels
+step = round(n/50);
+ws = spm('Winsize','Graphics');
+set(f,'MenuBar','none','Position',[10 10 2*ws(3) 0.9*ws(3)]);
+subplot(1,2,1)
+imagesc(YpY)
+a = gca;
+set(a,'YTick',2:step:n,'YTickLabel',fname.m(2:step:n),'TickDir','out','XTick',1:step:n,'XTickLabel','')
+% Xtick rotated by 90 degree
+hxLabel = get(gca,'XLabel');
+set(hxLabel,'Units','data');
+xLabelPosition = get(hxLabel,'Position');
+y = repmat(xLabelPosition(2),length(1:step:n),1);
+text(1:step:n, y, fname.m(1:step:n),'fontsize',get(gca,'fontsize'),'Rotation',90,'HorizontalAlignment','right','Interpreter','none');
+axis image
+colormap(hot)
+colorbar
+title('Original order')
+
+subplot(1,2,2)
+imagesc(YpYsorted)
+a = gca;
+set(a,'YTick',2:step:n,'YTickLabel',fname.m(j(2:step:n)),'TickDir','out','XTick',1:step:n,'XTickLabel','')
+% Xtick rotated by 90 degree
+hxLabel = get(gca,'XLabel');
+set(hxLabel,'Units','data');
+xLabelPosition = get(hxLabel,'Position');
+y = repmat(xLabelPosition(2),length(1:step:n),1);
+text(1:step:n, y, fname.m(j(1:step:n)),'fontsize',get(gca,'fontsize'),'Rotation',90,'HorizontalAlignment','right','Interpreter','none');
+colormap(hot)
+colorbar
+axis image
+title('Sorted according for covariance')
 
 % check for replicates
 for i=1:n
   for j=1:n
-	if (i>j) & (squared_distance(i) == squared_distance(j))
-		[s,differ] = unix(['diff ' V(i).fname ' ' V(j).fname]);
-		if (s==0), fprintf(['\nWarning: ' V(i).fname ' and ' V(j).fname ' are same files?\n']); end
-	end
+  if (i>j) & (squared_distance(i) == squared_distance(j))
+    [s,differ] = unix(['diff ' V(i).fname ' ' V(j).fname]);
+    if (s==0), fprintf(['\nWarning: ' V(i).fname ' and ' V(j).fname ' are same files?\n']); end
+  end
   end
 end
 
 %-Write output image (uses spm_write_vol - which calls spm_write_plane)
 %-----------------------------------------------------------------------
 if ~isempty(sd_name)
-	Vo1 = spm_write_vol(Vo1,std_data);
-	disp(['Writing ' Vo1.fname]);
+  Vo1 = spm_write_vol(Vo1,std_vol);
+  disp(['Writing ' Vo1.fname]);
 end
 
 if ~isempty(mean_name)
-	Vo2 = spm_write_vol(Vo2,mean_data);
-	disp(['Writing ' Vo2.fname]);
+  Vo2 = spm_write_vol(Vo2,mean_vol);
+  disp(['Writing ' Vo2.fname]);
 end
 
 %-End
@@ -153,12 +232,12 @@ spm_progress_bar('Clear')
 
 show = spm_input('Show files with CheckReg (beginning from bad)?','+1','yes|no',[1 0],2);
 if show
-	number = min([n 15]);
-	number = spm_input('How many files ?','+1','e',number);
-	
-	list = str2mat(V(ind(n:-1:1)).fname);
-	list2 = list(1:number,:);
-	spm_check_registration(list2)
+  number = min([n 15]);
+  number = spm_input('How many files ?','+1','e',number);
+  
+  list = str2mat(V(ind(n:-1:1)).fname);
+  list2 = list(1:number,:);
+  spm_check_registration(list2)
 end
 return
 
@@ -196,13 +275,13 @@ function s = cg_boxplot (data,notched,symbol,vertical,maxwhisker)
 %
 % The returned matrix s has one column for each dataset as follows:
 %
-%    1  minimum
-%    2  1st quartile
-%    3  2nd quartile (median)
-%    4  3rd quartile
-%    5  maximum
-%    6  lower confidence limit for median
-%    7  upper confidence limit for median
+%  1  minimum
+%  2  1st quartile
+%  3  2nd quartile (median)
+%  4  3rd quartile
+%  5  maximum
+%  6  lower confidence limit for median
+%  7  upper confidence limit for median
 %
 % Example
 %
@@ -258,9 +337,9 @@ end
 
 % compute statistics
 % s will contain
-%    1,5    min and max
-%    2,3,4  1st, 2nd and 3rd quartile
-%    6,7    lower and upper confidence intervals for median
+%  1,5  min and max
+%  2,3,4  1st, 2nd and 3rd quartile
+%  6,7  lower and upper confidence intervals for median
 s = zeros(7,nc);
 box = zeros(1,nc);
 whisker_x = ones(2,1)*[1:nc,1:nc];
@@ -273,9 +352,9 @@ outliers2_y = [];
 for i=1:nc
   % Get the next data set from the array or cell array
   if iscell(data)
-    col = data{i}(:);
+  col = data{i}(:);
   else
-    col = data(:,i);
+  col = data(:,i);
   end
   % Skip missing data
   col(isnan(col)) = [];
@@ -283,38 +362,38 @@ for i=1:nc
   nd = length(col);
   box(i) = nd;
   if (nd > 1)
-    % min,max and quartiles
-    s(1:5,i) = [min(col) prctile(col,[25 50 75]) max(col)]';
-    % confidence interval for the median
-    est = 1.57*(s(4,i)-s(2,i))/sqrt(nd);
-    s(6,i) = max([s(3,i)-est, s(2,i)]);
-    s(7,i) = min([s(3,i)+est, s(4,i)]);
-    % whiskers out to the last point within the desired inter-quartile range
-    IQR = maxwhisker*(s(4,i)-s(2,i));
-    whisker_y(:,i) = [min(col(col >= s(2,i)-IQR)); s(2,i)];
-    whisker_y(:,nc+i) = [max(col(col <= s(4,i)+IQR)); s(4,i)];
-    % outliers beyond 1 and 2 inter-quartile ranges
-    outliers = col((col < s(2,i)-IQR & col >= s(2,i)-2*IQR) | (col > s(4,i)+IQR & col <= s(4,i)+2*IQR));
-    outliers2 = col(col < s(2,i)-2*IQR | col > s(4,i)+2*IQR);
-    outliers_x = [outliers_x; i*ones(size(outliers))];
-    outliers_y = [outliers_y; outliers];
-    outliers2_x = [outliers2_x; i*ones(size(outliers2))];
-    outliers2_y = [outliers2_y; outliers2];
+  % min,max and quartiles
+  s(1:5,i) = [min(col) prctile(col,[25 50 75]) max(col)]';
+  % confidence interval for the median
+  est = 1.57*(s(4,i)-s(2,i))/sqrt(nd);
+  s(6,i) = max([s(3,i)-est, s(2,i)]);
+  s(7,i) = min([s(3,i)+est, s(4,i)]);
+  % whiskers out to the last point within the desired inter-quartile range
+  IQR = maxwhisker*(s(4,i)-s(2,i));
+  whisker_y(:,i) = [min(col(col >= s(2,i)-IQR)); s(2,i)];
+  whisker_y(:,nc+i) = [max(col(col <= s(4,i)+IQR)); s(4,i)];
+  % outliers beyond 1 and 2 inter-quartile ranges
+  outliers = col((col < s(2,i)-IQR & col >= s(2,i)-2*IQR) | (col > s(4,i)+IQR & col <= s(4,i)+2*IQR));
+  outliers2 = col(col < s(2,i)-2*IQR | col > s(4,i)+2*IQR);
+  outliers_x = [outliers_x; i*ones(size(outliers))];
+  outliers_y = [outliers_y; outliers];
+  outliers2_x = [outliers2_x; i*ones(size(outliers2))];
+  outliers2_y = [outliers2_y; outliers2];
   elseif (nd == 1)
-    % all statistics collapse to the value of the point
-    s(:,i) = col;
-    % single point data sets are plotted as outliers.
-    outliers_x = [outliers_x; i];
-    outliers_y = [outliers_y; col];
+  % all statistics collapse to the value of the point
+  s(:,i) = col;
+  % single point data sets are plotted as outliers.
+  outliers_x = [outliers_x; i];
+  outliers_y = [outliers_y; col];
   else
-    % no statistics if no points
-    s(:,i) = NaN;
+  % no statistics if no points
+  s(:,i) = NaN;
   end
 end
 
 % Note which boxes don't have enough stats
 chop = find(box <= 1);
-    
+  
 % Draw a box around the quartiles, with width proportional to the number of
 % items in the box. Draw notches if desired.
 box = box*0.3/max(box);
@@ -341,21 +420,21 @@ cap_y = whisker_y([1,1],:);
 
 % Do the plot
 if vertical
-	plot(quartile_x, quartile_y, 'b-')
-	hold on
-	plot(whisker_x, whisker_y, 'b-')
-	plot(cap_x, cap_y, 'b-')
-	plot(median_x, median_y, 'r-')
-	plot(outliers_x, outliers_y, [symbol(1),'r'])
-        plot(outliers2_x, outliers2_y, [symbol(2),'r']);
+  plot(quartile_x, quartile_y, 'b-')
+  hold on
+  plot(whisker_x, whisker_y, 'b-')
+  plot(cap_x, cap_y, 'b-')
+  plot(median_x, median_y, 'r-')
+  plot(outliers_x, outliers_y, [symbol(1),'r'])
+    plot(outliers2_x, outliers2_y, [symbol(2),'r']);
 else
-	plot(quartile_y, quartile_x, 'b-')
-	hold on
-	plot(whisker_y, whisker_x, 'b-')
-	plot(cap_y, cap_x, 'b-')
-	plot(median_y, median_x, 'r-')
-	plot(outliers_y, outliers_x, [symbol(1),'r'])
-        plot(outliers2_y, outliers2_x, [symbol(2),'r']);
+  plot(quartile_y, quartile_x, 'b-')
+  hold on
+  plot(whisker_y, whisker_x, 'b-')
+  plot(cap_y, cap_x, 'b-')
+  plot(median_y, median_x, 'r-')
+  plot(outliers_y, outliers_x, [symbol(1),'r'])
+    plot(outliers2_y, outliers2_x, [symbol(2),'r']);
 end
 
 hold off
@@ -377,33 +456,33 @@ function y = prctile(x,p);
 
 [prows pcols] = size(p);
 if prows ~= 1 & pcols ~= 1
-    error('P must be a scalar or a vector.');
+  error('P must be a scalar or a vector.');
 end
 if any(p > 100) | any(p < 0)
-    error('P must take values between 0 and 100');
+  error('P must take values between 0 and 100');
 end
 
 if (~any(isnan(x)))
    y = prctilecol(x,p);
-else                    % if there are NaNs, process each column
+else          % if there are NaNs, process each column
    if (size(x,1) == 1)
-      x = x';
+    x = x';
    end
    c = size(x,2);
    np = length(p);
    y = zeros(np,c);
    for j=1:c
-      xx = x(:,j);
-      xx = xx(~isnan(xx));
-      y(:,j) = prctilecol(xx,p)';
+    xx = x(:,j);
+    xx = xx(~isnan(xx));
+    y(:,j) = prctilecol(xx,p)';
    end
    if (min(size(x)) == 1)
-      y = y';
+    y = y';
    end
 end
 
 return
-      
+    
 %-----------------------------------------------------------------------
 function y = prctilecol(x,p);
 %-----------------------------------------------------------------------
@@ -411,17 +490,17 @@ xx = sort(x);
 [m,n] = size(x);
 
 if m==1 | n==1
-    m = max(m,n);
-	if m == 1,
-	   y = x*ones(length(p),1);
-	   return;
-	end
-    n = 1;
-    q = 100*(0.5:m - 0.5)./m;
-    xx = [min(x); xx(:); max(x)];
+  m = max(m,n);
+  if m == 1,
+     y = x*ones(length(p),1);
+     return;
+  end
+  n = 1;
+  q = 100*(0.5:m - 0.5)./m;
+  xx = [min(x); xx(:); max(x)];
 else
-    q = 100*(0.5:m - 0.5)./m;
-    xx = [min(x); xx; max(x)];
+  q = 100*(0.5:m - 0.5)./m;
+  xx = [min(x); xx; max(x)];
 end
 
 q = [0 q 100];
