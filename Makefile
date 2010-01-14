b669ca9f86396625c616e3a534cc3eaf2e3d5147
Christian Gaser <christian.gaser@uni-jena.de>
2010-01-14 12:38:51 +0000

    Added Optimized Blockwise Non Local Means Denoising Filter.
    git-svn-id: svn://141.35.69.214/cat12@239 9f44b166-36c4-483c-8295-4af200601734

--- Makefile
+++ Makefile
@@ -7,14 +7,18 @@ include Makefile.var
 OBS  = Amap.o KmeansProper.o MrfPrior.o Pve.o
 OBS1 = Amap.o Kmeans.o MrfPrior.o Pve.o SplineSmooth.o
 OBS2 = PveAmap.o Amap.o MrfPrior.o Pve.o Kmeans.o WarpPriors.o Bayes.o optimizer3d.o diffeo3d.o SplineSmooth.o
+OBS3 = ornlmMex.o ornlm.o
 
-all: AmapMex.$(SUF) AmapMexNu.$(SUF)
+all: AmapMex.$(SUF) ornlmMex.$(SUF) AmapMexNu.$(SUF)
 
 AmapMex.$(SUF): AmapMex.c $(OBS)
 	$(MEX) AmapMex.c $(OBS) $(MEXEND)
 
 AmapMexNu.$(SUF): AmapMexNu.c $(OBS1)
 	$(MEX) AmapMexNu.c $(OBS1) $(MEXEND) ./$(EXT)/libEBTKS.a $(MEXEND)
+
+ornlmMex.$(SUF): ornlmMex.c $(OBS3)
+	$(MEX) ornlmMex.c $(OBS3) $(MEXEND)
 
 PveAmapMex.$(SUF): PveAmapMex.c $(OBS2)
 	$(MEX) PveAmapMex.c $(OBS2) -I. ./$(EXT)/libEBTKS.a $(MEXEND)
---
+++ ornlmMex.m
@@ -0,0 +1,37 @@
+function out = ornlmMex(in, v, f, h)
+% FORMAT out = ornlmMex(in, v, f, h)
+% 
+% Optimized Blockwise Non Local Means Denoising Filter
+%
+% v - size of search volume (M in paper)
+% f - size of neighborhood (d in paper)
+% h - smoothing parameter
+%
+%                          Details on ONLM filter                        
+% ***************************************************************************
+%  The ONLM filter is described in:                                       
+%                                                                         
+%  P. Coupe, P. Yger, S. Prima, P. Hellier, C. Kervrann, C. Barillot.     
+%  An Optimized Blockwise Non Local Means Denoising Filter for 3D Magnetic
+%  Resonance Images. IEEE Transactions on Medical Imaging, 27(4):425-441, 
+%  April 2008                                                             
+% ***************************************************************************
+%
+% Christian Gaser
+% $Id: ornlmMex.m 224 2009-12-02 23:39:15Z gaser $
+
+rev = '$Rev: 224 $';
+
+disp('Compiling ornlmMex.c')
+
+pth = fileparts(which(mfilename));
+p_path = pwd;
+cd(pth);
+mex -O ornlmMex.c ornlm.c 
+cd(p_path);
+
+try 
+  out = ornlmMex(in, v, f, h);
+end
+
+return
