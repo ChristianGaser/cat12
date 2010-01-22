b736bf50454aa5587520294cec105aad9ab1815b
Christian Gaser <christian.gaser@uni-jena.de>
2010-01-22 14:30:21 +0000

    Updated w32/libEBTKS and EBTKS/trivials.h.
    git-svn-id: svn://141.35.69.214/cat12@256 9f44b166-36c4-483c-8295-4af200601734

--- EBTKS/trivials.h
+++ EBTKS/trivials.h
@@ -66,13 +66,13 @@ inline int doubleCompareAsc(const void *x, const void *y) {
   double diff = *(double *) x - *(double *) y; return SIGN(diff); }
 
 inline double gauss(double mean, double std) {
-  double v1 = 2.0*drand48() - 1.0;
-  double v2 = 2.0*drand48() - 1.0;
+  double v1 = 2.0*rand() - 1.0;
+  double v2 = 2.0*rand() - 1.0;
   double s = v1*v1 + v2*v2;
 
   while (s >= 1.0) {
-    v1 = 2.0*drand48() - 1.0;
-    v2 = 2.0*drand48() - 1.0;
+    v1 = 2.0*rand() - 1.0;
+    v2 = 2.0*rand() - 1.0;
     s  = v1*v1 + v2*v2;
   }
 
