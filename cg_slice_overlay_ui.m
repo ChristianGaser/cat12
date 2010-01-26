a8c33f9d2b6e74fa738dede5140ca3f44e30a6ae
Christian Gaser <christian.gaser@uni-jena.de>
2010-01-26 12:07:59 +0000

    Updated cg_slice_overlay.m.
    git-svn-id: svn://141.35.69.214/cat12@263 9f44b166-36c4-483c-8295-4af200601734

---
+++ cg_slice_overlay_ui.m
@@ -0,0 +1,43 @@
+function cg_slice_overay_ui
+
+OV.reference_image = fullfile(spm('dir'),'canonical','single_subj_T1.nii');
+OV.reference_range = [0.05 0.6];                         % intensity range for reference image
+OV.opacity = 1;                                      % transparence value for overlay (<1)
+OV.cmap    = jet;                                      % colormap for overlay
+
+% name of files
+OV.name=str2mat(fullfile(spm('dir'),'tpm','grey.nii'),...
+                fullfile(spm('dir'),'tpm','white.nii'));
+                
+% range for each file
+% Use range 0..0 if you want to autoscale range.
+% If you are using log. scaling, check the highest p-value in the table
+% and approximate the range; e.g. for a max. p-value of p=1e-7 and a
+% threshold of p<0.001 use a range of [3 7]. Check cg_spmT2x.m for details.
+% If you are unsure, simply use the autorange option by using a range of [0 0].
+% The log-scaled values are calculated by -log10(1-p):
+% p-value       -log10(1-P)
+%  0.1           1
+%  0.05          1.3
+%  0.01          2
+%  0.001         3
+%  0.0001        4
+
+% Number of fields in range should be the same as number of files (see above)
+% or give one value, which is valid for all.
+% If lower and upper range are equal, then the range will be automatically estimated.
+% Be carefule: intensities below the lower range are not shown!
+OV.range   =[[0.5 1]; [0.5 1]];
+
+% selection of slices and orientations
+OV.slices_str = char('[-30 -5 5 40 50 60]','-30:2:30','-20:5:45');
+OV.transform = char('axial','sagittal','coronal');
+OV.printstr = 'print -r300 -painters -noui';
+
+% define output format of slices
+OV.labels.format = '%3.1f';
+
+% Comment this out if you don't wish slice labels
+%OV.labels = 'none';
+
+cg_slice_overlay(OV)
