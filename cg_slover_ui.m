a9f798f4e31d612019ccce90ee1301e6d6a65eec
Christian Gaser <christian.gaser@uni-jena.de>
2010-01-26 07:26:43 +0000

    Updated cg_slover.m and cg_slover_ui.m.
    git-svn-id: svn://141.35.69.214/cat12@260 9f44b166-36c4-483c-8295-4af200601734

--- cg_slover_ui.m
+++ cg_slover_ui.m
@@ -10,7 +10,7 @@ options.reference_image = fullfile(spm('dir'),'canonical','single_subj_T1.nii');
 OV = slover(options.reference_image);
 
 options.reference_range = [0.05 0.6];  % intensity range for reference image
-options.opacity = 1;                   % transparence value for overlay (<1)
+options.opacity = 1;                   % transparence value for overlay (<1 for transparent overlay)
 options.cmap    = jet;                 % colormap for overlay
 
 % name of files
@@ -18,12 +18,11 @@ options.name=str2mat(fullfile(spm('dir'),'tpm','grey.nii'),...
                      fullfile(spm('dir'),'tpm','white.nii'));
                 
 % range for each file
-% use range 0..0 if you want to autoscale range
+% Use range 0..0 if you want to autoscale range.
 % If you are using log. scaling, check the highest p-value in the table
 % and approximate the range; e.g. for a max. p-value of p=1e-7 and a
-% threshold of p<0.001 use a range of [3 7]. Check cg_t2x for details.
-% if you are unsure, simply use the autorange option by with a range of [0 0]
-% to approximate the range.
+% threshold of p<0.001 use a range of [3 7]. Check cg_spmT2x.m for details.
+% If you are unsure, simply use the autorange option by using a range of [0 0].
 % The log-scaled values are calculated by -log10(1-p):
 % p-value       -log10(1-P)
 %  0.1           1
@@ -32,13 +31,22 @@ options.name=str2mat(fullfile(spm('dir'),'tpm','grey.nii'),...
 %  0.001         3
 %  0.0001        4
 
-% number of fields in range should be the same as number of files (see above)
-% if lower and upper range are equal, then the range will be automatically estimated
+% Number of fields in range should be the same as number of files (see above)
+% or give one value, which is valid for all.
+% If lower and upper range are equal, then the range will be automatically estimated.
+% Be carefule: intensities below the lower range are not shown!
 options.range   =[[0.5 1]; [0.5 1]];
 
 % selection of slices and orientations
 options.slices_str = char('-52:2:70','-90:2:90','-90:2:60','[-10 20 40 60]');
 options.transform = char('axial','sagittal','coronal','axial');
+options.printstr = 'print -r300 -painters -noui';
+
+% define output format of slices
 OV.labels.format = '%3.1f';
 
+% Comment this out if you don't wish slice labels
+%OV.labels = 'none';
+
+% call cg_slover
 OV = cg_slover(OV,options);
