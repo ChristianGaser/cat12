2175a1b54fde9700040520c36d2e4bafff155141
Christian Gaser <christian.gaser@uni-jena.de>
2010-02-22 14:18:22 +0000

    Changed paths: M      AmapMex.m M      AmapMex.mexmaci64 !      AmapMex.mexmaci !      AmapMex.mexa64 !      AmapMex.mexw32 M      KmeansMex.m M      cg_vbm8_write.m !      AmapMex.mexglx M      AmapMex.c
    New interims version.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@289 9f44b166-36c4-483c-8295-4af200601734

--- AmapMex.m
+++ AmapMex.m
@@ -11,7 +11,7 @@ disp('Compiling AmapMex.c')
 pth = fileparts(which(mfilename));
 p_path = pwd;
 cd(pth);
-mex -O AmapMex.c KmeansProper.c Amap.c MrfPrior.c Pve.c 
+mex -O AmapMex.c Kmeans.c Amap.c MrfPrior.c Pve.c vollib.c
 cd(p_path);
 
 [prob, mean] = AmapMex(src, label, n_classes, n_iters, sub, pve, init, mrf_weight, voxelsize);
