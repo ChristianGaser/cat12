265c82bf0f813714e66d0f713df0a92799205068
Christian Gaser <christian.gaser@uni-jena.de>
2010-03-08 13:25:15 +0000

    Changed paths: M      cg_noise_estimation.m
    Because of memory issues noise estimation is done using single format.
    
    
    git-svn-id: svn://141.35.69.214/cat12@303 9f44b166-36c4-483c-8295-4af200601734

--- cg_noise_estimation.m
+++ cg_noise_estimation.m
@@ -17,7 +17,7 @@ function h = cg_noise_estimation(ima)
 
 % estimate local mean
 k = 3;
-localMean = (convn(double(ima),ones(k,k,k),'same')/k^3);
+localMean = (convn(single(ima),ones(k,k,k),'same')/k^3);
 
 % find non-zero regions
 ind = find(localMean>0);
@@ -28,7 +28,7 @@ ind = find(localMean>0);
 if length(ind)>0.95*prod(size(ima))
   h = sqrt(2/pi)*moda(localMean(ind),1000);
 else % otherwise use mode of local variance (equation 15 in Aja-Fern‡ndez et al. 2009)
-  localVar = (convn(double(ima).^2,ones(k,k,k),'same')/k^3) - localMean.^2;
+  localVar = (convn(single(ima).^2,ones(k,k,k),'same')/k^3) - localMean.^2;
   h = sqrt(moda(localVar(ind),1000));
 end
 
@@ -57,11 +57,12 @@ if N==0
 	N = 1000;
 end
 
-u = double(u(:));
+u = single(u(:));
 
 M1 = mean(u);
 V1 = std(u);
 C2 = u(u<=(M1+2*V1));
+whos
 [h,x] = hist(C2,N);
 [M,M2] = max(h);
 
