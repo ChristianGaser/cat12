0a03c0d7ce75bf82923879cc35907bed05364c49
Christian Gaser <christian.gaser@uni-jena.de>
2010-03-08 23:25:06 +0000

    Changed paths: M       cg_vbm8_update.m M       cg_vbm8_tools.m
    (1) Changed some help text in cg_vbm8_tools.m.
    (2) Added automatic check for updates at start.
    
    
    git-svn-id: svn://141.35.69.214/cat12@305 9f44b166-36c4-483c-8295-4af200601734

--- cg_vbm8_update.m
+++ cg_vbm8_update.m
@@ -1,4 +1,4 @@
-function cg_vbm8_update
+function cg_vbm8_update(update)
 % check for new updates
 %
 % FORMAT cg_vbm8_update
@@ -10,6 +10,10 @@ function cg_vbm8_update
 % $Id: cg_vbm8_update.m 183 2009-10-07 21:10:26Z gaser $
 
 rev = '$Rev: 183 $';
+
+if nargin == 0
+  update = 0;
+end
 
 r = 0;
 
@@ -46,34 +50,41 @@ end
 if rnew > r
   fprintf('A new version of VBM8 is available on: %s\n',url);
   fprintf('Your version: %d - New version: %d\n',r,rnew);
+  if ~update
+    fprintf('In order to update use Toolbox|VBM8|Check for updates\n',r,rnew);
+  end
 
-  d = fullfile(spm('Dir'),'toolbox'); 
-  overwrite = spm_input('Update',1,'m','Download zip-file only|Overwrite old VBM8 installation',[0 1],2);
-  if overwrite
-    try
-      % list mex-files and delete these files to prevent that old
-      % compiled files are used
-      mexfiles = dir(fullfile(d,'vbm8','*.mex*'));
-      for i=1:length(mexfiles)
-        name = fullfile(d,'vbm8',mexfiles(i).name);
-        spm_unlink(name);
-        disp(name)
+  if update
+    d = fullfile(spm('Dir'),'toolbox'); 
+    overwrite = spm_input('Update',1,'m','Download zip-file only|Overwrite old VBM8 installation',[0 1],2);
+    if overwrite
+      try
+        % list mex-files and delete these files to prevent that old
+        % compiled files are used
+        mexfiles = dir(fullfile(d,'vbm8','*.mex*'));
+        for i=1:length(mexfiles)
+          name = fullfile(d,'vbm8',mexfiles(i).name);
+          spm_unlink(name);
+          disp(name)
+        end
+        s = unzip([url sprintf('vbm8_r%d.zip',rnew)], d);
+        fprintf('%d files have been updated.\nSPM should be restarted.\n',numel(s));
+        restart = spm_input('Restart SPM',1,'m','no|yes',[0 1],2);
+        if restart
+          eval(['spm fmri']);
+        end
+      catch
+        fprintf('Update failed: check file permissions. Download zip-file only.\n');
+        web([url sprintf('vbm8_r%d.zip',rnew)],'-browser');
+        fprintf('Unzip file to %s\n',d);
       end
-      s = unzip([url sprintf('vbm8_r%d.zip',rnew)], d);
-      fprintf('%d files have been updated.\nSPM should be restarted.\n',numel(s));
-      restart = spm_input('Restart SPM',1,'m','no|yes',[0 1],2);
-      if restart
-         eval(['spm fmri']);
-      end
-    catch
-      fprintf('Update failed: check file permissions. Download zip-file only.\n');
+    else
       web([url sprintf('vbm8_r%d.zip',rnew)],'-browser');
       fprintf('Unzip file to %s\n',d);
     end
-  else
-    web([url sprintf('vbm8_r%d.zip',rnew)],'-browser');
-    fprintf('Unzip file to %s\n',d);
   end
 else
-  fprintf('You already have the newest version %d.\n',r);
+  if ~update
+    fprintf('You already have the newest version %d.\n',r);
+  end
 end
