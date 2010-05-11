5c69e36dc7ab8e20a089e168b0601fc0cd4a665e
Christian Gaser <christian.gaser@uni-jena.de>
2010-05-11 21:35:03 +0000

    Changed paths: M       cg_vbm8_write.m M       cg_noise_estimation.m
    Removed noise estimation based on local mean in background because this
    estimation is too unstable. 
    
    
    git-svn-id: svn://141.35.69.214/cat12@326 9f44b166-36c4-483c-8295-4af200601734

--- cg_noise_estimation.m
+++ cg_noise_estimation.m
@@ -22,15 +22,9 @@ localMean = (convn(single(ima),ones(k,k,k),'same')/k^3);
 % find non-zero regions
 ind = find(localMean>0);
 
-% If image has non-zero background (we check that less than 5% of the image are zero) 
-% we can assume Rayleigh PDF in the background and noise estimation can be based on
-% mode of local mean (equation 11 in Aja-Fern‡ndez et al. 2009)
-if length(ind)>0.95*prod(size(ima))
-  h = sqrt(2/pi)*moda(localMean(ind),1000);
-else % otherwise use mode of local variance (equation 15 in Aja-Fern‡ndez et al. 2009)
-  localVar = (convn(single(ima).^2,ones(k,k,k),'same')/k^3) - localMean.^2;
-  h = sqrt(moda(localVar(ind),1000));
-end
+% use mode of local variance (equation 15 in Aja-Fern‡ndez et al. 2009)
+localVar = (convn(single(ima).^2,ones(k,k,k),'same')/k^3) - localMean.^2;
+h = sqrt(moda(localVar(ind),1000));
 
 return
 
