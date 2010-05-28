b9aa42ff729177b5c82fec153944f4ba5ff19dbc
Christian Gaser <christian.gaser@uni-jena.de>
2010-05-28 15:25:17 +0000

    Changed paths: M       kmeans3D.m M       upfirdn2dMex.mexw32 M       cg_noise_estimation.m
    (1) Michael Bodnar has recompiled upfirdn2dMex.c for Windows 32 bit because 
        of some problems with Windows XP and Vista.
    (2) Changed kmeans3D.m to decrease the large memory demand.
    
    
    git-svn-id: svn://141.35.69.214/cat12@339 9f44b166-36c4-483c-8295-4af200601734

--- kmeans3D.m
+++ kmeans3D.m
@@ -1,4 +1,4 @@
-function [mu,mask]=kmeans3D(ima,k)
+function mu = kmeans3D(ima,k)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   kmeans image segmentation
@@ -8,74 +8,69 @@ function [mu,mask]=kmeans3D(ima,k)
 %          k: Number of classes
 %   Output:
 %          mu: vector of class means 
-%          mask: clasification image mask
 %
 %   Author: Jose Vicente Manjon Herrera
 %    Email: jmanjon@fis.upv.es
 %     Date: 27-08-2005
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%_______________________________________________________________________
+% Christian Gaser
+% $Id$
+
+scale = 1000;       % scale to that value to have enough values in the histogram
 
 % check image
-ima=double(ima);
-copy=ima;         % make a copy
-ima=ima(:);       % vectorize ima
-mi=min(ima);      % deal with negative 
-ima=ima-mi+1;     % and zero values
-ima=round(ima);
+ima = double(ima);
+ima = ima(:);       % vectorize ima
+mx  = max(ima);
+ima = scale*ima/mx; % scale to have enough values in the histogram
+mi  = min(ima);     % deal with negative 
+ima = ima-mi+1;     % and zero values
+ima = round(ima);
 
-s=length(ima);
+s   = length(ima);
 
 % create image histogram
 
-m=max(ima)+1;
-h=zeros(1,m);
-hc=zeros(1,m);
+m  = max(ima)+1;
+h  = zeros(1,m);
+hc = zeros(1,m);
 
-for i=1:s
-  if(ima(i)>0) h(ima(i))=h(ima(i))+1;end;
+for i = 1:s
+  if(ima(i)>0) h(ima(i)) = h(ima(i))+1;end;
 end
-ind=find(h);
-hl=length(ind);
+
+ind = find(h);
+hl  = length(ind);
 
 % initiate centroids
 
-mu=(1:k)*m/(k+1);
+mu = (1:k)*m/(k+1);
 
 % start process
 
 while(true)
   
-  oldmu=mu;
+  oldmu = mu;
+  
   % current classification  
- 
-  for i=1:hl
-      c=abs(ind(i)-mu);
-      cc=find(c==min(c));
-      hc(ind(i))=cc(1);
+  for i = 1:hl
+      c = abs(ind(i)-mu);
+      cc = find(c == min(c));
+      hc(ind(i)) = cc(1);
   end
   
-  %recalculation of means  
-  
-  for i=1:k, 
-      a=find(hc==i);
-      mu(i)=sum(a.*h(a))/sum(h(a));
+  % recalculation of means  
+  for i = 1:k, 
+      a = find(hc == i);
+      mu(i) = sum(a.*h(a))/sum(h(a));
   end
   
-  if(mu==oldmu) break;end;
+  if(mu == oldmu) break; end;
   
 end
 
-% calculate mask
-s=size(copy);
-mask=zeros(s);
-for i=1:s(1),
-for j=1:s(2),
-  c=abs(copy(i,j)-mu);
-  a=find(c==min(c));  
-  mask(i,j)=a(1);
-end
-end
-
-mu=mu+mi-1;   % recover real range
+mu = mu+mi-1;   % recover real range
+mu = mx*mu/scale;
 
