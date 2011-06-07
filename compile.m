01727fb569596add017d17cc1b44b142d5080541
Christian Gaser <christian.gaser@uni-jena.de>
2011-06-07 14:13:48 +0000

    Changed paths: D      down_cut01.mexmaci64 M      Makefile.vbm M      sanlmMex.mexw32 A  +   down_cut.mexw64 D      down_cut01.mexw64 M      median3.mexa64 A  +   down_cut.mexmaci M      eikonal3.mexa64 A  +   down_cut.mexglx D      down_cut01.mexglx M      eikonal3.mexmaci M      median3.mexw32 D      down_cut01.mexmaci M      AmapMex.mexw32 M      compile.m M      eikonal3.c M      eikonal3.mexw32 M      median3.c M      eikonal3.mexmaci64 M      median3.mexmaci64 M      sanlmMex.mexw64 M      vbdist.mexw32 A  +   down_cut.mexa64 D      down_cut01.mexa64 M      median3.mexmaci A  +   down_cut.c A  +   down_cut.mexmaci64 M      GBM.m A  +   down_cut.mexw32 D      down_cut01.mexw32 M      median3.mexw64 M      AmapMex.mexw64 M      eikonal3.mexw64 M      median3.mexglx M      vbdist.mexw64 D      down_cut01.c M      eikonal3.mexglx
    Improved speed of skull-stripping by updating many mex-functions.
    
    
    git-svn-id: svn://141.35.69.214/cat12@419 9f44b166-36c4-483c-8295-4af200601734

--- compile.m
+++ compile.m
@@ -1,10 +1,21 @@
 function compile
 
+rand('state',0);
+d = single(rand(50,50,50));
+
 mex -O AmapMex.c Kmeans.c Amap.c MrfPrior.c Pve.c vollib.c
 mex -O median3.c
+d2 = median3(d);
+disp('Compilation of median3 successful')
 mex -O eikonal3.c
-mex -O down_cut01.c
+d2 = eikonal3(d);
+disp('Compilation of eikonal3 successful')
+mex -O down_cut.c
+d2 = down_cut(d,d.^1.5,1);
+disp('Compilation of down_cut successful')
 mex -O vbdist.c
+d2 = vbdist(d);
+disp('Compilation of vbdist successful')
 
 try % try OpenMP support
     if strcmp(mexext,'mexmaci64')
@@ -38,9 +49,5 @@ catch
     mex CFLAGS='-fPIC -O3' -O sanlmMex.c sanlm_float.c 
 end
 
-try
-    sanlmMex(single(rand(50,50,50)),3,1);
-    disp('Compilation of sanlmMex successful')
-catch
-    disp('Compilation of sanlmMex not successful')
-end
+sanlmMex(d,3,1);
+disp('Compilation of sanlmMex successful')
--- eikonal3.c
+++ eikonal3.c
@@ -25,60 +25,43 @@
 #include "math.h"
 #include "float.h"
 
+#define index(A,B,C,DIM) ((C)*DIM[0]*DIM[1] + (B)*DIM[0] + (A))
 
-/* estimate x,y,z position of index i in an array size sx,sxy=sx*sy... */
-void ind2sub(int i,int *x,int *y, int *z, int sxy, int sy) {
-  *z = (int)floor( (double)i / (double)sxy ) +1; 
-   i = i % (sxy);
-  *y = (int)floor( (double)i / (double)sy ) +1;        
-  *x = i % sy + 1;
-}
-
-int sub2ind(int x,int y, int z, int s[]) {
-  int i=(z-1)*s[0]*s[1] + (y-1)*s[0] + (x-1);
-  if (i<0 || i>s[0]*s[1]*s[2]) i=1;
-  return i;
-}
-
-float abs2(float n) {	if (n<0) return -n; else return n; }
+float abs2(float n) { if (n<0) return -n; else return n; }
 
 
 /* main function */
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
-  if (nrhs<1)                                       mexErrMsgTxt("ERROR:eikonal3: not enought input elements\n");
-  if (nrhs>2)                                       mexErrMsgTxt("ERROR:eikonal3: to many input elements.\n");
-  if (nlhs>2)                                       mexErrMsgTxt("ERROR:eikonal3: to many output elements.\n");
+  if (nrhs<1)                                       mexErrMsgTxt("ERROR:eikonal3: not enough input elements\n");
+  if (nrhs>2)                                       mexErrMsgTxt("ERROR:eikonal3: too many input elements.\n");
+  if (nlhs>2)                                       mexErrMsgTxt("ERROR:eikonal3: too many output elements.\n");
   if (mxIsSingle(prhs[0])==0)                       mexErrMsgTxt("ERROR:eikonal3: first  input must be an 3d single matrix\n");
-  if (nrhs==2 && mxIsDouble(prhs[1])==0)            mexErrMsgTxt("ERROR:eikonal3: fourth  input must be an double matrix\n");
-  if (nrhs==2 && mxGetNumberOfElements(prhs[1])!=3) mexErrMsgTxt("ERROR:eikonal3: fourth input must have 3 Elements");
+  if (nrhs==2 && mxIsDouble(prhs[1])==0)            mexErrMsgTxt("ERROR:eikonal3: second input must be an double matrix\n");
+  if (nrhs==2 && mxGetNumberOfElements(prhs[1])!=3) mexErrMsgTxt("ERROR:eikonal3: second input must have 3 Elements");
   
+  int i, j, k, n, ind, x, y, z; 
+  int ni, kll=0;
+  float diff, maxdiffi, maxdiff=1, Dio, DNi;
+  float TH=0.01;
+
   /* main informations about input data (size, dimensions, ...) */
-  const mwSize *sL = mxGetDimensions(prhs[0]); int sSEG[] = {sL[0],sL[1],sL[2]}; 
+  const mwSize *sL = mxGetDimensions(prhs[0]); 
   const int     dL = mxGetNumberOfDimensions(prhs[0]);
   const int     nL = (int)mxGetNumberOfElements(prhs[0]);
-  const int     x  = sL[0];
-  const int     y  = sL[1];
-  const int     xy = x*y;
-
   const int sS[] = {1,3}; 
   mxArray *SS = mxCreateNumericArray(2,sS,mxDOUBLE_CLASS,mxREAL);
-  double*S = mxGetPr(SS);
-  if (nrhs<2) {S[0]=1; S[1]=1; S[2]=1;} else {S=mxGetPr(prhs[1]);}
+  double *S = mxGetPr(SS);
+  if (nrhs<2) {S[0]=1; S[1]=1; S[2]=1;} else {S = mxGetPr(prhs[1]);}
   
   float s1 = abs2((float)S[0]),s2 = abs2((float)S[1]),s3 = abs2((float)S[2]);
   const float   s12  = sqrt( s1*s1  + s2*s2); /* xy - voxel size */
   const float   s13  = sqrt( s1*s1  + s3*s3); /* xz - voxel size */
   const float   s23  = sqrt( s2*s2  + s3*s3); /* yz - voxel size */
   const float   s123 = sqrt(s12*s12 + s3*s3); /* xyz - voxel size */
-  const int     nr = nrhs;
-  
-  /* indices of the neighbor Ni (index distance) and euclidean distance NW */
-  const int   NI[]  = {  1, -1,  x, -x, xy,-xy, -x-1,-x+1,x-1,x+1, -xy-1,-xy+1,xy-1,xy+1, -xy-x,-xy+x,xy-x,xy+x,  -xy-x-1,-xy-x+1,-xy+x-1,-xy+x+1, xy-x-1,xy-x+1,xy+x-1,xy+x+1};  
-  const float ND[]  = { s1, s1, s2, s2, s3, s3,  s12, s12,s12,s12,   s13,  s13, s13, s13,   s23,  s23, s23, s23,     s123,   s123,   s123,   s123,   s123,  s123,  s123,  s123};
-  
-  const int   sN  = sizeof(NI)/4;  
-  float       DN[sN],DI[sN],GMTN[sN],WMDN[sN],SEGN[sN],DNm;
-    
+        
+  /* indices of the euclidean distance NW */
+  const float ND[]  = {s123, s12, s123, s13, s1, s13, s123, s12, s123,   s23, s2, s23, s3, 0.0, s3, s23, s2, s23,   s123, s12, s123, s13, s1, s13, s123, s12, s123};
+
   /* main volumes - actual without memory optimation ... */
   plhs[0] = mxCreateNumericArray(dL,sL,mxSINGLE_CLASS,mxREAL);
   plhs[1] = mxCreateNumericArray(dL,sL,mxSINGLE_CLASS,mxREAL);
@@ -86,60 +69,70 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   float *L = (float *)mxGetPr(plhs[1]);  
     
   /* input variables */
-  float*SEG  = (float *)mxGetPr(prhs[0]);
-  float*F    = (float *)mxGetPr(prhs[1]);
+  float *SEG  = (float *)mxGetPr(prhs[0]);
 
-	float TH=0.01;
   if ( TH>=0.5 || TH<0.0001 ) mexErrMsgTxt("ERROR:eikonal3: threshhold must be >0.0001 and smaller than 0.5\n");
   
-  int i, n; 
   /* intitialisiation */
   for (i=0;i<nL;i++) {
     if ( SEG[i]<0 ) {D[i]=0; L[i]=SEG[i];} 
     else            {D[i]=FLT_MAX; L[i]=0;} 
   }
-  int u,v,w,nu,nv,nw,ni,kll=0;
-  float diff, maxdiffi, maxdiff=1, Dio, DNi;
   
   while ( ( maxdiff > TH ) && kll<2000 ) {
-  	maxdiffi=0;
-  	kll++;
-  	for (i=0;i<nL;i++) {
-      if ( SEG[i]>0 && SEG[i]<FLT_MAX) {
-        ind2sub(i,&u,&v,&w,xy,x);
+    maxdiffi=0;
+    kll++;
+    
+    for (z=0;z<sL[2];z++) for (y=0;y<sL[1];y++) for (x=0;x<sL[0];x++) {
+      ind = index(x,y,z,sL);
+      if ( SEG[ind]>0 && SEG[ind]<FLT_MAX) {
         /* read neighbor values */
-        Dio = D[i];
+        Dio = D[ind];
         
-        for (n=0;n<sN;n++) {
-        	if ( SEG[i]!=FLT_MAX ) {
-	          ni = i + NI[n]; ind2sub(ni,&nu,&nv,&nw,xy,x);
-    	  	 	if ( ( (ni<0) || (ni>=nL) || (abs(nu-u)>1) || (abs(nv-v)>1) || (abs(nw-w)>1) )==0 ) { DNi = D[ni] + ND[n]*SEG[i]; if ( DNi<D[i] ) {D[i]=DNi; L[i]=L[ni]; } } 
-      	  }
-				}
-				
-        diff  = abs2( Dio - D[i] );
-        if ( maxdiffi<diff ) maxdiffi=diff;  
-      }
-    }
-		for (i=nL-1;i<0;i--) {
-      if ( SEG[i]>=0 && SEG[i]<FLT_MAX) {
-        ind2sub(i,&u,&v,&w,xy,x);
+        n = 0;
+        /* go through all elements in a 3x3x3 box */
+        for (i=-1;i<=1;i++) for (j=-1;j<=1;j++) for (k=-1;k<=1;k++) {
+          ni = index(x+i,y+j,z+k,sL);
 
-        /* read neighbor values */
-        Dio = D[i];
+          if ( SEG[ind]!=FLT_MAX ) {
+            if ( ((x+i)>=0) && ((x+i)<sL[0]) && ((y+j)>=0) && ((y+j)<sL[1]) && ((z+k)>=0) && ((z+k)<sL[2]) ) {
+               DNi = D[ni] + ND[n]*SEG[ind];
+               if ( DNi<D[ind] ) {D[ind]=DNi; L[ind]=L[ni]; } 
+            } 
+          }
+          n++;
+        }
         
-        for (n=0;n<sN;n++) {
-        	if ( SEG[i]!=FLT_MAX ) {
-	          ni = i + NI[n]; ind2sub(ni,&nu,&nv,&nw,xy,x);
-    	  	 	if ( ( (ni<0) || (ni>=nL) || (abs(nu-u)>1) || (abs(nv-v)>1) || (abs(nw-w)>1) )==0 ) { DNi = D[ni] + ND[n]*SEG[i]; if ( DNi<D[i] ) {D[i]=DNi; L[i]=L[ni]; } } 
-       	  }
-				}
-				
-        diff  = abs2( Dio - D[i] ); 
+        diff  = abs2( Dio - D[ind] );
         if ( maxdiffi<diff ) maxdiffi=diff;  
       }
     }
-    maxdiff = maxdiffi;  	
+    
+    for (z=sL[2]-1;z>=0;z--) for (y=sL[1]-1;y>=0;y--) for (x=sL[0]-1;x>=0;x--) {
+      ind = index(x,y,z,sL);
+      if ( SEG[ind]>0 && SEG[ind]<FLT_MAX) {
+        /* read neighbor values */
+        Dio = D[ind];
+        
+        n = 0;
+        /* go through all elements in a 3x3x3 box */
+        for (i=-1;i<=1;i++) for (j=-1;j<=1;j++) for (k=-1;k<=1;k++) {
+          ni = index(x+i,y+j,z+k,sL);
+
+          if ( SEG[ind]!=FLT_MAX ) {
+            if ( ((x+i)>=0) && ((x+i)<sL[0]) && ((y+j)>=0) && ((y+j)<sL[1]) && ((z+k)>=0) && ((z+k)<sL[2]) ) {
+               DNi = D[ni] + ND[n]*SEG[ind];
+               if ( DNi<D[ind] ) {D[ind]=DNi; L[ind]=L[ni]; } 
+            } 
+          }
+          n++;
+        }
+        
+        diff  = abs2( Dio - D[ind] );
+        if ( maxdiffi<diff ) maxdiffi=diff;  
+      }
+    }
+
+    maxdiff = maxdiffi;   
   }
-	/* printf("%d",kll); */
 }
