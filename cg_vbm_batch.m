3d7bb6955253d50bec8b91346bcf7852dc7b0897
Christian Gaser <christian.gaser@uni-jena.de>
2013-04-22 15:07:28 +0000

    Changed paths: M       cg_vbm_batch.m M       cg_vbm_run.m M       cg_vbm_batch.sh
    Updated batch system.
    
    
    git-svn-id: svn://141.35.69.214/cat12@479 9f44b166-36c4-483c-8295-4af200601734

--- cg_vbm_batch.m
+++ cg_vbm_batch.m
@@ -33,7 +33,7 @@ else
         eval(['cd ' oldpath])
     end
 end
-global defaults vbm
+global defaults vbm matlabbatch
 
 % always deselect print option
 vbm.extopts.print = 0;
@@ -57,9 +57,50 @@ for i=1:n
 	end
 end
 
+tmp_fields = char('darteltpm','finalmask','gcut','kmeans','mrf','bias_fwhm','BVC','pbtres','mask');
+if writeonly
+    matlabbatch{1}.spm.tools.vbm.write.extopts = rmfield(matlabbatch{1}.spm.tools.vbm.write.extopts,tmp_fields);
+else
+    matlabbatch{1}.spm.tools.vbm.estwrite.extopts = rmfield(matlabbatch{1}.spm.tools.vbm.estwrite.extopts,tmp_fields);
+end
+
+tmp_fields = char('mlT','pcT','surf');
+try
+  if writeonly
+    matlabbatch{1}.spm.tools.vbm.write.output = rmfield(matlabbatch{1}.spm.tools.vbm.write.output,tmp_fields);
+  else
+    matlabbatch{1}.spm.tools.vbm.estwrite.output = rmfield(matlabbatch{1}.spm.tools.vbm.estwrite.output,tmp_fields);
+  end
+end
+
+tmp_fields = char('opts','bias','realign','defs');
+try
+  if writeonly
+    matlabbatch{1}.spm.tools.vbm.write = rmfield(matlabbatch{1}.spm.tools.vbm.write,tmp_fields);
+  else
+    matlabbatch{1}.spm.tools.vbm.estwrite = rmfield(matlabbatch{1}.spm.tools.vbm.estwrite,tmp_fields);
+  end
+end
+
+try
+  if writeonly
+    matlabbatch{1}.spm.tools.vbm.write.output.GM  = rmfield(matlabbatch{1}.spm.tools.vbm.write.output.GM,'mod');
+    matlabbatch{1}.spm.tools.vbm.write.output.WM  = rmfield(matlabbatch{1}.spm.tools.vbm.write.output.WM,'mod');
+    matlabbatch{1}.spm.tools.vbm.write.output.CSF = rmfield(matlabbatch{1}.spm.tools.vbm.write.output.CSF,'mod');
+    matlabbatch{1}.spm.tools.vbm.write.output.mgT = rmfield(matlabbatch{1}.spm.tools.vbm.write.output.mgT,'mod');
+  else
+    matlabbatch{1}.spm.tools.vbm.estwrite.output.GM  = rmfield(matlabbatch{1}.spm.tools.vbm.estwrite.output.GM,'mod');
+    matlabbatch{1}.spm.tools.vbm.estwrite.output.WM  = rmfield(matlabbatch{1}.spm.tools.vbm.estwrite.output.WM,'mod');
+    matlabbatch{1}.spm.tools.vbm.estwrite.output.CSF = rmfield(matlabbatch{1}.spm.tools.vbm.estwrite.output.CSF,'mod');
+    matlabbatch{1}.spm.tools.vbm.estwrite.output.mgT = rmfield(matlabbatch{1}.spm.tools.vbm.estwrite.output.mgT,'mod');
+  end
+end
+
 try
   spm_jobman('initcfg');
   spm_jobman('run',matlabbatch);
+catch
+  error('Batch failed.');
 end
 
 spm_unlink(char(namefile))
