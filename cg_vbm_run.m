331785bb31a12d0e0836c8feeeafba01c2bf7042
Christian Gaser <christian.gaser@uni-jena.de>
2013-05-22 08:20:57 +0000

    Changed paths: M       cg_vbm_run.m M       cg_vbm_batch.sh M       vbm12.man
    (1) Added missing stime variable in cg_vbm_run.
    (2) Log-file for cg_vbm_batch.sh os now saved as default in current folder.
    
    
    git-svn-id: svn://141.35.69.214/cat12@489 9f44b166-36c4-483c-8295-4af200601734

--- cg_vbm_run.m
+++ cg_vbm_run.m
@@ -124,8 +124,8 @@ nit = 1;
 
 for iter=1:nit,
     for subj=1:numel(job.channel(1).vols),
+        stime = clock;
         if estwrite % estimate and write segmentations
-            stime = clock;
           
             images = '';
             for n=1:numel(job.channel),
@@ -252,7 +252,7 @@ for iter=1:nit,
         df = job.warp.write;
         lb = job.label;
         jc = job.jacobian;
-        res.stime=stime;
+        res.stime = stime;
         cg_vbm_write(res, tc, bf, df, lb, jc, job.warp, tpm, job);
 
     end
