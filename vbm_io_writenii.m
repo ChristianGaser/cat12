b71c32021c1c67baa270db3dea7975ea9771e578
Robert Dahnke <robert.dahnke@uni-jena.de>
2013-10-28 17:22:52 +0000

    Changed paths: M       cg_vbm_defaults.m M       cg_vbm_write.m M       tbx_cfg_vbm.m A       templates_1.50mm/aal.nii A       templates_1.50mm/brodmann.nii M       vbm_io_writenii.m M       vbm_tst_pre.m M       vbm_tst_t1qa.m M       vbm_vol_eidist.cpp M       vbm_vol_eidist.mexmaci64
    (1)  Add masked export option for vbm_io_writenii for a correct mapping of local
         defined maps like the cortical thickness map th1.
    (2)  Add label export option for vbm_io_writenii for a correct mapping of atlas 
         maps l#.
    (3)  Add 2 atlas maps (aal, brodmann) to template_1.50mm directory and a simple
         mapping routine without further refinements. (The *.csv Files of both are 
         still missing, but for a first test its ok). 
    (4)  Simplify m* output, where also the locally intensity corrected image of the 
         LAS part is saved under this name.
    (5)  Correct segmentation input for the gcut+ path. Previous, old SPM maps lead 
         to problems with the skull-stripping. 
    (6)  Add a function that estimates the volumes, intensity and thickness values 
         for each ROI of the atlas maps in subject space and write this data to a 
         csv-file per atlas and to the vbm_*.xml.
    
    
    git-svn-id: svn://141.35.69.214/cat12@514 9f44b166-36c4-483c-8295-4af200601734

--- vbm_io_writenii.m
+++ vbm_io_writenii.m
@@ -1,10 +1,10 @@
-function varargout = vbm_io_writenii(V,Y,pre,desc,spmtype,range,write,addpre,transform)
+function varargout = vbm_io_writenii(V,Y,pre,desc,spmtype,range,write,addpre,transform,YM,YMth)
 % ______________________________________________________________________
 % Write an image Y with the properties described by V with the datatype 
 % spmtype for a specific range. Add the prefix pre and the description 
 % desc to V. 
 %
-%   VO = vbm_io_write_nii(Y,V[,pre,desc,spmtype,range,write])
+%   VO = vbm_io_write_nii(Y,V[,pre,desc,spmtype,range,write,addpre,transform,YM,YMth])
 %
 %   Y       = input volume
 %   V       = input volume structure
@@ -21,6 +21,8 @@ function varargout = vbm_io_writenii(V,Y,pre,desc,spmtype,range,write,addpre,tra
 %             1 - add prefix to the filename
 %   transform = transformation data to write the image to warped, 
 %               modulated, or dartel space (see cg_vbm_write)
+%   YM      = mask for the final image (i.e. save thickness and ROIs)
+%   YMth    = threshold for YM 
 %
 % Examples:
 %   
@@ -32,7 +34,7 @@ function varargout = vbm_io_writenii(V,Y,pre,desc,spmtype,range,write,addpre,tra
 %
 % $Id$
 %
-%#ok<*WNOFF,*WNON>
+%#ok<*WNOFF,*WNON,*ASGLU>
  
   % file name
   if ~exist('pre','var'),  pre  = ''; end
@@ -57,7 +59,16 @@ function varargout = vbm_io_writenii(V,Y,pre,desc,spmtype,range,write,addpre,tra
   if ~exist('addpre','var'), addpre = 0; end
   if isstruct(write), write = cell2mat(struct2cell(write)'); end
   if numel(write)==3, write = [write(1:2) 0 write(3)]; end
+  if ~exist('YMth','var'), YMth = 0.5; end
+  if exist('YM','var'), YM=single(YM); end
   
+  %{
+  if exist('mask','var')
+    if ~isfield(mask,'YM'),     error(''); end  
+    if ~isfield(mask,'YMth'),   mask.YMth   = 0.5; end
+    if ~isfield(mask,'YMfill'), mask.YMfill = 1; end
+  end
+  %}
   %if nargout>0, varargout{1}=struct(numel(write),1); end
   %if nargout>1, varargout{2}=struct(numel(write),1); end
   
@@ -78,17 +89,34 @@ function varargout = vbm_io_writenii(V,Y,pre,desc,spmtype,range,write,addpre,tra
 
     if isempty(V.descrip), N.descrip = desc; else  N.descrip = [desc ' < ' V.descrip]; end
     create(N);
-    N.dat(:,:,:) = double(Y);
+    
+    % final masking after transformation
+    if exist('YM','var')
+      N.dat(:,:,:) = double(Y) .* (YM>YMth); 
+    else
+      N.dat(:,:,:) = double(Y);
+    end
     
     if nargout>0, varargout{1}(1) = spm_vol(fname); end
     if nargout>1, varargout{2}{1} = []; end
     clear N; 
   end
 
-  
+  if any(write(2:end))
+    [D,I] = vbdist(single(Y)); Y(:)=Y(I(:)); clear D I; 
+  end
+  switch class(Y)
+    case {'single','double'}, labelmap=0;
+    otherwise, labelmap=2; Y=single(Y); 
+  end
   
   % warped
   % ____________________________________________________________________
+  % If we have a label map we have to correct the result, because spm_diffeo
+  % and spm_field allows no nearest neigbor deformation. Because the 
+  % interpolated values of the boundaries can not be rounded simply (it 
+  % maybe generates another label), we need to replace this voxel by 
+  % its nearest neighbor value.
   if write(2)
     if transform.warped.dartel
       pre2 = ['wr' pre]; desc2 = [desc '(warped non-linear only)'];
@@ -97,11 +125,41 @@ function varargout = vbm_io_writenii(V,Y,pre,desc,spmtype,range,write,addpre,tra
     end
     
     fname = vbm_io_handle_pre(V.fname,pre2,'',addpre,1);
-
-    [wT,w] = spm_diffeo('push',Y,transform.warped.y,transform.warped.odim(1:3));
-    spm_field('bound',1);
-    wT     = spm_field(w,wT,[sqrt(sum(transform.warped.M1(1:3,1:3).^2)) 1e-6 1e-4 0  3 2]);
+    if labelmap==0
+      [wT,w]  = spm_diffeo('push',Y ,transform.warped.y,transform.warped.odim(1:3)); wT0=wT==0;
+      spm_field('bound',1);
+      wT      = spm_field(w,wT ,[sqrt(sum(transform.warped.M1(1:3,1:3).^2)) 1e-6 1e-4 0  3 2]); 
+      wT(wT0) = 0; % clear regions that were not defined in the deformation
+    elseif labelmap==1
+      [wT,w]  = spm_diffeo('push',Y ,transform.warped.y,transform.warped.odim(1:3)); wT0=wT==0;
+      spm_field('bound',1);
+      wT      = spm_field(w,wT ,[sqrt(sum(transform.warped.M1(1:3,1:3).^2)) 1e-6 1e-4 0  3 2]); 
+      wT(wT0) = 0; % clear regions that were not defined in the deformation
+      wT = round(wT*10000)/10000; 
+      wT(round(wT)~=wT | isnan(wT) | wT>max(Y(:))+1)=0;
+      [D,I] = vbdist(wT); wT(:)=wT(I(:)); clear D I; 
+    elseif labelmap==2
+      wT = zeros([transform.warped.odim(1:3),max(Y(:))],'uint8'); 
+      for yi=1:max(Y(:)); 
+        [wTi,w]  = spm_diffeo('push',single(Y==yi),transform.warped.y,transform.warped.odim(1:3));
+        %spm_field('bound',1);
+        %wTi     = spm_field(w,wTi,[sqrt(sum(transform.warped.M1(1:3,1:3).^2)) 1e-6 1e-4 0  3 2]); 
+        wT(:,:,:,yi) = uint8(wTi*100); 
+      end
+      [wTmax,wT] = max(wT,[],4); 
+    end  
     clear w;
+    
+    % final masking after transformation
+    if exist('YM','var')
+      [wTM,w] = spm_diffeo('push',YM,transform.warped.y,transform.warped.odim(1:3)); wT0=wT==0;
+      spm_field('bound',1);
+      wTM = spm_field(w,wTM,[sqrt(sum(transform.warped.M1(1:3,1:3).^2)) 1e-6 1e-4 0  3 2]); 
+      wT(wT0)=0; clear wT0; % clear regions that were not defined in the deformation
+      wTM = round(wTM*100)/100; 
+      wT  = wT .* (wTM>YMth);
+      clear w;
+    end
     
     N      = nifti;
     N.dat  = file_array(fname,transform.warped.odim, ...
@@ -122,6 +180,7 @@ function varargout = vbm_io_writenii(V,Y,pre,desc,spmtype,range,write,addpre,tra
   
   % modulated
   % ____________________________________________________________________
+  % no label case ...
   if write(3)
        
     if transform.warped.dartel
@@ -138,10 +197,18 @@ function varargout = vbm_io_writenii(V,Y,pre,desc,spmtype,range,write,addpre,tra
     
     if write(3)==2
       [wT,wr] = spm_diffeo('push',Y,transform.warped.y,transform.warped.odim(1:3)); 
+      if exist('YM','var') % final masking after transformation
+        wTM = spm_diffeo('push',YM,transform.warped.y,transform.warped.odim(1:3)); 
+        wT = wT .* (wTM>YMth);
+      end
       %w       = vbm_vol_smooth3X(wr,1.0); wT = wT./wr.*w; clear wr w;  % smoother warping
     else
       if ~exist('wT','var')
         wT = spm_diffeo('push',Y,transform.warped.y,transform.warped.odim(1:3));   
+        if exist('YM','var') % final masking after transformation
+          wTM = spm_diffeo('push',YM,transform.warped.y,transform.warped.odim(1:3));   
+          wT = wT .* (wTM>YMth); clear wTM; 
+        end
       end
     end
     
@@ -179,40 +246,60 @@ function varargout = vbm_io_writenii(V,Y,pre,desc,spmtype,range,write,addpre,tra
   % write dartel files
   % ____________________________________________________________________
   if write(4) 
-    if write(4)==1 
+    if write(4)==1 && isfield(transform,'rigid')
       transf=transform.rigid; 
       pre4=['ra' pre]; post=''; desc4 = [desc '(rigid)'];
-    elseif write(4)==2, 
+    elseif write(4)==2 && isfield(transform,'affine')
       transf=transform.affine;   
       pre4=['ra' pre]; post='_affine'; desc4 = [desc '(affine)'];
+    elseif isfield(transform,'rigid')
+      transf=transform.rigid; 
+      pre4=['ra' pre]; post=''; desc4 = [desc '(rigid)'];
+    elseif isfield(transform,'affine')
+      transf=transform.affine;   
+      pre4=['ra' pre]; post='_affine'; desc4 = [desc '(affine)'];      
     end
 
-    fname = vbm_io_handle_pre(V.fname,pre4,post,addpre,1);
+    if exist('pre4','var')
+      fname = vbm_io_handle_pre(V.fname,pre4,post,addpre,1);
 
-    VraT = struct('fname',fname,'dim',transf.odim,...
-         'dt',   [spm_type(spmtype) spm_platform('bigend')],...
-         'pinfo',[range(2) range(1)]','mat',transf.mat);%[1.0 0]'
-    VraT = spm_create_vol(VraT);
+      VraT = struct('fname',fname,'dim',transf.odim,...
+           'dt',   [spm_type(spmtype) spm_platform('bigend')],...
+           'pinfo',[range(2) range(1)]','mat',transf.mat);%[1.0 0]'
+      VraT = spm_create_vol(VraT);
 
-    N  = nifti(VraT.fname);
+      N  = nifti(VraT.fname);
 
-    % get rid of the QFORM0 rounding warning
-    warning off
-    N.mat0  = transf.mat; % hier stand mit mat0, aber da stimmten die dimensionen nicht!!!
-    warning on
+      % get rid of the QFORM0 rounding warning
+      warning off
+      N.mat0  = transf.mat; % hier stand mit mat0, aber da stimmten die dimensionen nicht!!!
+      warning on
 
-    N.mat_intent  = 'Aligned';
-    N.mat0_intent = 'Aligned';
-    if isempty(V.descrip), N.descrip = desc; else  N.descrip = [desc4 ' < ' V.descrip]; end
-    create(N);
+      N.mat_intent  = 'Aligned';
+      N.mat0_intent = 'Aligned';
+      if isempty(V.descrip), N.descrip = desc; else  N.descrip = [desc4 ' < ' V.descrip]; end
+      create(N);
 
-    for i=1:transf.odim(3),
-      tmp  = spm_slice_vol(double(Y),transf.M*spm_matrix([0 0 i]),transf.odim(1:2),[1,NaN]);
-      VraT = spm_write_plane(VraT,tmp,i);
+      for i=1:transf.odim(3),
+        if labelmap
+          tmp  = spm_slice_vol(double(Y) ,transf.M*spm_matrix([0 0 i]),transf.odim(1:2),0);
+        else
+          tmp  = spm_slice_vol(double(Y) ,transf.M*spm_matrix([0 0 i]),transf.odim(1:2),[1,NaN]);
+        end
+        if exist('YM','var') % final masking after transformation
+          if labelmap
+            tmpM = spm_slice_vol(double(YM),transf.M*spm_matrix([0 0 i]),transf.odim(1:2),0);
+          else
+            tmpM = spm_slice_vol(double(YM),transf.M*spm_matrix([0 0 i]),transf.odim(1:2),[1,NaN]);
+          end
+          tmp  = tmp .* (tmpM>YMth); clear tmpM; 
+        end
+        VraT = spm_write_plane(VraT,tmp,i);
+      end
+
+      if nargout>0, varargout{1}(4) = spm_vol(fname); end
+      if nargout>1, varargout{2}{4} = []; end
     end
-    
-    if nargout>0, varargout{1}(4) = spm_vol(fname); end
-    if nargout>1, varargout{2}{4} = []; end
   end
 
   
@@ -235,7 +322,7 @@ function FO = vbm_io_handle_pre(F,pre,post,addpre,existfile)
       if numel(ff)>pf+1 && any(strcmp(ff(1:pf),prefix{pf})) && ...
         (~existfile || exist(fullfile(pp,[ff(pf+1:end) ee]),'file'))
          FN = vbm_io_handle_pre(fullfile(pp,[ff(pf+1:end) ee]),'','',addpre,existfile); 
-         if (~existfile || exist(FN,'file')), [ppn,ffn] = spm_fileparts(FN); ff=ffn; end
+         if (~existfile || exist(FN,'file')), [ppn,ffn] = spm_fileparts(FN); ff=ffn; end 
       end
     end
   end
