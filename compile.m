6a44fec25a7e129a843867ce4444d6cb80f9e472
Christian Gaser <christian.gaser@uni-jena.de>
2013-12-12 09:43:04 +0000

    Changed paths: M       AmapMex.mexmaci64 M       Makefile.vbm A       cg_boxplot.m M       cg_check_cov.m A       cg_check_mesh_cov.m M       cg_vbm_batch.m M       cg_vbm_defaults.m M       cg_vbm_tools.m M       compile.m M       sanlmMex.mexmaci64 M       sanlmMex_noopenmp.mexmaci64 M       spm_vbm12.m M       vbdist.mexmaci64 M       vbm12.man M       vbm_vol_downcut.mexmaci64 M       vbm_vol_eidist.mexmaci64 M       vbm_vol_eikonal3.mexmaci64 M       vbm_vol_gradient3.mexmaci64 M       vbm_vol_interp3f.mexmaci64 M       vbm_vol_laplace3.mexmaci64 M       vbm_vol_laplace3R.mexmaci64 M       vbm_vol_localstat.mexmaci64 M       vbm_vol_median3.mexmaci64 M       vbm_vol_median3c.mexmaci64 M       vbm_vol_pbtp.mexmaci64 M       vbm_vol_simgrow.mexmaci64
    (1) Removed multi-thread support for sanlmMex because results differ and it's
        not working for OSX < 10.6.
    (2) Compiled mex-files for OSX < 10.6.
    (3) Added cg_check_mesh_cov.m to check surface covariance.
    (4) Added cg_boxplot (extracted from cg_check_cov.m).
    
    
    git-svn-id: svn://141.35.69.214/cat12@546 9f44b166-36c4-483c-8295-4af200601734

--- compile.m
+++ compile.m
@@ -34,13 +34,13 @@ function compile
 
   try % try OpenMP support
       if strcmp(mexext,'mexmaci64')
-          mex CC='gcc-4.4' CFLAGS='-U_OPENMP -m64 -fPIC -O3' -O sanlmMex.c sanlm_float.c
+          mex CC='gcc-4.0' CFLAGS='-U_OPENMP -m64 -fPIC -O3' -O sanlmMex.c sanlm_float.c
           movefile(['sanlmMex.' mexext], ['sanlmMex_noopenmp.' mexext],'f');
-          mex CC='gcc-4.4' CFLAGS='-fopenmp -m64 -fPIC -O3' -O /usr/local/lib/x86_64/libgomp.a sanlmMex.c sanlm_float.c
+          mex CC='gcc-4.0' CFLAGS='-m64 -fPIC -O3' -O /usr/local/lib/libgomp.a sanlmMex.c sanlm_float.c
       elseif strcmp(mexext,'mexmaci')
           mex CC='gcc-4.0' CFLAGS='-U_OPENMP -m32 -fPIC -O3' -O sanlmMex.c sanlm_float.c
           movefile(['sanlmMex.' mexext], ['sanlmMex_noopenmp.' mexext],'f');
-          mex CC='gcc-4.0' CFLAGS='-fopenmp -m32 -fPIC -O3' -O /usr/local/lib/x86/libgomp.a sanlmMex.c sanlm_float.c
+          mex CC='gcc-4.0' CFLAGS='-fopenmp -m32 -fPIC -O3' -O /usr/local/lib/libgomp.a sanlmMex.c sanlm_float.c
       elseif strcmp(mexext,'mexa64')
           mex CFLAGS='-U_OPENMP -m64 -fPIC -O3' -O sanlmMex.c sanlm_float.c
           movefile(['sanlmMex.' mexext], ['sanlmMex_noopenmp.' mexext],'f');
