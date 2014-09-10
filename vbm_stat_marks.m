50bef0d0f4fe09522f49e304e3c0f91ca60c1bd3
Robert Dahnke <robert.dahnke@uni-jena.de>
2014-09-10 15:01:16 +0000

    Changed paths: M       cg_vbm_write.m M       distribute_to_server.sh M       vbm_io_xml.m M       vbm_stat_marks.m M       vbm_tst_qa.m M       vbm_vol_sanlm.m
    (1)  Added some comments in "vbm_vol_sanlm" and "distribute_to_server.sh".
    (2)  Corrected "vbm_io_xml" write error for empty structures.
    (3)  Corrected small export/display errors. 
    (4)  Updated "vbm_tst_qa" and "vbm_stat_marks" functions for "simpler" quality 
         measures that were restricted to uncorrected exor corrected images. 
    
    
    git-svn-id: svn://141.35.69.214/cat12@598 9f44b166-36c4-483c-8295-4af200601734

--- vbm_stat_marks.m
+++ vbm_stat_marks.m
@@ -63,76 +63,43 @@ function varargout = vbm_stat_marks(action,uselevel,varargin)
    'FD'  'Fp0'                   ''          []                 0 0     'fname'
 %  % -- further image quality measures on the original image --------------
   % - resolution - 
-   'QMo'  'res_RMS'              'linear'    [  0.75   2.00]    1 1     'RMS error of voxel size'
-   'QMm'  'res_RMS'              'linear'    [  0.75   2.00]    1 1     'RMS error of voxel size'
-   'QMv'  'res_RMS'              'linear'    [  0.75   2.00]    1 1     'RMS error of voxel size'
-   'QMo'  'res_BB'               'normal'    [     0   1000]    1 1     'brain next to the image boundary'
-   'QMm'  'res_BB'               'normal'    [     0   1000]    1 1     'brain next to the image boundary'
-   'QMv'  'res_BB'               'normal'    [     0   1000]    1 1     'brain next to the image boundary'
-   'QMo'  'res_vx_vol'           'linear'    [  0.75   3.00]    1 0     'voxel dimensions'
-   'QMm'  'res_vx_vol'           'linear'    [  0.75   3.00]    1 0     'voxel dimensions'
-   'QMv'  'res_vx_vol'           'linear'    [  0.75   3.00]    1 0     'voxel dimensions'
-   'QMo'  'res_vol'              'linear'    [  0.50   8.00]    1 0     'voxel volume'
-   'QMm'  'res_vol'              'linear'    [  0.50   8.00]    1 0     'voxel volume'
-   'QMv'  'res_vol'              'linear'    [  0.50   8.00]    1 0     'voxel volume'
-   'QMo'  'res_isotropy'         'linear'    [  1.00    7/3]    1 0     'voxel isotropy'
-   'QMm'  'res_isotropy'         'linear'    [  1.00    7/3]    1 0     'voxel isotropy'
-   'QMv'  'res_isotropy'         'linear'    [  1.00    7/3]    1 0     'voxel isotropy'
+   'QM'  'res_RMS'               'linear'    [  0.75   2.00]    1 1     'RMS error of voxel size'
+   'QM'  'res_BB'                'normal'    [     0   1000]    1 1     'brain next to the image boundary'
+   'QM'  'res_vx_vol'            'linear'    [  0.75   3.00]    1 0     'voxel dimensions'
+   'QM'  'res_vol'               'linear'    [  0.50   8.00]    1 0     'voxel volume'
+   'QM'  'res_isotropy'          'linear'    [  1.00    7/3]    1 0     'voxel isotropy'
   % - tissue mean and varianz - 
-   'QMo'  'tissue_mn'             'normal'    def.tissue        1 1     'mean within the tissue classes'
-   'QMm'  'tissue_mn'             'normal'    def.tissue        1 1     'mean within the tissue classes'
-   'QMv'  'tissue_mn'             'normal'    def.tissue        1 1     'mean within the tissue classes'
-   'QMo'  'tissue_std'            'normal'    [  0.10   0.20]   1 1     'std within the tissue classes'
-   'QMm'  'tissue_std'            'normal'    [  0.10   0.20]   1 1     'std within the tissue classes'
-   'QMv'  'tissue_std'            'normal'    [  0.10   0.20]   1 1     'std within the tissue classes'
+   'QM'  'tissue_mn'             'normal'    def.tissue         1 1     'mean within the tissue classes'
+   'QM'  'tissue_std'            'normal'    [  0.10   0.20]    1 1     'std within the tissue classes'
+   'QM'  'tissue_std'            'normal'    [  0.10   0.20]    1 1     'std within the tissue classes'
   % - contrast - 
-   'QMo'  'contrast'              'linear'    [   1/3   1/12]   1 1     'contrast between tissue classe'
-   'QMm'  'contrast'              'linear'    [   1/3   1/12]   1 1     'contrast between tissue classe'
-   'QMv'  'contrast'              'linear'    [   1/3   1/12]   1 1     'contrast between tissue classe'
+   'QM'  'contrast'              'linear'    [   1/3   1/12]    1 1     'contrast between tissue classe'
   % - noise & contrast -
-   'QMo'  'NCR'                   'linear'    [  1/20    1/2]   1 1     'noise to contrast ratio'
-   'QMm'  'NCR'                   'linear'    [  1/20    1/2]   1 1     'noise to contrast ratio'
-   'QMv'  'NCR'                   'linear'    [  1/20    1/8]   1 1     'noise to contrast ratio'
-   'QMo'  'CNR'                   'linear'    [    20      2]   1 1     'contrast to noise ratio'
-   'QMm'  'CNR'                   'linear'    [    20      2]   1 1     'contrast to noise ratio'
-   'QMv'  'CNR'                   'linear'    [    20      8]   1 1     'contrast to noise ratio'
+   'QM'  'NCR'                   'linear'    [  1/20    1/2]    1 1     'noise to contrast råatio'
+   'QM'  'CNR'                   'linear'    [    20      2]    1 1     'contrast to noise ratio'
   % - inhomogeneity & contrast -
-   'QMo'  'ICR'                   'linear'    [  1/10      1]   1 1     'inhomogeneity to contrast ratio'
-   'QMm'  'ICR'                   'linear'    [  1/10      1]   1 1     'inhomogeneity to contrast ratio'
-   'QMv'  'ICR'                   'linear'    [  1/10    1/8]   1 1     'inhomogeneity to contrast ratio'
-   'QMo'  'CIR'                   'linear'    [    10      1]   1 1     'contrast to inhomogeneity ratio'
-   'QMm'  'CIR'                   'linear'    [    10      1]   1 1     'contrast to inhomogeneity ratio'
-   'QMv'  'CIR'                   'linear'    [    10      8]   1 1     'contrast to inhomogeneity ratio'
+   'QM'  'ICR'                   'linear'    [  1/10      1]    1 1     'inhomogeneity to contrast ratio'
+   'QM'  'CIR'                   'linear'    [    10      1]    1 1     'contrast to inhomogeneity ratio'
   % - artefacts & resolution -
-   'QMo'  'NERR'                  'linear'    [   1/5    1/2]   2 1     'noise edge resolution relation'
-   'QMm'  'NERR'                  'linear'    [   1/5    1/2]   2 1     'noise edge resolution relation'
-   'QMv'  'NERR'                  'linear'    [   1/5    1/4]   2 1     'noise edge resolution relation'
+   'QM'  'NERR'                  'linear'    [   1/5    1/2]    2 1     'noise edge resolution relation'
   % - subject measures / preprocessing measures -
-   'QMo'  'MPC'                   'linear'    [  0.08   0.32]   2 1     'changes between t1 and label'
-   'QMm'  'MPC'                   'linear'    [  0.08   0.32]   2 1     'changes between t1 and label'
-   'QMv'  'MPC'                   'linear'    [  0.08   0.16]   2 1     'changes between t1 and label'
-   'QMo'  'MJD'                   'linear'    [  0.05   0.15]   2 1     'changes between t1 and label'
-   'QMm'  'MJD'                   'linear'    [  0.05   0.15]   2 1     'changes between t1 and label'
-   'QMv'  'MJD'                   'linear'    [  0.05   0.15]   2 1     'changes between t1 and label'
-   'QMo'  'STC'                   'linear'    [  0.05   0.15]   2 1     'difference between template and label'
-   'QMm'  'STC'                   'linear'    [  0.05   0.15]   2 1     'difference between template and label'
-   'QMv'  'STC'                   'linear'    [  0.05   0.15]   2 1     'difference between template and label'
+%   'QM'  'CJV'                   'linear'    [  0.12   0.18]    2 1     'coefficiant of variation - avg. std in GM and WM'
+%   'QM'  'MPC'                   'linear'    [  0.06   0.12]    2 1     'mean preprocessing change map - diff. betw. opt. T1 and p0'
+   'QM'  'CJV'                   'linear'    [  0.12   0.36]    2 1     'coefficiant of variation - avg. std in GM and WM'
+   'QM'  'MPC'                   'linear'    [  0.06   0.26]    2 1     'mean preprocessing change map - diff. betw. opt. T1 and p0'
+   'QM'  'MJD'                   'linear'    [  0.05   0.15]    2 1     'mean jacobian determinant'
+   'QM'  'STC'                   'linear'    [  0.05   0.15]    2 1     'difference between template and label'
 % -- subject-related data from the preprocessing -----------------------
-   'SM'  'vol_TIV'               'normal'    [  1500   1000]   1 1     'total intracranial volume (GM+WM+VT)'
-  %'SM'  'vol_CHvsGW'            'linear'    def.CHvsCG        2 1     'relation between brain and non brain'
-  %'SM'  'vol_abs_CGW'           'linear'    def.tisvola       1 1     'absolut  tissue volume (CSF,GM,WM)'
-  %'SM'  'vol_abs_BG'            'linear'    [    10     10]   1 1     'absolut  tissue volume of basal structures'
-  %'SM'  'vol_abs_VT'            'linear'    [    10     10]   1 1     'absolut  tissue volume of the ventricle'
-  %'SM'  'vol_abs_BV'            'linear'    [     0     10]   1 1     'absolut  blood vessel volume'
-  %'SM'  'vol_abs_WMH'           'linear'    [     0     10]   2 1     'absolut WMH volume'
-   'SM'  'vol_rel_CGW'           'linear'    def.tisvolr       1 1     'relative tissue volume (CSF,GM,WM)'
-   'SM'  'vol_rel_BG'            'linear'    [  0.05   0.05]   2 1     'relative tissue volume of basal structures'
-   'SM'  'vol_rel_VT'            'linear'    [  0.05   0.05]   2 1     'relative tissue volume of the ventricle'
-   'SM'  'vol_rel_BV'            'linear'    [  0.00   0.05]   2 1     'relative blood vessel volume'
-   'SM'  'vol_rel_WMH'           'linear'    [  0.00   0.05]   2 1     'relative WMH volume'
-   'SM'  'dist_thickness'        'normal'    def.thickness     1 1     'absolut  thickness (CSF,GM,WM)'
-   'SM'  'dist_abs_depth'        'normal'    [  5.00   2.00]   0 0     'absolut  sulcal depth'
-   'SM'  'dist_rel_depth'        'normal'    [  0.50   0.20]   0 0     'relative sulcal depth'
+   'SM'  'vol_TIV'               'normal'    [  1500   1000]    1 1     'total intracranial volume (GM+WM+VT)'
+   'SM'  'vol_CHvsGW'            'linear'    def.CHvsCG         2 1     'relation between brain and non brain'
+   'SM'  'vol_rel_CGW'           'linear'    def.tisvolr        1 1     'relative tissue volume (CSF,GM,WM)'
+   'SM'  'vol_rel_BG'            'linear'    [  0.05   0.05]    2 1     'relative tissue volume of basal structures'
+   'SM'  'vol_rel_VT'            'linear'    [  0.05   0.05]    2 1     'relative tissue volume of the ventricle'
+   'SM'  'vol_rel_BV'            'linear'    [  0.00   0.05]    2 1     'relative blood vessel volume'
+   'SM'  'vol_rel_WMH'           'linear'    [  0.00   0.05]    2 1     'relative WMH volume'
+   'SM'  'dist_thickness'        'normal'    def.thickness      1 1     'absolut  thickness (CSF,GM,WM)'
+   'SM'  'dist_abs_depth'        'normal'    [  5.00   2.00]    0 0     'absolut  sulcal depth'
+   'SM'  'dist_rel_depth'        'normal'    [  0.50   0.20]    0 0     'relative sulcal depth'
   };
   % create structure
   for QSi=1:size(def.QS,1)
@@ -144,17 +111,15 @@ function varargout = vbm_stat_marks(action,uselevel,varargin)
       end
     end
   end
-  def.QMo.avg = {'res_RMS','NCR','ICR','MPC'}; %,'NERR'
-  def.QMm.avg = def.QMo.avg; 
-  def.QMv.avg = def.QMo.avg; 
+  def.QM.avg = {'res_RMS','NCR','ICR','MPC','CJV'}; %,'NERR'
   def.SM.avg = {'vol_rel_CGW'};
   
 
   % mark functions
   setnan=[1 nan];
-  evalnormal  = @(x,best,worst,marks) min(9.5-eps,max(0,(abs(best-x)./worst)*(marks-1))) + setnan(isnan(x)+1);      
+  evalnormal  = @(x,best,worst,marks) min(90.5-eps,max(0,(abs(best-x)./worst)*(marks-1))) + setnan(isnan(x)+1);      
   evalnormalb = @(x,best,worst,marks) min(marks  ,max(1,(abs(best-x)./worst)*(marks-1))) + setnan(isnan(x)+1);    
-  evallinear  = @(x,best,worst,marks) min(9.5-eps,max(0,((best-x)./diff([worst,best])*(marks-1)))) + setnan(isnan(x)+1);   
+  evallinear  = @(x,best,worst,marks) min(90.5-eps,max(0,((best-x)./diff([worst,best])*(marks-1)))) + setnan(isnan(x)+1);   
   evallinearb = @(x,best,worst,marks) min(marks  ,max(1,((best-x)./diff([worst,best])*(marks-1)))) + setnan(isnan(x)+1);   
 
   
@@ -178,7 +143,7 @@ function varargout = vbm_stat_marks(action,uselevel,varargin)
       if ~isstruct(varargin{1})
         error('MATLAB:vbm_stat_marks:input','Second input has to be a structure!\n');
       end
-      
+       
       % evaluation
       QA = varargin{1};
       for QSi=1:size(def.QS,1)
@@ -223,11 +188,11 @@ function varargout = vbm_stat_marks(action,uselevel,varargin)
           end  
         end
       end
-      QAM.QMv=QAM.QMm;
+     
       
       %% average
-      Qavg = {'QMo','QMm','QMv','SM'};
-      for Qavgi=1:4;
+      Qavg = {'QM','SM'};
+      for Qavgi=1:numel(Qavg);
         if isfield(QAM,Qavg{Qavgi})
           QAM.(Qavg{Qavgi}).mean = 0; 
           QAM.(Qavg{Qavgi}).max  = 0;
@@ -293,7 +258,7 @@ function varargout = vbm_stat_marks(action,uselevel,varargin)
       varargout{1} = QAM;
     case 'init',    % ausgabe einer leeren struktur
       varargout{1} = QS;
-      varargout{2} = def.QMo.avg; 
+      varargout{2} = def.QM.avg; 
     case 'marks',    % ausgabe einer leeren struktur
       varargout{1} = def.QS;
   end
