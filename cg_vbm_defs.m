a5ab2b9687b0c4bc5f1c45d0dd42d05d8285ae9f
Robert Dahnke <robert.dahnke@uni-jena.de>
2014-10-23 10:11:37 +0000

    Changed paths: M       cg_vbm_defs.m
    (1)  Added spm_load_float (that was removed in SPM12) into cg_vbm_defs (the only
         function that requires this function.
    
    
    git-svn-id: svn://141.35.69.214/cat12@640 9f44b166-36c4-483c-8295-4af200601734

--- cg_vbm_defs.m
+++ cg_vbm_defs.m
@@ -140,6 +140,7 @@ for d=1:3,
     y1(:,:,:,d) = y1(:,:,:,d) + y0(:,:,:,2)*M(d,2);
     y1(:,:,:,d) = y1(:,:,:,d) + y0(:,:,:,3)*M(d,3) + M(d,4);
 end
+return;
 %=======================================================================
 
 %=======================================================================
@@ -151,5 +152,30 @@ for i=1:d(3),
     x(:,:,i,2) = x2;
     x(:,:,i,3) = single(i);
 end
+return;
 %=======================================================================
+
+%=======================================================================
+function dat = spm_load_float(V)
+% Load a volume into a floating point array
+% FORMAT dat = spm_load_float(V)
+% V   - handle from spm_vol
+% dat - a 3D floating point array
+%_______________________________________________________________________
+% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+
+% John Ashburner
+% $Id$
+
+
+dim = V(1).dim(1:3);
+dat = single(0);
+dat(dim(1),dim(2),dim(3))=0;
+for i=1:V(1).dim(3),
+    M = spm_matrix([0 0 i]);
+    dat(:,:,i) = single(spm_slice_vol(V(1),M,dim(1:2),1));
+end;
+return;
+%=======================================================================
+
 
