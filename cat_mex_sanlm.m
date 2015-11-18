1614f8292310596442ad735aecb4520955800303
Christian Gaser <christian.gaser@uni-jena.de>
2015-11-18 10:54:01 +0000

    Changed paths: M       CHANGES.txt R  +    Makefile D       Makefile.cat D       Makefile.var D       cat_amap_mex.c D       cat_amap_mex.m D       cat_amap_mex.mexa64 D       cat_amap_mex.mexglx D       cat_amap_mex.mexmaci64 D       cat_amap_mex.mexw32 D       cat_amap_mex.mexw64 M       cat_main.m A  +    cat_mex_amap.c A  +    cat_mex_amap.m A  +    cat_mex_amap.mexa64 A  +    cat_mex_amap.mexglx A  +    cat_mex_amap.mexmaci64 A  +    cat_mex_amap.mexw32 A  +    cat_mex_amap.mexw64 A  +    cat_mex_ornlm.c A  +    cat_mex_ornlm.m A  +    cat_mex_ornlm.mexa64 A  +    cat_mex_ornlm.mexglx A  +    cat_mex_ornlm.mexmaci A  +    cat_mex_ornlm.mexmaci64 A  +    cat_mex_ornlm.mexw32 A  +    cat_mex_ornlm.mexw64 A  +    cat_mex_sanlm.c A  +    cat_mex_sanlm.m A  +    cat_mex_sanlm.mexa64 A  +    cat_mex_sanlm.mexglx A  +    cat_mex_sanlm.mexmaci64 A  +    cat_mex_sanlm.mexw32 A  +    cat_mex_sanlm.mexw64 D       cat_ornlm_mex.c D       cat_ornlm_mex.m D       cat_ornlm_mex.mexa64 D       cat_ornlm_mex.mexglx D       cat_ornlm_mex.mexmaci D       cat_ornlm_mex.mexmaci64 D       cat_ornlm_mex.mexw32 D       cat_ornlm_mex.mexw64 M       cat_run_job.m D       cat_sanlm_mex.c D       cat_sanlm_mex.m D       cat_sanlm_mex.mexa64 D       cat_sanlm_mex.mexglx D       cat_sanlm_mex.mexmaci64 D       cat_sanlm_mex.mexw32 D       cat_sanlm_mex.mexw64 M       cat_vbdist.mexmaci64 M       cat_vol_downcut.mexmaci64 M       cat_vol_eidist.mexmaci64 M       cat_vol_genus0.mexmaci64 M       cat_vol_gradient3.mexmaci64 M       cat_vol_interp3f.mexmaci64 M       cat_vol_isarnlm.m M       cat_vol_laplace3.mexmaci64 M       cat_vol_laplace3R.mexmaci64 M       cat_vol_localstat.mexmaci64 M       cat_vol_median3.mexmaci64 M       cat_vol_median3c.mexmaci64 M       cat_vol_pbtp.mexmaci64 M       cat_vol_sanlm.m M       cat_vol_simgrow.mexmaci64 M       compile.m
    (1) Removed Makefile.var and renamed Makefile.cat to Makefile because all
        the compilation is done inside compile.m
    (2) Renamed mex-files according to cat_mex_name.*.
    
    
    git-svn-id: svn://141.35.69.214/cat12@771 9f44b166-36c4-483c-8295-4af200601734

--- cat_mex_sanlm.m
+++ cat_mex_sanlm.m
@@ -1,5 +1,5 @@
-function cat_sanlm_mex(in, v, f)
-% FORMAT cat_sanlm_mex(in, v, f)
+function cat_mex_sanlm(in, v, f)
+% FORMAT cat_mex_sanlm(in, v, f)
 % 
 % Spatial Adaptive Non Local Means Denoising Filter
 %
@@ -23,14 +23,14 @@ function cat_sanlm_mex(in, v, f)
 
 rev = '$Rev$';
 
-disp('Compiling cat_sanlm_mex.c')
+disp('Compiling cat_mex_sanlm.c')
 
 pth = fileparts(which(mfilename));
 p_path = pwd;
 cd(pth);
-mex -O cat_sanlm_mex.c sanlm_float.c 
+mex -O cat_mex_sanlm.c sanlm_float.c 
 cd(p_path);
 
-cat_sanlm_mex(in, v, f);
+cat_mex_sanlm(in, v, f);
 
 return
