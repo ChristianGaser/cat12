2138a661bc38b0994afec25a42626653028756b3
Christian Gaser <christian.gaser@uni-jena.de>
2015-11-20 10:19:09 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_batch_vbm.m M       cat_batch_vbm.sh MM      cat_stat_TIV.m M       html/cat.html
    Updated batch mode files.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@778 9f44b166-36c4-483c-8295-4af200601734

--- cat_stat_TIV.m
+++ cat_stat_TIV.m
@@ -1,17 +1,19 @@
-function cat_stat_TIV
+function TIV = cat_stat_TIV
 %cat_stat_TIV to read total intracranial volume (TIV) from xml-files
 %
 %_______________________________________________________________________
 % Christian Gaser
-% $Id: cat_stat_TIV.m 769 2015-11-17 23:06:11Z gaser $
+% $Id$
 
 P = spm_select(Inf,'xml','select xml-files');
 
-spm_progress_bar('Init',size(P,1),'Load xml-files','subjects completed')
-for i=1:size(P,1)
-    xml = convert(xmltree(deblank(P(i,:))));
+n = size(P,1);
 
-    fprintf('%s\n',xml.QAS.SM.vol_TIV);
+TIV = zeros(n,1);
+spm_progress_bar('Init',n,'Load xml-files','subjects completed')
+for i=1:n
+    xml = convert(xmltree(deblank(P(i,:))));
+    TIV(i) = str2double(xml.QAS.SM.vol_TIV);
     spm_progress_bar('Set',i);  
 end
 spm_progress_bar('Clear');
