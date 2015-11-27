c2550ca9b1d6f818c96d87d7feca98269435b120
Christian Gaser <christian.gaser@uni-jena.de>
2015-11-27 15:18:10 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_defaults.m M       cat_main.m M       cat_surf_createCS.m M       html/cat.html
    Changed: Set debug option and use debug=2 for saving debugging information
    and debug=1 for being more verbose.
    
    
    git-svn-id: svn://141.35.69.214/cat12@795 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version  793  (CAT12)  2015-11-27
+% Version  794  (CAT12)  2015-11-27
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
