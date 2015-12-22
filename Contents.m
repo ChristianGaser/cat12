05d92c6fe67b5a56418efa0938c354fc87688fca
Christian Gaser <christian.gaser@uni-jena.de>
2015-12-22 15:50:35 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_surf_render.m M       html/cat.html
    Added: Options to clip cdata abd to allow slider context menu in 
    cat_surf_render.m
    
    
    git-svn-id: svn://141.35.69.214/cat12@825 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 823 (CAT12) 2015-12-22
+% Version 824 (CAT12) 2015-12-22
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
