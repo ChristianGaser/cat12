57148ff41d4824f81726b0d9956ea3c11e99c55b
Robert Dahnke <robert.dahnke@uni-jena.de>
2016-01-12 11:39:00 +0000

    Changed paths: M       CHANGES.txt M       cat_defaults.m M       cat_main.m M       cat_surf_createCS.m M       cat_surf_display.m M       cat_surf_info.m M       cat_surf_resamp.m M       cat_surf_resample.m M       cat_surf_smooth.m M       cat_vol_pbt.m M       tbx_cfg_cat.m
    Removed:    Removed subject space ROI analysis, because (i) it was to specific 
                for some atlas maps, (ii) the missing systematic evaluation, (iii)
                only small improvement that did not correct major hinds of some 
                atlas maps. 
    Removed:    Deleted other old code segments and functions. 
    Updated:    ROI analysis text.
    Changed:    Changed CSF/WM depth measures (span/width/thickness of CSF/WM), 
                because of problems showed up after the last major corrections.  
    Changed:    Changed handling of background in PBT to avoid surface mapping 
                problems.
    Fixed:      Corrected smaller errors in cat_surf_resamp/resample/smooth.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@829 9f44b166-36c4-483c-8295-4af200601734

--- cat_vol_pbt.m
+++ cat_vol_pbt.m
@@ -43,47 +43,58 @@ function [Ygmt,Ypp] = cat_vol_pbt(Ymf,opt)
   %Ymf  = round(Ymf*20)/20; 
   
    
-  % Estimate WM distance Ywmd and the outer CSF distance Ycsfdc to correct
-  % values in CSF area to limit the Ywmd to the maximum value that is 
-  % possible within the cortex.
+  %% Estimate WM distance Ywmd and the outer CSF distance Ycsfdc to correct
+  %  values in CSF area to limit the Ywmd to the maximum value that is 
+  %  possible within the cortex.
   if opt.verb, fprintf('\n'); end
   stime = cat_io_cmd('    WM distance: ','g5','',opt.verb);
-  YM = max(0,min(1,(Ymf-2))); YM(Ymf<=0)=nan; Ywmd   = cat_vol_eidist(YM,max(0,min(1,Ymf/2)),[1 1 1],1,1,0,opt.debug); 
-  YM = max(0,min(1,(Ymf-1))); YM(Ymf<=0)=nan; Ycsfdc = cat_vol_eidist(YM,max(0,min(1,Ymf/2)),[1 1 1],1,1,0,opt.debug);   
+  YMM = ~(cat_vol_morph(Ymf>0.5,'d',1) | cat_vol_morph(Ymf>0.5,'d',1));
+  YM = max(0,min(1,(Ymf-2))); YM(YMM)=nan; Ywmd   = cat_vol_eidist(YM,max(0,min(1,Ymf/2)),[1 1 1],1,1,0,opt.debug); 
+  YM = max(0,min(1,(Ymf-1))); YM(YMM)=nan; Ycsfdc = cat_vol_eidist(YM,max(0,min(1,Ymf/2)),[1 1 1],1,1,0,opt.debug); 
+  YM = cat_vol_morph(Ymf>2.5,'d'); Ywmd(YM) =  Ywmd(YM)/2;
+  YM = cat_vol_morph(Ymf>1.5,'d'); Ycsfdc(YM) =  Ycsfdc(YM)/2;
   YM = Ymf>0 & Ymf<1.5; Ywmd(YM) = Ywmd(YM) - Ycsfdc(YM); Ywmd(isinf(Ywmd)) = 0; clear Ycsfdc;
   YwmdM = cat_vol_median3(Ywmd,Ywmd>opt.resV & YM,Ywmd>1,0.25); Ywmd(YM) = YwmdM(YM); clear YwmdM YM;
 
   
-  % Estimate CSF distance Ycsfd and the CSF distance Ycsfdc to correct 
-  % values in WM area to limit the Ycsfd to the maximum value that
-  % is possible within the cortex.
+  %% Estimate CSF distance Ycsfd and the CSF distance Ycsfdc to correct 
+  %  values in WM area to limit the Ycsfd to the maximum value that
+  %  is possible within the cortex.
   stime = cat_io_cmd('    CSF distance: ','g5','',opt.verb,stime);
   Ywmm = cat_vol_morph(Ymf>2.5,'e'); % this was dilate???
   YM = max(0,min(1,(2-Ymf))); YM(Ywmm)=nan; Ycsfd = cat_vol_eidist(YM,max(0,min(1,(4-Ymf)/2)),[1 1 1],1,1,0,opt.debug); 
   YM = max(0,min(1,(3-Ymf))); YM(Ywmm)=nan; Ywmdc = cat_vol_eidist(YM,max(0,min(1,(4-Ymf)/2)),[1 1 1],1,1,0,opt.debug);   
+  YM = cat_vol_morph(Ymf<1.5,'d'); Ycsfd(YM) =  Ycsfd(YM)/2;
+  YM = cat_vol_morph(Ymf<2.5,'d'); Ywmdc(YM) =  Ywmdc(YM)/2;
   YM = Ymf>2.5 & ~Ywmm; Ycsfd(YM) = Ycsfd(YM) - Ywmdc(YM); Ycsfd(isinf(-Ycsfd)) = 0; clear Ywmdc;
   YcsfdM = cat_vol_median3(Ycsfd,Ycsfd>opt.resV & YM,Ycsfd>1,0.25); Ycsfd(YM) = YcsfdM(YM); clear YcsfdM YM;
   
   
-  % PBT is the default thickness estimation, but PBT2x is the optimized
-  % version that use both sulci and gyris refinements, because not only 
-  % thin sulci can blurred. PBT2x is furthermore the methode that is
-  % described in the paper, but PBT is faster.
+  %% PBT is the default thickness estimation, but PBT2x is the optimized
+  %  version that use both sulci and gyris refinements, because not only 
+  %  thin sulci can blurred. PBT2x is furthermore the methode that is
+  %  described in the paper, but PBT is faster.
   if strcmp(opt.method,'pbt2x')  
     % Estimation of the cortical thickness with sulcus (Ygmt1) and gyri 
     % correction (Ygmt2) to create the final thickness as the minimum map
     % of both.
     stime = cat_io_cmd('    PBT2x thickness: ','g5','',opt.verb,stime);
-    Ygmt1 = cat_vol_pbtp(Ymf,Ywmd,Ycsfd);   Ygmt1 = cat_vol_median3(Ygmt1,Ygmt1>0,Ygmt1>0,opt.resV/4);
-    Ygmt2 = cat_vol_pbtp(4-Ymf,Ycsfd,Ywmd); Ygmt2 = cat_vol_median3(Ygmt2,Ygmt2>0,Ygmt2>0,opt.resV/4);
+    %Ywmd  = cat_vol_median3(Ywmd,Ywmd>0,Ywmd>0);
+    %Ycsfd = cat_vol_median3(Ycsfd,Ycsfd>0,Ycsfd>0);
+    Ygmt1 = cat_vol_pbtp(Ymf,Ywmd,Ycsfd);   Ygmt1 = cat_vol_median3(Ygmt1,Ygmt1>0,Ygmt1>0,opt.resV/4); %,opt.resV/4
+    Ygmt2 = cat_vol_pbtp(4-Ymf,Ycsfd,Ywmd); Ygmt2 = cat_vol_median3(Ygmt2,Ygmt2>0,Ygmt2>0,opt.resV/4); %,opt.resV/4
+    Ygmt1(Ygmt1<=0.5 & Ygmt2>0) = Ygmt2(Ygmt1<=0.5 & Ygmt2>0);
+    Ygmt2(Ygmt2<=0.5 & Ygmt1>0) = Ygmt1(Ygmt2<=0.5 & Ygmt1>0);
     [Ygmt,Yi] = min(cat(4,Ygmt1,Ygmt2+0.25*mean(opt.resV)),[],4); 
-        
+    Ygmt  = cat_vol_median3(Ygmt,Ygmt>eps,Ygmt>eps);    
+    %Yi = round(cat_vol_smooth3X(Yi,2)); 
     
-    % Estimation of a mixed percentual possion map Ypp.
+    %% Estimation of a mixed percentual possion map Ypp.
     Ypp=zeros(size(Ymf),'single');
-    YM=Ymf>=1.5 & Ymf<=2.0; Ypp(YM) =   (Ygmt1(YM) - Ywmd(YM)) ./ (Ygmt1(YM) + eps); 
-    YM=Ymf> 2.0 & Ymf<2.5;  Ypp(YM) = ( (Ygmt1(YM) - Ywmd(YM)).*(Yi(YM)==1) + Ycsfd(YM).*(Yi(YM)==2) ) ./ (Ygmt(YM) + eps); 
-    Ypp(Ymf>=2.5 | (Ymf>2 & Ygmt<=1))=1;
+    YM=Ymf>1.5 & Ymf<=2;  Ypp(YM) =   (Ygmt1(YM) - Ywmd(YM)) ./ (Ygmt1(YM) + eps); 
+    YM=Ymf>2.0 & Ymf<2.5; Ypp(YM) = ( (Ygmt1(YM) - Ywmd(YM)).*(Yi(YM)==1) + Ycsfd(YM).*(Yi(YM)==2) ) ./ (Ygmt(YM) + eps); 
+    Ypp(Ymf>2.5 | (Ymf>2 & Ygmt<=1))=1;
+    Ypp = cat_vol_median3(Ypp,Ymf>0,Ymf>0);
   else
     % Estimation of thickness map Ygmt and percentual position map Ypp.
     stime = cat_io_cmd('    PBT2 thickness: ','g5','',opt.verb,stime);
@@ -96,7 +107,7 @@ function [Ygmt,Ypp] = cat_vol_pbt(Ymf,opt)
   %   ds('l2','',1,Ymf/3,Ypp>0.5,Ypp,Ygmt/opt.resV,250)
   stime = cat_io_cmd('    Final Corrections: ','g5','',opt.verb,stime);
   Ypp(isnan(Ypp)) = 0; 
-  Ypp = cat_vol_median3(Ypp,Ymf>0 & Ymf<3,Ymf>0 & Ymf<3,2);
+  Ypp = cat_vol_median3(Ypp,Ymf>0 & Ymf<3,Ymf>0 & Ymf<3,0.3);
   YM  = Ypp>=0.5 & ~cat_vol_morph(Ypp>=0.5,'labopen',1);  
   Ypp(YM) = 0.5-eps; clear YM
   
--- tbx_cfg_cat.m
+++ tbx_cfg_cat.m
@@ -75,38 +75,52 @@ surface.help   = {
 
 %------------------------------------------------------------------------
 % Parameter to choose between different ways to extract data by ROIs.
-% Inactive due to missing evaluation of the subject space ROI analysis. 
-% RD 20150924
+% Inactive due to missing implementation and evaluation of the optimized 
+% space ROI analysis. 
+% RD 20160112
 %------------------------------------------------------------------------
 ROI        = cfg_menu;
 ROI.tag    = 'ROI';
 ROI.name   = 'ROI analyis';
-ROI.labels = {'No ROI analyis','Subject space ROI analysis','Template space ROI analyis','Both'};
-ROI.values = {0 1 2 3};
+ROI.labels = {'No ROI analyis','ROI analysis'}; %,'optimzed ROI analysis'};
+ROI.values = {0 1 2};
 ROI.def    = @(val)cat_get_defaults('output.ROI', val{:});
 ROI.help   = {
-  'Export of ROI data of volume, intensity, and thickness to csv-files. The values of a ROI can be estimated in subject and/or normalized spaced. '
+  'Export of ROI data of volume, intensity, and thickness to a xml and csv-files. '
+  'For further information see atlas specific text files in "templates_1.50mm" CAT12 subdir. '
   ''
-  'For thickness estimation the projection-based thickness (PBT) [Dahnke:2012] is used that estimates cortical thickness for each GM voxel. '
+  'For thickness estimation the projection-based thickness (PBT) [Dahnke:2012] is used that average cortical thickness for each GM voxel. '
   ''
   'There are different atlas maps available: '
-  '(1) Anatomy Toolbox Maps (Version 2.0, 2014-07-23):' 
+  '(1) Hammers (68 CSF/GM/[WM] ROIs of 20 subjects, 2003):'
+  '    Alexander Hammers brain atlas from the Euripides project (www.brain-development.org).'
+  '    Hammers et al. Three-dimensional maximum probability atlas of the human brain, with particular reference to the temporal lobe. Hum Brain Mapp 2003, 19: 224-247.'
   ''
-  '    Eickhoff SB, Stephan KE, Mohlberg H, Grefkes C, Fink GR, Amunts K, Zilles K. A new SPM toolbox for combining probabilistic cytoarchitectonic maps and functional imaging data. NeuroImage 25(4), 1325-1335, 2005'
-  ''
-  '(2) Hammers:'
-  '    Alexander Hammers brain atlas from the Euripides project: '
-  '    www.brain-development.org'
-  ''
-  '    Hammers A, Allom R, Koepp MJ, Free SL, Myers R, Lemieux L, Mitchell TN, Brooks DJ, Duncan JS. Three-dimensional maximum probability atlas of the human brain, with particular reference to the temporal lobe. Hum Brain Mapp 2003, 19: 224-247.'
-  ''
-  '(3) Neuromorphometrics:'
+  '(2) Neuromorphometrics (142 GM ROIs of 15 subjects, 2012):'
   '    Maximum probability tissue labels derived from the MICCAI 2012 Grand Challenge and Workshop on Multi-Atlas Labeling'
   '    https://masi.vuse.vanderbilt.edu/workshop2012/index.php/Challenge_Details'
-''
+  ''
+  '(3) LPBA40 (182 GM ROIs of 40 subjects, 2008):'
+  '    The LONI Probabilistic Brain Atlas (LPBA40) is a series of maps of brain anatomic regions. These maps were produced from a set of whole-head MRI of 40 human volunteers. Each MRI was manually delineated to identify a set of 56 structures in the brain, most of which are within the cortex. These delineations were then transformed into a common atlas space to produce a set of coregistered anatomical labels. The original MRI data were also transformed into the atlas space. '
+  '    Shattuck et al. 2008. Construction of a 3D Probabilistic Atlas of Human Cortical Structures, NeuroImage 39 (3): 1064-1070. DOI:	10.1016/j.neuroimage.2007.09.031'
+  ''
+  '(4) IBSR (32 CSF/GM ROIs of 18 subjects, 2004):'
+  '    See IBSR terms "http://www.nitrc.org/projects/ibsr"'
+  ''
+  '(5) AAL (122 GM ROIs of 1 subject, 2002):'
+  '    Combining probabilistic cytoarchitectonic maps and functional imaging data of a single Brain.'
+  '    Tzourio-Mazoyer et al., Automated anatomical labelling of activations in spm using a macroscopic anatomical parcellation of the MNI MRI single subject brain. Neuroimage 2002, 15: 273-289.'
+  '    http://www.fz-juelich.de/inm/inm-1/DE/Forschung/_docs/SPMAnatomyToolbox/SPMAnatomyToolbox_node.html'
+  ''
+  '(6) MORI (128 GM/WM ROIs of 1 subject, 2009):'
+  '   Oishi et al. Atlas-based whole brain white matter analysis using large deformation diffeomorphic metric mapping: application to normal elderly and Alzheimer''s disease participants. 2009'
+  ... ''
+  ... '() Anatomy ():
+  ... '() COBRA ():'
+  ''
+  ''
 };
-
-
+ 
 native        = cfg_menu;
 native.tag    = 'native';
 native.name   = 'Native space';
