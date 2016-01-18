c01930a41f98dcb852388eff378a94953861b4b8
Christian Gaser <christian.gaser@uni-jena.de>
2016-01-18 13:26:38 +0000

    Changed paths: M       CHANGES.txt M       cat_plot_circular.m M       cat_stat_TIV.m M       cat_surf_render.m
    (1) Fixed: Issue with missing "ned" in cat_surf_render.m.
    (2) Fixed: Wrong order of tissues in cat_stat_TIV.m (thanks to Nikolaos
        Koutsouleris for pointing to that issues)
    (3) Changed: Added some more functionality to cat_plot_circular.m
    
    
    git-svn-id: svn://141.35.69.214/cat12@834 9f44b166-36c4-483c-8295-4af200601734

--- cat_stat_TIV.m
+++ cat_stat_TIV.m
@@ -6,7 +6,7 @@ function cat_stat_TIV(p)
 % $Id$
 
 if ~p.calcvol_TIV
-  fprintf('%35s\t%5s\t%5s\t%5s\t%5s\t%5s\n','Name','Total','GM','WM','CSF','WMH');
+  fprintf('%50s\t%5s\t%5s\t%5s\t%5s\t%5s\n','Name','Total','GM','WM','CSF','WMH');
 end
 fid = fopen(p.calcvol_name,'w');
 
@@ -19,16 +19,15 @@ for i=1:length(p.data_xml)
     xml = convert(xmltree(deblank(p.data_xml{i})));
     tmp = eval(xml.subjectmeasures.vol_abs_CGW);
 
-    [pth,nam]     = spm_fileparts(p.data_xml{i});
+    name = spm_str_manip(xml.filedata.fname,'a50');
+
     % only save TIV
     if p.calcvol_TIV
         fprintf(fid,'%3.2f\n',sum(tmp));
-        fprintf('%35s\t%5.2f\n',nam(5:end),sum(tmp));
+        fprintf('%50s\t%5.2f\n',name,sum(tmp));
     else % also save GM/WM/CSF 
-        fprintf(fid,'%3.2f\t%3.2f\t%3.2f\t%3.2f\t%3.2f\n',sum(tmp),tmp(3),tmp(1),...
-			tmp(2),tmp(4));
-        fprintf('%35s\t%5.2f\t%5.2f\t%5.2f\t%5.2f\t%5.2f\n',nam(5:end),sum(tmp),tmp(3),tmp(1),...
-			tmp(2),tmp(4));
+        fprintf(fid,'%3.2f\t%3.2f\t%3.2f\t%3.2f\t%3.2f\n',sum(tmp),tmp(2),tmp(3),tmp(1),tmp(4));
+        fprintf('%50s\t%5.2f\t%5.2f\t%5.2f\t%5.2f\t%5.2f\n',name,sum(tmp),tmp(2),tmp(3),tmp(1),tmp(4));
     end
     spm_progress_bar('Set',i);  
 end
