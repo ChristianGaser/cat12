613b2b8f1da25e855087a701407caa0c89399b26
Christian Gaser <christian.gaser@uni-jena.de>
2016-01-18 21:52:48 +0000

    Changed paths: M       CHANGES.txt M       Contents.m MM      cat_plot_circular.m A       cat_stat_cva_ui.m M       html/cat.html
    New: Function for CVA.
    
    
    git-svn-id: svn://141.35.69.214/cat12@835 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 830 (CAT12) 2016-01-14
+% Version 833 (CAT12) 2016-01-17
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
