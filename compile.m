4c1b92aa9786dadf3bdea054f186ba5ee151a60b
Robert Dahnke <robert.dahnke@uni-jena.de>
2016-02-08 15:54:14 +0000

    Changed paths: M       CHANGES.txt M       cat12.fig M       cat_main.m M       cat_run_job.m M       cat_surf_surf2roi.m M       compile.m
    Added:      Added functions test in for cat_vol_simgrow.c and cat_vol_downcut.c
                in compile.m function.
    Update:     Modified histogram limitation in APP==0 (default) processing.
                Try to correct errors in background estimation for masked data 
                ("Only nonempty 3D volumes!" error in cat_main line ~468).
    Added:      Added Help call in GUI>Internet/Help that (re)open CAT Help in
                SPM Graphics window.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@855 9f44b166-36c4-483c-8295-4af200601734

--- compile.m
+++ compile.m
@@ -42,7 +42,8 @@ function varargout = compile(comp,test,verb)
   dc  = zeros(10,10,10,'single'); for si=3:8; dc(si,:,:)=si-2.5; end; dc(5,5,5) = NaN; % csf distance
   dw  = zeros(10,10,10,'single'); for si=3:8; dw(si,:,:)=8.5-si; end; dw(5,5,5) = NaN; % wm distance
   dcube = zeros(10,10,10,'single'); dcube(3:end-2,3:end-2,3:end-2) = 1; d1(5,5,5) = NaN;
-  
+  r = []; 
+  s = []; 
   
   %% compiling c-functions
   if comp==1
@@ -54,7 +55,7 @@ function varargout = compile(comp,test,verb)
       mexflag='';
     end
 
-    % main c-functions
+    %% main c-functions
     eval(['mex ' mexflag ' -O cat_amap.c Kmeans.c Amap.c MrfPrior.c Pve.c vollib.c'])
     eval(['mex ' mexflag ' -O cat_vol_median3.c'])
     eval(['mex ' mexflag ' -O cat_vol_median3c.c'])
@@ -92,8 +93,6 @@ function varargout = compile(comp,test,verb)
   
   
   %% test c-functions
-  r = []; 
-  s = []; 
   if test==1
   
     ntests = 15;
@@ -106,9 +105,9 @@ function varargout = compile(comp,test,verb)
     %nstd = @(x) cat_stat_nanstd( x(:) );
     
     
-    % Median 
-    % Test the noise reduction by the median filter in the segment image.  
-    %   ds('l2','',1,d0,d1/2,d1/2 + d0/10,d{1},5)
+    %% Median 
+    %  Test the noise reduction by the median filter in the segment image.  
+    %    ds('l2','',1,d0,d1/2,d1/2 + d0/10,d{1},5)
     n{1} = 'cat_vol_median3';   
     d{1} = cat_vol_median3(d1/2 + d0/10);         
     r(1) = rms(d{1} - d1/2);
@@ -120,9 +119,9 @@ function varargout = compile(comp,test,verb)
     s(2) = rms(round(d1 + (d0-0.5)*1.2) - d1) > r(2);
     
     
-    % NLM
-    % Test reminding noise;
-    %   ds('l2','',1,d0,d1/2,d1/2 + d0/10,d{4},10)
+    %% NLM
+    %  Test reminding noise;
+    %    ds('l2','',1,d0,d1/2,d1/2 + d0/10,d{4},10)
     n{3} = 'cat_ornlm';         
     d{3} = cat_ornlm(d1/2 + d0/10,3,1,0.05);
     r(3) = rms(d{3}(:) - d1(:)/2); 
@@ -135,9 +134,9 @@ function varargout = compile(comp,test,verb)
     s(4) = r(4)<0.1;
     
 
-    % Laplace
-    % Laplace filtering is similar to distance transformation.
-    %   ds('d2','',1,d0,d1/2,dc/6 + 0.5/6,d{5}(d1==1) - dc(d1==1)/6 + 0.5/6,5)
+    %% Laplace
+    %  Laplace filtering is similar to distance transformation.
+    %    ds('d2','',1,d0,d1/2,dc/6 + 0.5/6,d{5}(d1==1) - dc(d1==1)/6 + 0.5/6,5)
     n{5} = 'cat_vol_laplace3';  
     d{5} = cat_vol_laplace3(d1/2,0,1,0.01); 
     r(5) = rms(d{5}(d1==1) - (dc(d1==1)+0.5)/7.5 );   
@@ -147,9 +146,10 @@ function varargout = compile(comp,test,verb)
     r(6) = rms(d{6}(d1==1) - (dc(d1==1)+0.5)/7.5 );   
     s(6) = r(6)<0.05;
 
-    % gradient 
-    % compare the result to the matlab function ... 
+    %% gradient 
+    %  compare the result to the matlab function ... 
     %##### HIER GIBTS UNTERSCHIEDE?! ... beim zufälligen bild???
+    %    ds('l2','',1,dg,(abs(dx)+abs(dy)+abs(dz))*3,dg,d{7},10)
     n{7} = 'cat_vol_gradient3'; 
     dg   = d1/2+d0/10; 
     [y,x,z] = gradient(dg);
@@ -159,24 +159,27 @@ function varargout = compile(comp,test,verb)
     s(7) = r(7)<0.1;
    
     
-    % voxelbased distance / thickness
+    %% voxelbased distance / thickness
+    %    ds('l2','',1,d1,d1,d1/2,d{8}/10,10)
     n{8} = 'cat_vbdist';
     d{8} = cat_vbdist(single(d1==0),d1==1);
     r(8) = max(d{8}(d1(:)==1)) - 6; % grid distance 
     s(8) = r(8)>=0 & r(8)<0.5;  
-    % eikonal distance
+    %  eikonal distance
+    %    ds('l2','',1,d1,d1,d1/2,d{9}/10,10)
     n{9} = 'cat_vol_eidist';   
     d{9} = cat_vol_eidist(single(d1==0),ones(size(d1),'single'));
     r(9) = max(d{9}(d1(:)==1)) - 5.5; % distance to boundary 
     s(9) = r(9)>=0 & r(9)<0.5;  
-    % projection-based thickness
+    %  projection-based thickness ############## 
+    %    ds('l2','',1,d1,d1,(d1+1)/3,d{10}/10,10)
     n{10} = 'cat_vol_pbtp';      
     [d{10},dpp] = cat_vol_pbtp(d1+1,dw,dc);  %#ok<NASGU>
     r(10) = rms(d{10}(d1==1)) - 5.5; % warum nicht 6?
     s(10) = r(10)<0.05;
 
     
-    % interpolation
+    %% interpolation
     n{11} = 'cat_vol_interp3f'; sD = size(d0);        
     [Rx,Ry,Rz] = meshgrid(single(1.75:0.5:sD(2)),single(1.75:0.5:sD(1)),single(1.75:0.5:sD(3)));
     d0nan = d0+0; d0nan(isnan(d0)) = 0; 
@@ -189,25 +192,31 @@ function varargout = compile(comp,test,verb)
     r(11) = rms(d{11}{1}) + rms(d{11}{2}) ; 
     s(11) = rms(d{11}{1})<10^-6 & rms(d{11}{2})<0.04; 
     
-    % local values
+    %% local values
+    %    ds('l2','',1,d0,d0,d0,d{12},10)
     n{12} = 'cat_vol_localstat';      
     d{12} = cat_vol_localstat(d0,d1==1,8,1);
     r(12) = rms(cat_stat_nanmean(d0(d1(:)==0)) - d{12}(d1(:)==1));
     s(12) = r(12)<0.05;
     
-    % region growing
+    %% region growing
+    %    ds('l2','',1,d1 + (d0-0.5)/10,dsg,d1,(d1 + (d0-0.5)/10)/2,d{13}{2}*5,10)
     n{13} = 'cat_vol_simgrow'; 
-    d{13} = cat_vol_simgrow(d0,d0,0.01);    
+    dsg   = single(cat_vol_morph(d1>1,'d')); dsg(d1==0) = nan; 
+    [d{13}{1},d{13}{2}] = cat_vol_simgrow(dsg,d1 + (d0-0.5)/10,0.05);    
     % no test yet
-    s(13) = 1; 
+    r(13) = rms((d1>0.5) - d{13}{1});
+    s(13) = r(12)<0.05;
      
-    % downcut
+    %% downcut
+    %    ds('l2','',1,d1 + (d0-0.5)/10,dsg,(d1 + (d0-0.5)/10)/2,d{14}{2}/200,10)
     n{14} = 'cat_vol_downcut';
-    d{14} = cat_vol_downcut(d0,d0.^1.5,1);  
-    % no test yet
+    dsg   = single(cat_vol_morph(d1>1,'d')); dsg(d1==0) = nan; 
+    [d{14}{1},d{14}{2}] = cat_vol_downcut(dsg,d1 + (d0-0.5)/10,0.1); 
+    r(14) = rms((d1>0.5) - d{14}{1});
     s(14) = 1;
     
-    % surface genersation
+    %% surface genersation
     % Compare a simple cube - vertices should be identical, faces not. 
     n{15} = 'cat_vol_genus0';   
     MS    = isosurface(dcube,0.5);
@@ -227,7 +236,7 @@ function varargout = compile(comp,test,verb)
       subplot(2,2,4), patch(MSs,'facecolor',[.8 .7 .6]); axis equal off; lighting phong; view(3); camlight; zoom(1.8)
     end
     
-    % NLM upsampling 
+    %% NLM upsampling 
     % c-fuction used iterative based on an interpolated image
     try
       n{16} = 'cat_vol_cMRegularizarNLM3D';   
