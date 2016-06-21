1e8fc617ca51d61d928030e2eb91dfc03ebc3e93
Christian Gaser <christian.gaser@uni-jena.de>
2016-06-21 15:22:38 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_check_system_output.m
    Fixed: Error in cat_check_system_output.m due to special characters 
           under windows.
    
    
    git-svn-id: svn://141.35.69.214/cat12@958 9f44b166-36c4-483c-8295-4af200601734

--- cat_check_system_output.m
+++ cat_check_system_output.m
@@ -15,11 +15,14 @@ function varargout = cat_check_system_output(status,result,debugON,trerr)
   if ~exist('debugON','var'), debugON=0; end
   if ~exist('trerr','var'), trerr=1; end
   if nargout>0, varargout{1} = false; end 
-
-  if status==1 || ...
+  
+  % replace special characters
+  result = genstrarray(result);
+  
+  if status || ...
      ~isempty(strfind(result,'ERROR')) || ...
      ~isempty(strfind(result,'Segmentation fault'))
-    if nargout>0, varargout{1} = true; end
+   if nargout>0, varargout{1} = true; end
     if trerr
       error('CAT:system_error',result); 
     else
@@ -28,5 +31,25 @@ function varargout = cat_check_system_output(status,result,debugON,trerr)
   end
   if nargin > 2
     if debugON, disp(result); end
+  end
+end
+
+function str = genstrarray(stritem)
+% generate a string of properly quoted strings 
+
+  str = strrep(stritem, '''', '''''');
+  if ~any(str  == char(0)) &&  ~any(str  == char(9)) && ~any(str  == char(10))
+    str  = sprintf('''%s''', str );
+  else
+    % first, quote sprintf special chars % and \
+    % second, replace special characters by sprintf equivalents
+    replacements = {'%', '%%'; ...
+        '\', '\\'; ...
+        char(0), '\0'; ...
+        char(9), '\t'; ...
+        char(10), '\n'};
+    for cr = 1:size(replacements, 1)
+        str  = strrep(str , replacements{cr,:});
+    end
   end
 end
