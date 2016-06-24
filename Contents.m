fb0c5efff6a481096d18fb75fd444b92ae78c824
Christian Gaser <christian.gaser@uni-jena.de>
2016-06-24 09:30:22 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_surf_render.m
    Changed: Removed filename in windows in cat_surf_render.m 
    
    
    git-svn-id: svn://141.35.69.214/cat12@961 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 958 (CAT12) 2016-06-21
+% Version 959 (CAT12) 2016-06-23
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
