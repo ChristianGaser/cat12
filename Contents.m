8baf87b844e788f6a4eddb891023055d0455e881
Christian Gaser <christian.gaser@uni-jena.de>
2016-07-22 10:56:01 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_surf_render.m M       html/cat.html
    Fixed: cat_surf_render did not work properly for all matlab version, now use
    old version again.
    
    
    git-svn-id: svn://141.35.69.214/cat12@970 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 966 (CAT12) 2016-07-20
+% Version 967 (CAT12) 2016-07-21
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
