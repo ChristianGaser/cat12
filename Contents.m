8ac52829be9dfae09d034b0ece5a8a67dad37150
Christian Gaser <christian.gaser@uni-jena.de>
2016-08-08 14:21:16 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_long_main.m M       cat_surf_resamp.m M       html/cat.html M       templates_1.50mm/hammers.csv M       templates_1.50mm/labels_dartel_hammers.xml M       templates_1.50mm/labels_dartel_mori.xml M       templates_1.50mm/labels_dartel_neuromorphometrics.xml M       templates_1.50mm/mori.csv M       templates_1.50mm/neuromorphometrics.csv
    (1) Changed: Resampled surfaces have less artefacts due to resampling.
    (2) Fixed: Corrected some anatomical names in atlases.
    
    
    git-svn-id: svn://141.35.69.214/cat12@979 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 973 (CAT12) 2016-07-25
+% Version 978 (CAT12) 2016-08-08
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
