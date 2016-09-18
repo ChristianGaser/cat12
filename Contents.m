91d06f7efb5554f2deaa8da1f7a5a2b4016996c1
Christian Gaser <christian.gaser@uni-jena.de>
2016-09-18 15:41:37 +0000

    Changed paths: M       CAT12-Manual.doc M       CHANGES.txt M       Contents.m M       html/cat.html M       html/cat_orthogonality.html M       html/cat_starting.html
    Changed: Updated quick starting guide wrt. to new ROI analysis.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1009 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1004 (CAT12) 2016-09-14
+% Version 1008 (CAT12) 2016-09-19
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
