99065329f0742b16246cf70e470422c433795480
Christian Gaser <christian.gaser@uni-jena.de>
2016-10-20 14:33:54 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_long_main.m M       cat_run.m M       cat_stat_check_cov.m M       cat_surf_createCS.m M       cat_surf_results.m M       html/cat.html
    (1) Fixed: Dependencies in cat_run.m were incorrect and ignored multiple
        files. 
    (2) Changed: Removed unnecessary options in cat_long_main.m.
    (3) Changed: Extended cat_stat_check_conv.m for 4D data.
    (4) Fixed: Threshold function in cat_surf_results was not working with 
        multiple files per hemisphere.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1040 9f44b166-36c4-483c-8295-4af200601734

--- cat_long_main.m
+++ cat_long_main.m
@@ -36,31 +36,24 @@ end
 
 matlabbatch{1}.spm.tools.cat.tools.series.bparam = 1000000;
 matlabbatch{2}.spm.tools.cat.estwrite.data(1) = cfg_dep('Longitudinal Rigid Registration: Midpoint Average', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','avg', '()',{':'}));
+matlabbatch{2}.spm.tools.cat.estwrite.nproc = 0;
 matlabbatch{2}.spm.tools.cat.estwrite.output.surface = 0;
-matlabbatch{2}.spm.tools.cat.estwrite.output.GM.native = 0;
 matlabbatch{2}.spm.tools.cat.estwrite.output.GM.mod = 0;
-matlabbatch{2}.spm.tools.cat.estwrite.output.WM.native = 0;
 matlabbatch{2}.spm.tools.cat.estwrite.output.WM.mod = 0;
 matlabbatch{2}.spm.tools.cat.estwrite.output.bias.warped = 1;
-matlabbatch{2}.spm.tools.cat.estwrite.output.jacobian.warped = 0;
 matlabbatch{2}.spm.tools.cat.estwrite.output.warps = [1 0];
 matlabbatch{3}.spm.tools.cat.estwrite.data(1) = cfg_dep('Longitudinal Rigid Registration: Realigned images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','rimg', '()',{':'}));
+matlabbatch{3}.spm.tools.cat.estwrite.nproc = 0;
 matlabbatch{3}.spm.tools.cat.estwrite.output.GM.native = 1;
 matlabbatch{3}.spm.tools.cat.estwrite.output.GM.mod = 0;
-matlabbatch{3}.spm.tools.cat.estwrite.output.GM.dartel = 0;
 matlabbatch{3}.spm.tools.cat.estwrite.output.WM.native = 1;
 matlabbatch{3}.spm.tools.cat.estwrite.output.WM.mod = 0;
-matlabbatch{3}.spm.tools.cat.estwrite.output.WM.dartel = 0;
 matlabbatch{3}.spm.tools.cat.estwrite.output.bias.warped = 0;
-matlabbatch{3}.spm.tools.cat.estwrite.output.jacobian.warped = 0;
-matlabbatch{3}.spm.tools.cat.estwrite.output.warps = [0 0];
 matlabbatch{4}.spm.tools.cat.tools.defs.field1(1) = cfg_dep('CAT12: Segmentation: Deformation Field', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','fordef', '()',{':'}));
 matlabbatch{4}.spm.tools.cat.tools.defs.images(1) = cfg_dep('CAT12: Segmentation: p1 Images', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','p', '()',{':'}));
 matlabbatch{4}.spm.tools.cat.tools.defs.images(2) = cfg_dep('CAT12: Segmentation: p2 Images', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','p', '()',{':'}));
 matlabbatch{4}.spm.tools.cat.tools.defs.interp = 4;
 matlabbatch{5}.cfg_basicio.file_dir.file_ops.file_move.files(1) = cfg_dep('CAT12: Segmentation: Deformation Field', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','fordef', '()',{':'}));
-%if ~exist('dartel','var')
-  matlabbatch{5}.cfg_basicio.file_dir.file_ops.file_move.files(2) = cfg_dep('CAT12: Segmentation: p1 Images', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','p', '()',{':'}));
-  matlabbatch{5}.cfg_basicio.file_dir.file_ops.file_move.files(3) = cfg_dep('CAT12: Segmentation: p2 Images', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','p', '()',{':'}));
-%end
+matlabbatch{5}.cfg_basicio.file_dir.file_ops.file_move.files(2) = cfg_dep('CAT12: Segmentation: p1 Images', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','p', '()',{':'}));
+matlabbatch{5}.cfg_basicio.file_dir.file_ops.file_move.files(3) = cfg_dep('CAT12: Segmentation: p2 Images', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','p', '()',{':'}));
 matlabbatch{5}.cfg_basicio.file_dir.file_ops.file_move.action.delete = false;
