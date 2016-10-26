dcd544682c915dc4c822de13b372298c61a82ab4
Christian Gaser <christian.gaser@uni-jena.de>
2016-10-26 09:01:27 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat12.fig M       cat_conf_tools.m M       cat_stat_spm2x.m M       cat_surf_results.m M       html/cat.html
    Changed: Updated use of atlas labeling in cat_stat_spm2x.m and now 
    differentiates between conversion of images and surfaces in cat12.m
    
    
    git-svn-id: svn://141.35.69.214/cat12@1051 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1046 (CAT12) 2016-10-25
+% Version 1049 (CAT12) 2016-10-25
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
