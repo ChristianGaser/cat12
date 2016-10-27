afdb66ffc1a6735cfd5eb5216c50e022ed252572
Christian Gaser <christian.gaser@uni-jena.de>
2016-10-27 22:15:12 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       html/cat.html
    Changed: Updated html code and corrected typos and language issues.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1058 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1056 (CAT12) 2016-10-27
+% Version 1057 (CAT12) 2016-10-28
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
