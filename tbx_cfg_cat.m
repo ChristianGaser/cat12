2fddb440ef5f4069c0531da1371dc11f3b3bd434
Christian Gaser <christian.gaser@uni-jena.de>
2016-10-27 19:49:16 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_long_main.m M       html/cat.html M       tbx_cfg_cat.m
    (1) Fixed: Changed interpolation for applying deformations in long. batch to 
        trilinear interpolation because b-spline interpolation resulted in neg. 
        values for writing GM/WM maps.
    (2) Changed: ROI option is now also available in standard mode because of 
        interferences with long. batch mode.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1055 9f44b166-36c4-483c-8295-4af200601734

--- tbx_cfg_cat.m
+++ tbx_cfg_cat.m
@@ -365,7 +365,7 @@ if expert==2
 elseif expert==1
   output.val  = {ROI surface grey white csf wmh label bias las jacobian warps};
 else
-  output.val  = {surface grey white bias jacobian warps};
+  output.val  = {ROI surface grey white bias jacobian warps};
 end
 output.help = {
 'There are a number of options about what data you would like the routine to produce. The routine can be used for producing images of tissue classes, as well as bias corrected images. The native space option will produce a tissue class image (p*) that is in alignment with the original image. You can also produce spatially normalised versions - both with (m[0]wp*) and without (wp*) modulation. In the cat toolbox, the voxel size of the spatially normalised versions is 1.5 x 1.5 x 1.5mm as default. The produced images of the tissue classes can directly be used for doing voxel-based morphometry (both un-modulated and modulated). All you need to do is smooth them and do the stats (which means no more questions on the mailing list about how to do "optimized VBM").'
