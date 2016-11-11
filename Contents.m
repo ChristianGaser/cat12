624488b2d87ddbc6c6c19b3da0aa7aad32a1dd37
Christian Gaser <christian.gaser@uni-jena.de>
2016-11-11 11:21:54 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_conf_stools.m M       cat_conf_tools.m M       cat_surf_createCS.m M       html/cat.html M       html/cat_starting.html
    Changed: Updated help text and quick starting guide.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1074 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1070 (CAT12) 2016-11-08
+% Version 1073 (CAT12) 2016-11-10
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
