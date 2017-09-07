a00888fc690beb301bb4ab74afe504ff404025a3
Christian Gaser <christian.gaser@uni-jena.de>
2017-09-07 13:00:45 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_io_report.m M       cat_run_newcatch.m M       cat_surf_createCS.m M       compile.m M       html/cat.html
    (1) Fixed: Updated error output that was not correctly working for windows.
    (2) Changed: Added warning for compiling under Mac with Matlab version < 9.2.
    (3) Changed: Easier check for cerebellar preprocessing in cat_surf_createCS.m
    
    
    git-svn-id: svn://141.35.69.214/cat12@1176 9f44b166-36c4-483c-8295-4af200601734

--- compile.m
+++ compile.m
@@ -30,6 +30,10 @@ function varargout = compile(comp,test,verb)
   fprintf('Matlab >= R2017a will be not successful!\n');
   fprintf('-----------------------------------------------------------------------------------\n\n');
   
+  if strcmp(mexext,'mexmaci64') && verLessThan('matlab','9.2')
+    warning('WARNING: Matlab version should be at least R2017a for compilation under Mac.');
+  end
+
   try % failed in older MATLABs
     rng('default'); rng(13); % fix random numbers
   end
