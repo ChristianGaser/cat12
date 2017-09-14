c0f280d52d5642b5cd615bad37716e16b4f305b9
Christian Gaser <christian.gaser@uni-jena.de>
2017-09-14 14:58:48 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_batch_cat.m M       cat_batch_long.m M       cat_defaults.m M       cat_system.m M       html/cat.html
    (1) Fixed: Batch mode is now correctly using defined default files.
    (2) Changed: Updated comments in cat_defaults.m
    
    
    git-svn-id: svn://141.35.69.214/cat12@1186 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1184 (CAT12) 2017-09-08
+% Version 1185 (CAT12) 2017-09-13
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
