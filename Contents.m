89e85972a5252019a6bbfd52fc60dbedd0308659
Christian Gaser <christian.gaser@uni-jena.de>
2018-01-19 16:03:11 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_roi_fun.m M       cat_surf_createCS.m M       html/cat.html
    (1) Changed: Estimation of mean ROI values now skips some of unnecessary values.
    (2) Changed: Added final correction of central surface in highly folded areas 
        with high mean curvature.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1257 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1254 (CAT12.1) 2018-01-10
+% Version 1255 (CAT12.1) 2018-01-12
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
