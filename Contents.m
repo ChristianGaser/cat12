69c8d1adbdc77f55b9f95e37d858ab6ea3f09a3d
Christian Gaser <christian.gaser@uni-jena.de>
2018-02-02 14:47:02 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_stat_check_cov.m M       html/cat.html
    Fixed: Surface maps for Mahalanobis plot were shifted.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1267 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1265 (CAT12.1) 2018-02-01
+% Version 1266 (CAT12.1) 2018-02-01
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
