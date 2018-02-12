1cb29aa5ba711e84d7cad4fb41720caa8ad8b202
Christian Gaser <christian.gaser@uni-jena.de>
2018-02-12 08:36:30 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_main.m M       html/cat.html
    Fixed: cat_surf_main.m estimated completely wrong TIV and tissue volumes.
    
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1274 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1271 (CAT12.1) 2018-02-08
+% Version 1273 (CAT12.1) 2018-02-09
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
