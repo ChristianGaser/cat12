696bdded1769a72df1fa60d7557b9f8e9984a88e
Christian Gaser <christian.gaser@uni-jena.de>
2018-02-13 09:56:53 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       html/cat.html M       html/cat.txt M       html/style.css
    Changed: Added button to open external "Quick Start Guide" to CAT12 winodw.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1276 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1273 (CAT12.1) 2018-02-09
+% Version 1274 (CAT12.1) 2018-02-12
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
