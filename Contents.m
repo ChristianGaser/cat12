3d85a528a0bffed34c4b765a15610a83379890d5
Christian Gaser <christian.gaser@uni-jena.de>
2018-03-05 15:17:42 +0000

    Changed paths: M       CAT12-Manual.docx M       CAT12-Manual.pdf M       CHANGES.txt M       Contents.m M       cat_batch_cat.sh M       cat_conf_extopts.m MM      cat_spm_preproc_write8.m MM      cat_stat_histth.m  M      cat_vol_pbtv.cpp  M      cat_vol_pbtv.mexmaci64 M       html/cat.html M       html/cat_versions.html
    (1) Fixed: Option "-nojvm" in cat_batch_cat.sh was not correctly working.
    (2) Fixed: Corrected typos in help text.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1287 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1285 (CAT12.2) 2018-02-28
+% Version 1286 (CAT12.2) 2018-03-05
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
