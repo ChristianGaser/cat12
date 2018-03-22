103f86080b8cea3c18ca100717d71b1374c16f69
Robert Dahnke <robert.dahnke@uni-jena.de>
2018-03-22 15:23:05 +0000

    Changed paths: M       CHANGES.txt M       cat_conf_tools.m M       cat_vol_div.m M       cat_vol_grad.m M       cat_vol_sanlm.m
    (1) Changed: Modified NAN handling in cat_vol_div and cat_vol_grad. 
                 The replacement by the cat_vol_vbdist function create
                 edges in the NAN regions. Now, only the neighborhood 
                 is replaced for the gradient/divergence estimation and 
                 the NAN region is set to zero.   
    (2) New:     Added option in the cat_vol_sanlm filter to garantie a 
                 minimal amount of noise in the images to avoid problems
                 by segmentation routines that expect a little variation
                 in the data.
    (3) Fixed:   Corrected bugs in NAN and INF replacement in cat_vol_sanlm.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1297 9f44b166-36c4-483c-8295-4af200601734

--- cat_conf_tools.m
+++ cat_conf_tools.m
@@ -588,6 +588,20 @@ NCstr.help    = {
 ''
 };
 
+ 
+addnoise         = cfg_entry;
+addnoise.tag     = 'addnoise';
+addnoise.name    = 'Strength of additional noise in noise free regions';
+addnoise.strtype = 'r';
+addnoise.val     = {0.5}; 
+addnoise.num     = [1 1];
+addnoise.help    = {
+ ['Add a minimal amount of noise in regions without noise to avoid  image segmentation problems. ' ...
+  ... 'The noise is only added to values unequal zero to avoid adding of noise in skull-stripped regions. ' ...
+  'This parameter defines the strength of additional noise as percentage of the average signal intensity. ']
+  ''
+};
+
 replaceNANandINF         = cfg_menu;
 replaceNANandINF.tag     = 'replaceNANandINF';
 replaceNANandINF.name    = 'Replace NAN and INF';
@@ -723,9 +737,9 @@ sanlm        = cfg_exbranch;
 sanlm.tag    = 'sanlm';
 sanlm.name   = 'Spatially adaptive non-local means denoising filter';
 if expert>1 % developer
-  sanlm.val    = {data spm_type prefix postfix intlim rician replaceNANandINF nlmfilter};
+  sanlm.val    = {data spm_type prefix postfix intlim addnoise rician replaceNANandINF nlmfilter};
 elseif expert
-  sanlm.val    = {data spm_type prefix postfix rician replaceNANandINF nlmfilter};
+  sanlm.val    = {data spm_type prefix postfix addnoise rician replaceNANandINF nlmfilter};
 else
   sanlm.val    = {data spm_type prefix nlmfilter};
 end
--- cat_vol_grad.m
+++ cat_vol_grad.m
@@ -4,8 +4,11 @@ function Yg = cat_vol_grad(Ym,vx_vol)
 % ----------------------------------------------------------------------
   if ~exist('vx_vol','var'), vx_vol=ones(1,3); end
   Ym = single(Ym); 
-  [D,I] = cat_vbdist(single(~isnan(Ym))); Ym = Ym(I); % replace nan
+  Ynan = isnan(Ym); 
+  [D,I] = cat_vbdist(single(~Ynan),cat_vol_morph(~Ynan,'d',1)); Ym(D<2) = Ym(I(D<2)); % replace nan
+  clear D I; 
   [gx,gy,gz] = cat_vol_gradient3(single(Ym)); 
   Yg = abs(gx./vx_vol(1))+abs(gy./vx_vol(2))+abs(gz./vx_vol(3)); 
+  Yg(Ynan) = 0; % restore nan?
   %Yg = Yg ./ (Ym+eps);
 return
