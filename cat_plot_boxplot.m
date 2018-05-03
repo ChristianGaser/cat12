0db8c982aba8abd5fa8c3566b589308fade9d6fc
Christian Gaser <christian.gaser@uni-jena.de>
2018-05-03 07:07:58 +0000

    Changed paths: M       CAT12-Manual.docx M       CAT12-Manual.pdf M       CHANGES.txt M       Contents.m M       cat_conf_extopts.m M       cat_defaults.m M       cat_main.m M       cat_plot_boxplot.m M       html/cat.html M       html/cat_methods_VBM.html M       html/cat_versions.html M       tbx_cfg_cat.m M       templates_1.50mm/cobra.csv M       templates_1.50mm/cobra.nii M       templates_1.50mm/cobra.txt
    (1) Changed: Updaed and extended Cobra atlas. See 
        cat12/templates_1.50mm/cobra.txt for more information.
    (2) Changed: Updated online help and CAT12 manual.
    (3) Changed: Added option for internal interpolation to default GUI.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1314 9f44b166-36c4-483c-8295-4af200601734

--- cat_plot_boxplot.m
+++ cat_plot_boxplot.m
@@ -31,7 +31,7 @@ function [out,s] = cat_plot_boxplot(data,opt)
 %  opt.median      = 2;             % show median: 0 - no; 1 - line; 2 - with different fill colors 
 %  opt.edgecolor   = 'none';        % edge color of box 
 %  opt.trans       = 0.25;          % transparency of the box
-%  opt.sat         = 0.50;          % satuation of the box
+%  opt.sat         = 0.50;          % saturation of the box
 %
 % The box plot is a graphical display that simultaneously describes several 
 % important features of a data set, such as center, spread, departure from 
