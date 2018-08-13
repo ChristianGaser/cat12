c5f7db6f4a6ac87ac3aabeeb026598c8a894d5ce
Christian Gaser <christian.gaser@uni-jena.de>
2018-08-13 10:21:10 +0000

    Changed paths: M       CAT12-Manual.docx M       CAT12-Manual.pdf M       CHANGES.txt M       Contents.m M       Makefile M       cat_conf_extopts.m M       cat_defaults.m M       cat_surf_results.m M       check_pipeline.sh A       check_pipeline_ROIs.m M       html/cat.html M       html/cat_methods_SBM.html M       html/cat_tools_VBM.html M       html/cat_versions.html M       spm_cat12.m M       tbx_cfg_cat.m
    (1) Fixed: Crahs in cat_surf_results due to typo.
    (2) Changed: Updated version information to CAT12.5-rc1.
    (3) Changed: Updated online help text and CAT12 manual.
    (4) Changed: Default interpolation is now "Fixed 1 mm" in cat_defaults.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1354 9f44b166-36c4-483c-8295-4af200601734

---
+++ check_pipeline_ROIs.m
@@ -0,0 +1,40 @@
+function check_pipeline_ROIs(P)
+%check_pipeline_ROIs to check distribution of ROI-values for different
+% cat12 releases using check_pipeline.sh
+%
+% _________________________________________________________________________
+% $Id: check_pipeline_ROIs.m 1341 2018-07-27 13:14:51Z gaser $
+
+if nargin == 0
+  P = spm_select(Inf,'csv','Select csv files');
+end
+
+for i=1:size(P,1)
+  csv_name = deblank(P(i,:));
+  C = cat_io_csv(csv_name);
+  [pth, name] = spm_fileparts(csv_name);
+  
+  [m,n] = size(C);
+  for j=1:m
+    for k=1:n
+      if strcmp(C{j,k},'NaN'), C{j,k} = 0; end
+    end
+  end
+
+  rev = num2str(cell2mat(C(2:end,1)));
+  
+  roi_names = C(1,:);
+
+  roi_values = cell2mat(C(2:end,2:end));
+  median_values = median(roi_values);
+  roi_values = roi_values - median_values;
+
+  figure
+  cat_plot_boxplot(roi_values,struct('names',rev,'violin',2,'showdata',1,'outliers',0,'ylim',[-3 3]));
+  pos = get(gcf,'Position');
+  pos(3:4) = [1000 500];
+  set(gcf,'MenuBar','none','Name',name,'Position',pos)
+  title('Neuromorphometric atlas: Difference to median [ml]')
+  saveas(gcf,[name '.png']);
+  
+end
