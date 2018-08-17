dbbbf88a422ca095849079fcf3805da80cb21eac
Christian Gaser <christian.gaser@uni-jena.de>
2018-08-17 12:02:44 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_surf_createCS.m MM      check_pipeline_ROIs.m M       html/cat.html
    (1) Changed: Added new flag to CAT_DeformSurf command in cat_surf_createCS.m
    (2) Changed: Updated new binaries because of new flag for CAT_DeformSurf.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1356 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1353 (CAT12.5-rc1) 2018-08-13
+% Version 1355 (CAT12.5-rc1) 2018-08-13
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
