b0485dac9c15da967af90711f9b9684bfaee3c7f
Christian Gaser <christian.gaser@uni-jena.de>
2019-01-18 15:37:58 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_main_reportfig.m M       html/cat.html
    Changed: Switched off some warnings in cat_main_reportfig.m for windows systems.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1420 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1412 (CAT12.6-rc1) 2019-01-09
+% Version 1419 (CAT12.6-rc1) 2019-01-18
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
