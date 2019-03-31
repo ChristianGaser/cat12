419ce4dc8066d7543f51b4d569e06d6763b3b7c1
Christian Gaser <christian.gaser@uni-jena.de>
2019-03-31 21:03:39 +0000

    Changed paths: A       CAT12-Manual-GoogleDocs.webloc M       CHANGES.txt M       Contents.m M       Makefile M       cat_stat_analyze_ROIs.m M       html/cat.html M       spm_cat12.m
    (1) Fixed: Corrected error in cat_stat_analyze_ROIs.m if no xml-files were found.
    (2) Added: GoogleDoc fpr CAT12 manual.
    (3) Changed: Updated version information.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1447 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1436 (CAT12.6-rc1) 2019-03-06
+% Version 1446 (CAT12.6) 2019-03-26
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
