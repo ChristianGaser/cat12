87317218203c146ca3658423cc0d72d0176c9573
Christian Gaser <christian.gaser@uni-jena.de>
2019-07-31 07:50:15 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat12.m M       cat_batch_cat.m M       cat_defaults.m M       cat_io_report.m A       cat_io_send_to_server.m M       cat_main_reportfig.m M       cat_plot_boxplot.m M       cat_run_newcatch.m M       html/cat.html M       html/cat.txt D       html/cat_ackknowledgement.html         > moved to html/cat_acknowledgement.html A  +    html/cat_acknowledgement.html         > moved from html/cat_ackknowledgement.html
    (1) Fixed: Many typos...
    (2) Changed: Added image tracker to provide statistics about Matlab and CAT12 
        versions and (un)successful preprocessings.
    (3) Changed: Added cat.extopts.send_info flag to control sending of statistics
        tp SBM server.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1490 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1487 (CAT12.6) 2019-06-19
+% Version 1489 (CAT12.6) 2019-07-02
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
