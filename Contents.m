1e22643e698b396e5f3bd525d6a1d552543b0d42
Christian Gaser <christian.gaser@uni-jena.de>
2019-08-16 14:05:38 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_run_newcatch.m M       cat_surf_createCS.m M       html/cat.html
    (1) Changed: Optimized parameters for surface reconstruction in 
        cat_surf_createCS.m.
    (2) Changed: Updated and optimized creation of pial and white surface in
        cat_surf_createCS.m.
    (3) Changed: Added further anonymization of filenames to cat_run_newcatch.m
    
    
    git-svn-id: svn://141.35.69.214/cat12@1498 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1495 (CAT12.7) 2019-08-14
+% Version 1497 (CAT12.7) 2019-08-15
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
