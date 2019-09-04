f6d171266d0184ba572f168eebdcbb88e92f71f0
Christian Gaser <christian.gaser@uni-jena.de>
2019-09-04 09:03:30 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_defaults.m M       cat_surf_createCS.m M       cat_surf_vol2surf.m M       html/cat.html
    (1) Fixed: Corrected deleting of temporary files in cat_surf_createCS.m for 
        creation of pial and WM surface.
    (2) Changed: Added automatically filling up of filenames if only 1 volume but 
        multiple surfaces are given in cat_surf_vol2surf.m
    
    
    git-svn-id: svn://141.35.69.214/cat12@1502 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1499 (CAT12.7) 2019-08-20
+% Version 1501 (CAT12.7) 2019-09-02
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
