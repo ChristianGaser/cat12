0f918771ab06b36072285bf567f83c19e87525a8
Christian Gaser <christian.gaser@uni-jena.de>
2019-09-23 13:13:43 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat12.m M       cat_conf_opts.m M       html/cat.html
    (1) Changed: Moved "spm processing accuracy" option to expert mode.
    (2) Fixed: Handling of font size in cat12.m was not correct.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1507 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1502 (CAT12.7) 2019-09-04
+% Version 1506 (CAT12.7) 2019-09-13
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
