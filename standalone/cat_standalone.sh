3b0af31578f8b53d7ddc6fb1ec90085b5d0812fb
Christian Gaser <christian.gaser@uni-jena.de>
2019-11-13 11:27:46 +0000

    Changed paths: M       CHANGES.txt M       Makefile D       cat_batch_resample_standalone.m         > moved to standalone/cat_standalone_resample.txt D       cat_batch_simple_standalone.m         > moved to standalone/cat_standalone_simple.txt D       cat_batch_standalone.m         > moved to standalone/cat_standalone_segment.txt D       cat_batch_standalone.sh         > moved to standalone/cat_standalone.sh D       cat_parallelize.sh         > moved to standalone/cat_parallelize.sh A       standalone A  +    standalone/cat_parallelize.sh         > moved from cat_parallelize.sh A  +    standalone/cat_standalone.sh         > moved from cat_batch_standalone.sh A  +    standalone/cat_standalone_resample.txt         > moved from cat_batch_resample_standalone.m A  +    standalone/cat_standalone_segment.txt         > moved from cat_batch_standalone.m A  +    standalone/cat_standalone_simple.txt         > moved from cat_batch_simple_standalone.m A       standalone/cat_standalone_smooth.txt
    (1) Changed: Moved standalone functions to separate folder "standalone" and
        renamed files to begin with "cat_standalone".
    (2) Changed: Added option to define two additional arguments for 
        cat_standalone.sh.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1518 9f44b166-36c4-483c-8295-4af200601734

--- standalone/cat_standalone.sh
+++ standalone/cat_standalone.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# $Id: cat_batch_standalone.sh 1361 2018-08-31 08:44:57Z gaser $
+# $Id$
 
 ########################################################
 # run main
@@ -49,6 +49,16 @@ parse_args ()
         --spm* | -s*)
             exit_if_empty "$optname" "$optarg"
             SPMROOT=$optarg
+            shift
+            ;;
+        --arg1* | -a1*)
+            exit_if_empty "$optname" "$optarg"
+            ARG1=$optarg
+            shift
+            ;;
+        --arg2* | -a2*)
+            exit_if_empty "$optname" "$optarg"
+            ARG2=$optarg
             shift
             ;;
         -h | --help | -v | --version | -V)
@@ -163,20 +173,19 @@ run_cat ()
 
   # copy everything except rows with UNDEFINED to temp file	
 	grep -v "<UNDEFINED>" $BATCHFILE > $TMP
-
-  # count lines with parameter UNDEFINED and give warning that only the first line will be used
-	n_undefined=`grep "<UNDEFINED>" $BATCHFILE | wc -l`
-	if [ "$n_undefined" -gt 1 ]; then
-		echo "------------------------------------------------------------------------------------------------------"
-		echo "Warning: Only the first found occurence of the parameter <UNDEFINED> will be dynamically replaced:"
-		echo `grep -m 1 "<UNDEFINED>" $BATCHFILE`
-		echo "------------------------------------------------------------------------------------------------------"
-		echo
-	fi
-
+  
 	# extract parameter name of data structure (1st occurance of "<UNDEFINED>")
 	data=`grep -m 1 "<UNDEFINED>" $BATCHFILE | cut -f1 -d'='`
 
+	# extract parameter name of optional argument(s) (additional occurances of "<UNDEFINED>")
+  if [ -n "$ARG1" ]; then # ARG1 defined?
+	  param1=`grep -m 2 "<UNDEFINED>" $BATCHFILE | tail -n 1 | cut -f1 -d'='`
+	  # extract parameter name of optional argument (3rd occurance of "<UNDEFINED>")
+		if [ -n "$ARG2" ]; then # ARG2 defined?
+			param2=`grep -m 3 "<UNDEFINED>" $BATCHFILE | tail -n 1 | cut -f1 -d'='`
+		fi
+  fi
+  
   # surface data need an additional curly bracket
   if grep -q "\.data_surf" $BATCHFILE ; then
     echo "$data = {{" >> $TMP
@@ -209,6 +218,14 @@ run_cat ()
 	else
 	  echo "     };" >> $TMP
 	fi
+	
+  if [ -n "$ARG1" ]; then # ARG1 defined?
+    echo "$param1 = $ARG1 ;" >> $TMP
+    if [ -n "$ARG2" ]; then # ARG2 defined?
+      echo "$param2 = $ARG2 ;" >> $TMP
+    fi
+  fi
+  
 	eval "\"${SPMROOT}/run_spm12.sh\"" $MCRROOT "batch" $TMP
 #	rm $TMP
 
@@ -225,56 +242,66 @@ help ()
 cat <<__EOM__
 
 USAGE:
-   cat_batch_standalone.sh filename(s) [-s spm_standalone_folder] [-m mcr_folder] [-b batch_file]
+   cat_standalone.sh filename(s) [-s spm_standalone_folder] [-m mcr_folder] [-b batch_file] 
+                                 [-a1 additional_argument1] [-a2 additional_argument2]
    
    -s   SPM12 folder of standalone version (can be also defined by SPMROOT)
    -m   Matlab Compiler Runtime (MCR) folder (can be also defined by MCRROOT)
-   -b   batch file
+   -b   batch file to execute
+   -a1  1st additional argument 
+   -a2  2nd additional argument 
    
    The first occurance of the parameter "<UNDEFINED>" in the batch file will be replaced by the
-   list of input files. You can use the existing batch files in CAT12 or create your own batch 
-   file with the SPM12 batch editor and leave the data field undefined. See cat_batch_standalone.m
-   for an example.
+   list of input files. You can use the existing batch files in this folder or create your own batch 
+   file with the SPM12 batch editor and leave the data field undefined. Please note that for creating
+   your own batch file CAT12 has to be called in expert mode because the CAT12 standalone installation 
+   will only run in expert mode to allow more options.
+   See cat_standalone_segment.txt for an example. 
+   
+   You can also define an one or two optional arguments to change other parameters that are indicated by "<UNDEFINED>"
+   in the batch file. Please take care of the order of the "<UNDEFINED>" fields in the batch file.
+   If you use a computer cluster it is recommended to use the batch files to only process one data set 
+   and use a job or queue tool to provide the single jobs to the cluster.
    
 PURPOSE:
-   Command line call of CAT12 segmentation for SPM12 standalone installation
+   Command line call of (CAT12) batch files for SPM12 standalone installation
 
 EXAMPLE
-   cat_batch_standalone.sh -b ${cwd}/cat_batch_standalone.m sTRIO0001.nii
-   Preprocess the single file sTRIO0001.nii using the default CAT12 
-   preprocessing batch. 
+   cat_standalone.sh -s ~/spm/standalone/ -m /Applications/MATLAB/MATLAB_Runtime/v93/ -b ${cwd}/cat_standalone_segment.txt sTRIO0001.nii
+   Preprocess (segment) the single file sTRIO0001.nii using the default CAT12 preprocessing batch. 
+   SPM12 standalone version is located in ~/spm/standalone and Matlab Compiler Runtime in
+   /Applications/MATLAB/MATLAB_Runtime/v93/.
 
-   cat_batch_standalone.sh -b ${cwd}/cat_batch_simple_standalone.m sTRIO0001.nii
+   cat_standalone.sh -s ~/spm/standalone/ -m /Applications/MATLAB/MATLAB_Runtime/v93/ -b ${cwd}/cat_standalone_simple.txt sTRIO0001.nii
    Process the single file sTRIO0001.nii using the simple processing batch. 
 
-   cat_batch_standalone.sh  -s ~/spm/standalone/ -m /Applications/MATLAB/MATLAB_Runtime/v93/ -b ${cwd}/cat_batch_standalone.m sTRIO0001
-   Preprocess the single file sTRIO0001.nii using the simple processing batch
-   and the SPM12 standalone version in ~/spm/standalone and Matlab Compiler Runtime in
-   /Applications/MATLAB/MATLAB_Runtime/v93/
-   
-   cat_batch_standalone.sh -b ${cwd}/cat_batch_resample_standalone.m lh.thickness.sTRIO0001
-   Resample and smooth the single thickness file sTRIO0001.nii. Only the left surface file has to be defined.
+   cat_standalone.sh -s ~/spm/standalone/ -m /Applications/MATLAB/MATLAB_Runtime/v93/ -b ${cwd}/cat_standalone_resample.txt -a1 "12" -a2 "1" lh.thickness.sTRIO0001
+   Resample and smooth the single thickness file lh.thickness.sTRIO0001 with 12mm and save the resampled mesh as 32k mesh from HCP. 
+   Only the left surface file has to be defined.
 
-   cat_parallelize.sh -p 8 -l /tmp -c "cat_batch_standalone.sh  -s ~/spm/standalone/ -m /Applications/MATLAB/MATLAB_Runtime/v93/ -b ${cwd}/cat_batch_standalone.m" sTRIO*.nii
+   cat_standalone.sh -s ~/spm/standalone/ -m /Applications/MATLAB/MATLAB_Runtime/v93/ -b ${cwd}/cat_standalone_smooth.txt -a1 "[6 6 6]" -a2 "'s6'" sTRIO*nii
+   Smooth the volume files sTRIO*nii with 6mm and prepend the string "s6" to smoothed files.
+
+   cat_parallelize.sh -p 8 -l /tmp -c "cat_standalone.sh  -s ~/spm/standalone/ -m /Applications/MATLAB/MATLAB_Runtime/v93/ -b ${cwd}/cat_standalone_segment.txt" sTRIO*.nii
    Parallelize CAT12 preprocessing by splitting all sTRIO*.nii files into 8 jobs 
    (processes) and save log file in /tmp folder. 
 
-   The parameter SPMROOT and MCRROOT have to be defined if no additional flags -s -m are used.
+   The parameters SPMROOT and MCRROOT have to be defined (exported) to skip the use of the flags -s -m.
 
 INPUT:
-   analyze or nifti files
+   nifti files or surface data
 
 OUTPUT:
-   segmented images and optionally surfaces according to settings in cat_batch_standalone.m or
-   cat_batch_simple_standalone.m.
+   segmented images and optionally surfaces according to settings in cat_standalone_segment.txt or
+   cat_standalone_simple.txt.
 
 USED FUNCTIONS:
-   cat_batch_standalone.m
-   cat_batch_simple_standalone.m
-   cat_batch_resample_standalone.m
+   cat_standalone_segment.txt
+   cat_standalone_simple.txt
+   cat_standalone_resample.txt
    cat_parallelize.sh
    SPM12 standalone version (compiled)
-   CAT12 toolbox (compiled within SPM12)
+   CAT12 toolbox (compiled within SPM12 if installed)
    MATLAB Compiler Runtime R2017b (Version 9.3)
 
 This script was written by Christian Gaser (christian.gaser@uni-jena.de).
