c56fc1874802e6a05818e660b59f8d0228a65132
Christian Gaser <christian.gaser@uni-jena.de>
2019-12-05 08:36:42 +0000

    Changed paths: M       CHANGES.txt M       cat_main_reportstr.m M       cat_surf_createCS.m M       cat_surf_createCS2.m M       cat_surf_fun.m M       cat_vol_findfiles.m
    (1) Fixed: prevent use of doubled wildcards in cat_fol_findfiles.m that were
        not allowed.
    (2) Changed: Disabled printout of computation time for each single step of 
        collission detection in cat_surf_fun.m and reformated some code.
    (3) Changed: Added some changes for compatibility for Octave.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1530 9f44b166-36c4-483c-8295-4af200601734

--- cat_main_reportstr.m
+++ cat_main_reportstr.m
@@ -262,14 +262,14 @@ function str = cat_main_reportstr(job,res,qa,cat_warnings)
   thstr = 'Thickness';
   if isfield(qa.subjectmeasures,'dist_thickness') && ~isempty(qa.subjectmeasures.dist_thickness)
     str{3} = [str{3} struct('name', ['\bf' thstr ':'],'value',sprintf('%5.2f%s%5.2f mm', ...
-           qa.subjectmeasures.dist_thickness{1}(1),char(177),qa.subjectmeasures.dist_thickness{1}(2)))];
+           qa.subjectmeasures.dist_thickness{1}(1),native2unicode(177, 'latin1'),qa.subjectmeasures.dist_thickness{1}(2)))];
     if isfield(qa.subjectmeasures,'dist_gyruswidth') && ~isnan(qa.subjectmeasures.dist_gyruswidth{1}(1))
       str{3} = [str{3} struct('name', '\bfGyruswidth:','value',sprintf('%5.2f%s%5.2f mm', ...
-             qa.subjectmeasures.dist_gyruswidth{1}(1),char(177),qa.subjectmeasures.dist_gyruswidth{1}(2)))];
+             qa.subjectmeasures.dist_gyruswidth{1}(1),native2unicode(177, 'latin1'),qa.subjectmeasures.dist_gyruswidth{1}(2)))];
     end
     if isfield(qa.subjectmeasures,'dist_sulcuswidth') && ~isnan(qa.subjectmeasures.dist_sulcuswidth{1}(1))
       str{3} = [str{3} struct('name', '\bfSulcuswidth:','value',sprintf('%5.2f%s%5.2f mm', ...
-             qa.subjectmeasures.dist_sulcuswidth{1}(1),char(177),qa.subjectmeasures.dist_sulcuswidth{1}(2)))];
+             qa.subjectmeasures.dist_sulcuswidth{1}(1),native2unicode(177, 'latin1'),qa.subjectmeasures.dist_sulcuswidth{1}(2)))];
     end
   end
 
