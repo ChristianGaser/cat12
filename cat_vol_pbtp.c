3343a83766649de13b97793f53f5088308ecc288
Christian Gaser <christian.gaser@uni-jena.de>
2020-01-24 23:47:44 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_amap.mexa64 M       cat_amap.mexw64 M       cat_ornlm.mexa64 M       cat_ornlm.mexw64 M       cat_sanlm.mexa64 M       cat_sanlm.mexw64 M       cat_vbdist.mexa64 M       cat_vbdist.mexw64 M       cat_vol_downcut.mexa64 M       cat_vol_downcut.mexw64 M       cat_vol_eidist.mexa64 M       cat_vol_eidist.mexw64 M       cat_vol_genus0.mexa64 M       cat_vol_genus0.mexmaci64 M       cat_vol_genus0.mexw64 M       cat_vol_gradient3.mexa64 M       cat_vol_gradient3.mexw64 M       cat_vol_interp3f.mexa64 M       cat_vol_interp3f.mexw64 M       cat_vol_laplace3.mexa64 M       cat_vol_laplace3.mexw64 M       cat_vol_laplace3R.mexa64 M       cat_vol_laplace3R.mexw64 M       cat_vol_localstat.mexa64 M       cat_vol_localstat.mexw64 M       cat_vol_median3.mexa64 M       cat_vol_median3.mexw64 M       cat_vol_median3c.mexa64 M       cat_vol_median3c.mexw64 MM      cat_vol_pbtp.c M       cat_vol_pbtp.mexa64 M       cat_vol_pbtp.mexw64  M      cat_vol_pbtp2.c  M      cat_vol_pbtp2.mexmaci64 M       cat_vol_simgrow.mexa64 M       cat_vol_simgrow.mexw64 M       html/cat.html M       use_of_external_software.txt
    1. Changed: Recompiled mex-files for all systems.
    2. Changed: Made cat_vol_pbtp.c more conform to older gcc versions.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1555 9f44b166-36c4-483c-8295-4af200601734

--- cat_vol_pbtp.c
+++ cat_vol_pbtp.c
@@ -31,7 +31,6 @@ struct opt_type {
 	int   PVE;													/* 0, 1=fast, 2=exact */
 	float LB, HB, LLB, HLB, LHB, HHB;  	/* boundary */
 	int   sL[3];
-	// ...
 	} opt;
 
 float min(float a, float b) {
@@ -42,7 +41,7 @@ float max(float a, float b) {
   if (a>b) return a; else return b; 
 }
 
-// get all values of the voxels which are in WMD-range (children of this voxel)  
+/* get all values of the voxels which are in WMD-range (children of this voxel) */
 float pmax(const float GMT[], const float RPM[], const float SEG[], const float ND[], const float WMD, const float SEGI, const int sA) {
   float T[27]; for (int i=0;i<27;i++) T[i]=-1; 
   float n=0.0, maximum=WMD; 
@@ -62,7 +61,7 @@ float pmax(const float GMT[], const float RPM[], const float SEG[], const float 
   /* the mean of the highest values*/
   float maximum2=maximum; float m2n=0; 
   for (int i=0;i<=sA;i++) {
-    if ( ( GMT[i] < 1e15 ) && ( maximum < GMT[i] ) &&   // ( (maximum - 1) < GMT[i] ) && 
+    if ( ( GMT[i] < 1e15 ) && ( maximum < GMT[i] ) &&   /* ( (maximum - 1) < GMT[i] ) && */
          ( SEG[i] >= 1.0 ) && ( SEGI>1.2 && SEGI<=2.75 ) && 
          ( ( (RPM[i] - ND[i] * 1.2 ) <= WMD ) ) && 
          ( ( (RPM[i] - ND[i] * 0.5 ) >  WMD ) || ( SEG[i]<1.5 ) ) &&
@@ -77,7 +76,7 @@ float pmax(const float GMT[], const float RPM[], const float SEG[], const float 
 
 
 
-// estimate x,y,z position of index i in an array size sx,sxy=sx*sy...
+/* estimate x,y,z position of index i in an array size sx,sxy=sx*sy... */
 void ind2sub(int i, int *x, int *y, int *z, int snL, int sxy, int sy) {
   /* not here ... 
    *  if (i<0) i=0; 
@@ -92,7 +91,7 @@ void ind2sub(int i, int *x, int *y, int *z, int snL, int sxy, int sy) {
 
 
 
-// main function
+/* main function */
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   if (nrhs<3) mexErrMsgTxt("ERROR: not enought input elements\n");
   if (nrhs>4) mexErrMsgTxt("ERROR: to many input elements.\n");
@@ -100,7 +99,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   if (mxIsSingle(prhs[0])==0) mexErrMsgTxt("ERROR: first  input must be an 3d single matrix\n");
  
   
-  // main information about input data (size, dimensions, ...)
+  /* main information about input data (size, dimensions, ...) */
   const mwSize *sL = mxGetDimensions(prhs[0]); 
   mwSize sSEG[] = {sL[0],sL[1],sL[2]}; 
   const int     dL = mxGetNumberOfDimensions(prhs[0]);
@@ -112,7 +111,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   const float   s3 = sqrt(3.0);
   const int     nr = nrhs;
   
-  // indices of the neighbor Ni (index distance) and euclidean distance NW
+  /* indices of the neighbor Ni (index distance) and euclidean distance NW */
   const int   NI[]  = {  0, -1,-x+1, -x,-x-1,  -xy+1,-xy,-xy-1,  -xy+x+1,-xy+x,-xy+x-1,  -xy-x+1,-xy-x,-xy-x-1};  
   const float ND[]  = {0.0,1.0,  s2,1.0,  s2,     s2,1.0,   s2,       s3,   s2,     s3,       s3,   s2,     s3};
   const int   sN  = sizeof(NI)/4;  
@@ -121,7 +120,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   float 	    du, dv, dw, dnu, dnv, dnw, d, dcf, WMu, WMv, WMw, GMu, GMv, GMw, SEGl, SEGu, tmpfloat;
   int         ni,u,v,w,nu,nv,nw, tmpint, WMC=0, CSFC=0;
     
-  // main volumes - actual without memory optimization ...
+  /* main volumes - actual without memory optimization ... */
   plhs[0] = mxCreateNumericArray(dL,sL,mxSINGLE_CLASS,mxREAL);
   plhs[1] = mxCreateNumericArray(dL,sL,mxSINGLE_CLASS,mxREAL);
   
@@ -131,7 +130,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   plhs[4] = mxCreateNumericArray(dL,sL,mxUINT32_CLASS,mxREAL);  
 */  
 
-  // input variables
+  /* input variables */
   float*SEG  = (float *)mxGetPr(prhs[0]);
   float*WMD  = (float *)mxGetPr(prhs[1]);
   float*CSFD = (float *)mxGetPr(prhs[2]);
@@ -145,15 +144,15 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	else */{ opt.CSFD = 1;opt.PVE = 2;opt.LB = 1.5;opt.HB	= 2.5; }
 	opt.LLB=floor(opt.LB), opt.HLB=ceil(opt.LB), opt.LHB=floor(opt.HB), opt.HHB=ceil(opt.HB);
   
-  // output variables
+  /* output variables */
   float        *GMT  = (float *)mxGetPr(plhs[0]);
   float        *RPM  = (float *)mxGetPr(plhs[1]);
   
-  // intitialisiation
+  /* intitialisiation */
   for (int i=0;i<nL;i++) {
   	GMT[i] = WMD[i];
     RPM[i] = WMD[i];
-		// proof distance input
+		/* proof distance input */
     if ( SEG[i]>=opt.HB ) WMC++;
     if ( SEG[i]<=opt.LB ) CSFC++;
   }
@@ -162,13 +161,13 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 
   
   
-// thickness calcuation
-// =============================================================================
+/* thickness calcuation
+ ============================================================================= */
   for (int i=0;i<nL;i++) {
     if (SEG[i]>opt.LLB && SEG[i]<opt.HHB) {
       ind2sub(i,&u,&v,&w,nL,xy,x);
       
-      // read neighbor values
+      /* read neighbor values */
       for (int n=0;n<sN;n++) {
         ni = i + NI[n];
         ind2sub(ni,&nu,&nv,&nw,nL,xy,x);
@@ -176,7 +175,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
         GMTN[n] = GMT[ni]; WMDN[n] = RPM[ni]; SEGN[n] = SEG[ni];
       }
 
-      // find minimum distance within the neighborhood
+      /* find minimum distance within the neighborhood */
       DNm = pmax(GMTN,WMDN,SEGN,ND,WMD[i],SEG[i],sN);
       GMT[i] = DNm;
     }
@@ -186,7 +185,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     if (SEG[i]>opt.LLB && SEG[i]<opt.HHB) {
       ind2sub(i,&u,&v,&w,nL,xy,x);
       
-      // read neighbor values
+      /* read neighbor values */
       for (int n=0;n<sN;n++) {
         ni = i - NI[n];
         ind2sub(ni,&nu,&nv,&nw,nL,xy,x);
@@ -194,21 +193,21 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
         GMTN[n] = GMT[ni]; WMDN[n] = RPM[ni]; SEGN[n] = SEG[ni];
       }
 
-      // find minimum distance within the neighborhood
+      /* find minimum distance within the neighborhood */
       DNm = pmax(GMTN,WMDN,SEGN,ND,WMD[i],SEG[i],sN);
       if ( GMT[i] < DNm && DNm>0 ) GMT[i] = DNm;
     }
   }
   
-  for (int i=0;i<nL;i++) if (SEG[i]<opt.LB || SEG[i]>opt.HB) GMT[i]=0; //WMD[i]
+  for (int i=0;i<nL;i++) if (SEG[i]<opt.LB || SEG[i]>opt.HB) GMT[i]=0; 
 
  
   
 
 
-// final setings...
-// =============================================================================
-	float CSFDc = 0, GMTi, CSFDi; // 0.125
+/* final setings...
+ ============================================================================= */
+	float CSFDc = 0, GMTi, CSFDi; /* 0.125 */
 	for (int i=0;i<nL;i++) { 
 		/* GMT[i] = min(CSFD[i] + WMD[i],GMT[i]); */
 		if (SEG[i]>=opt.LB & SEG[i]<=opt.LB) {
@@ -221,8 +220,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	}
 
  
-// estimate RPM
-// =============================================================================
+/* estimate RPM
+ ============================================================================= */
 	for (int i=0;i<nL;i++) {
 		if ( SEG[i]>=opt.HB ) 	
       RPM[i]=1.0; 
