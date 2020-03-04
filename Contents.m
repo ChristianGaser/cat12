f54506fb7481db8fa8669b93f2186498b2830902
Christian Gaser <christian.gaser@uni-jena.de>
2020-03-04 13:38:09 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       Makefile M       cat_conf_stools.m M       cat_conf_stoolsexp.m M       cat_main.m M       cat_surf_parameters.m M       cat_surf_results.m M       check_pipeline.sh M       html/cat.html M       html/cat_methods_RBM.html M       html/cat_starting.html M       html/cat_versions.html
    (1) Changed: Lots of changes in cat_surf_results.
    (2) Changed: Updated online help.
    (3) Fixed: Surface ROI estimation during preprocessing selected too many atlases.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1576 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1571 (CAT12.7) 2020-02-27
+% Version 1575 (CAT12.7-Beta) 2020-03-03
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
