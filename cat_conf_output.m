880ca3fe55a33e89d0a7f9659a751b78e3e59358
Christian Gaser <christian.gaser@uni-jena.de>
2020-05-19 14:53:30 +0000

    Changed paths: M       CHANGES.txt M       cat_conf_output.m M       cat_long_main.m M       cat_vol_maskimage.m
    (1) Changed: Removed expert-call in long. batch mode which is not needed 
                 anymore.
    (2) Fixed:   Corrected weird bug in cat_conf_tools.m that caused crash of SPM 
                 batch system.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1630 9f44b166-36c4-483c-8295-4af200601734

--- cat_conf_output.m
+++ cat_conf_output.m
@@ -392,19 +392,35 @@ function [output,output_spm,output1173,output1445] = cat_conf_output(expert)
   [ROI1173,atlases1173]         = cat_conf_ROI1173(expert);       % ROI options
   [ROI1445,atlases1445]         = cat_conf_ROI1445(expert);       % ROI options
 %  [ROI1585,atlases1585]         = cat_conf_ROI1585(expert);       % ROI options
+
+  % CG 20200819: this is not yet working and crashes SPM batch system and we need the old exoert check below
   atlases1173.hidden    = expert<1;
   atlases1445.hidden    = expert<1;
   
   output1173            = output;
-  output1173.val        = {surface ROI1173 atlases1173 grey white csf wmh tpmc atlas label bias las jacobian warps}; 
-  
-  output1445            = output;
-  output1445.val        = {surface ROI1445 atlases1445 grey white csf wmh tpmc atlas label bias las jacobian warps}; 
+  if expert
+    output1173.val      = {surface ROI1173 atlases1173 grey white csf wmh tpmc atlas label bias las jacobian warps}; 
+  else
+    output1173.val      = {surface ROI1173 grey white csf wmh tpmc atlas label bias las jacobian warps};
+  end
 
-  %output1585            = output;
-  %output1585.val        = {surface ROI1585 atlases1585 grey white csf wmh tpmc atlas label bias las jacobian warps}; 
+  output1445            = output;
+  if expert
+    output1445.val      = {surface ROI1445 atlases1445 grey white csf wmh tpmc atlas label bias las jacobian warps}; 
+  else
+    output1445.val      = {surface ROI1445 grey white csf wmh tpmc atlas label bias las jacobian warps};
+  end
+
+if 0
+  output1585            = output;
+  if expert
+    output1585.val      = {surface ROI1585 atlases1585 grey white csf wmh tpmc atlas label bias las jacobian warps}; 
+  else
+    output1585.val      = {surface ROI1585 atlases1585 grey white csf wmh tpmc atlas label bias las jacobian warps};
+  end
+end
   
-	output_spm            = output; % also CSF output because it is requiered as input ... 
+	output_spm  = output; 
   output_spm.val        = {ROI surface grey_spm white_spm csf_spm label labelnative jacobianwarped warps}; 
 
 return
