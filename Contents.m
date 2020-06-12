3e5795a63f23e6c0486fafc7e6744651794f9124
Christian Gaser <christian.gaser@uni-jena.de>
2020-06-12 13:57:55 +0000

    Changed paths: M       CAT12-Manual.pdf M       CHANGES.txt M       Contents.m M       cat_conf_long.m M       html/cat.html
    Changed: Updated help text and CAT12 manual.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1652 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1646 (CAT12.7-Beta) 2020-06-08
+% Version 1651 (CAT12.7-RC1) 2020-06-12
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
