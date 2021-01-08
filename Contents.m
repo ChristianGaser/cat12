23f4c75e8562068bf6bcf068a281fe91c46b107b
Christian Gaser <christian.gaser@uni-jena.de>
2021-01-08 16:19:26 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       Makefile M       html/cat.html R       templates_volumes/anatomy3.csv RM      templates_volumes/anatomy3.nii R       templates_volumes/anatomy3.txt
    Changed: Resolved issue with anatomy3-files.
    
    
    git-svn-id: svn://141.35.69.214/cat12@1748 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1740 (CAT12.7) 2020-12-01
+% Version 1742 (CAT12.7) 2020-12-30
 % __________________________________________________________________________
 % Copyright (C) Christian Gaser christian.gaser@uni-jena.de
 %
