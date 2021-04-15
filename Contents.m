0365e4f66c0c6c934cd4d8143ccc0b38bcae5dbd
Christian Gaser <christian.gaser@uni-jena.de>
2021-04-15 19:03:25 +0000

    Changed paths: M       CHANGES.txt M       Contents.m A       cat_conf_factorial.m M       cat_io_data2mat.m M       cat_stat_factorial_design.m M       cat_stat_spm.m M       html/cat.html M       tbx_cfg_cat.m
    (1) Added:   Added new batch for factorial designs for VBM that skips many
                 options that are not needed for VBM. There are two models 
                 available for cross-sectional and longitudinal data. 
    (2) Changed: Some smaller changes to cat_stat_spm.m and cat_io_data2mat.m.
    (3) Changed: Updated basic design that is called from CAT12 GUI. 
    
    
    git-svn-id: svn://141.35.69.214/cat12@1802 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1800 (CAT12.8-Beta) 2021-04-14
+% Version 1801 (CAT12.8-Beta) 2021-04-14
 % ______________________________________________________________________
 %
 % Christian Gaser, Robert Dahnke
