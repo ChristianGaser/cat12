6ef91c08f18dba4b502a7f636e3e1c7b34708745
Christian Gaser <christian.gaser@uni-jena.de>
2021-05-12 15:32:40 +0000

    Changed paths: M       CAT12-Manual.pdf M       CHANGES.txt M       Contents.m M       cat_batch_cat.sh M       cat_batch_long.sh M       cat_batch_spm.sh M       development/Anatomy2CAT12.m M       development/JulichBrain2CAT12.m A       development/ROI_summarize_MPM.m M       html/cat.html M       html/cat_licences.html M       html/cat_methods_RBM.html M       html/cat_versions.html M       templates_MNI152NLin2009cAsym/Schaefer2018_100Parcels_17Networks_order.csv M       templates_MNI152NLin2009cAsym/Schaefer2018_200Parcels_17Networks_order.csv M       templates_MNI152NLin2009cAsym/Schaefer2018_400Parcels_17Networks_order.csv A  +    templates_MNI152NLin2009cAsym/Template_T1_masked.nii         > moved from templates_MNI152NLin2009cAsym/mTemplate_T1.nii M       templates_MNI152NLin2009cAsym/aal3.csv M       templates_MNI152NLin2009cAsym/aal3.txt M       templates_MNI152NLin2009cAsym/anatomy3.nii A       templates_MNI152NLin2009cAsym/brainmask_T1.nii M       templates_MNI152NLin2009cAsym/cobra.csv M       templates_MNI152NLin2009cAsym/cobra.nii M       templates_MNI152NLin2009cAsym/cobra.txt M       templates_MNI152NLin2009cAsym/hammers.csv M       templates_MNI152NLin2009cAsym/hammers.nii M       templates_MNI152NLin2009cAsym/hammers.txt M       templates_MNI152NLin2009cAsym/ibsr.csv M       templates_MNI152NLin2009cAsym/ibsr.nii M       templates_MNI152NLin2009cAsym/ibsr.txt M       templates_MNI152NLin2009cAsym/julichbrain.csv M       templates_MNI152NLin2009cAsym/julichbrain.nii M       templates_MNI152NLin2009cAsym/julichbrain.txt M       templates_MNI152NLin2009cAsym/lpba40.csv M       templates_MNI152NLin2009cAsym/lpba40.nii M       templates_MNI152NLin2009cAsym/lpba40.txt D       templates_MNI152NLin2009cAsym/mTemplate_T1.nii         > moved to templates_MNI152NLin2009cAsym/Template_T1_masked.nii M       templates_MNI152NLin2009cAsym/mori.csv M       templates_MNI152NLin2009cAsym/mori.txt M       templates_MNI152NLin2009cAsym/neuromorphometrics.csv M       templates_MNI152NLin2009cAsym/neuromorphometrics.nii M       templates_MNI152NLin2009cAsym/neuromorphometrics.txt M       templates_MNI152NLin2009cAsym/schaefer.txt M       templates_MNI152NLin2009cAsym/templates.man
    (1) Changed: Updated atlas text in version information and description.
    (2) Changed: Added brainmask_T1.nii to templateb folder.
    (3) Changed: Updated information about templates.
    (4) Changed: Updated hammers atlas to new version with 95 regions.
    (5) Changed: Updated neuromorphometrics atlas by estimating maximum probability
                 maps.
    (6) Changed: Updated julicbrain atlas to version v2.6.
    (7) Changed: Some small modifications to scripts for creating anatomy and 
                 julichbrain atlas.
    (8) Fixed:   Call of shell scripts from relative path is now working.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1823 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1820 (CAT12.8-Beta) 2021-05-03
+% Version 1821 (CAT12.8-Beta) 2021-05-05
 % ______________________________________________________________________
 %
 % Christian Gaser, Robert Dahnke
