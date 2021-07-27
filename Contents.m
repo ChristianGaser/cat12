46cc24190ecaef40e2742fa00837afba70e3605a
Christian Gaser <christian.gaser@uni-jena.de>
2021-07-27 11:22:08 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       cat_main_amap.m M       cat_run_job.m M       cat_stat_quality_measures.m M       html/cat.html M       spm_cat12.m
    (1) Changed: Replaced volume reading functions in cat_stat_quality_measures.m
                 by faster nifti functions.
    (2) Fixed:   Corrected bugs in backup pipeline.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1867 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1860 (CAT12.8-Beta) 2021-07-11
+% Version 1862 (CAT12.8) 2021-07-20
 % ______________________________________________________________________
 %
 % Christian Gaser, Robert Dahnke
