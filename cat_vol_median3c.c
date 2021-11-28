6f25e05f07ae8193f26174417f6db1f6ea7a8879
Robert Dahnke <robert.dahnke@uni-jena.de>
2021-11-28 20:44:47 +0000

    Changed paths: M       CHANGES.txt M       cat_vol_downcut.c M       cat_vol_downcut.mexmaci64 M       cat_vol_gradient3.c M       cat_vol_gradient3.mexmaci64 M       cat_vol_median3.c M       cat_vol_median3.m M       cat_vol_median3.mexmaci64 M       cat_vol_median3c.c M       cat_vol_median3c.m M       cat_vol_median3c.mexmaci64 M       cat_vol_pbtp.c M       cat_vol_pbtp.mexmaci64
    (1) Fixed:   Corrected Octave bugs and added examples in documentation in 
                   cat_vol_laplace3(R), cat_vol_simgrow, cat_vol_gradient3, 
                   cat_vol_eikonal3, cat_vol_eidist, cat_vol_downcut,
                   cat_vol_pbtp, and cat_vol_median3(c).
    (2) Fixed:   Small adaptions for display function ds.               
    (3) New:     Added normalization parameter to cat_vol_gradient3. 
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1913 9f44b166-36c4-483c-8295-4af200601734

--- cat_vol_median3c.c
+++ cat_vol_median3c.c
@@ -12,6 +12,13 @@
  *  Bi     (logical)  3d matrix that mark voxel that should be filtered
  *  Bn     (logical)  3d matrix that mark voxel that are used as neighbors 
  *
+ *  Examples: 
+ *   1)
+ *     A = round(smooth3(rand(50,50,3,'single')*3));
+ *     B = false(size(A)); B(5:end-4,5:end-4,:)=true; 
+ *     C = cat_vol_median3c(A,B); C = cat_vol_median3c(C,B); 
+ *     ds('d2smns','',1,A+B,C,2);
+ *
  * ______________________________________________________________________
  *
  * Christian Gaser, Robert Dahnke
@@ -63,7 +70,7 @@ float abs2(float n) {	if (n<0) return -n; else return n; }
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
   if (nrhs<1) mexErrMsgTxt("ERROR:median3: not enough input elements\n");
-  if (nrhs>8) mexErrMsgTxt("ERROR:median3: too many input elements\n");
+  if (nrhs>3) mexErrMsgTxt("ERROR:median3: too many input elements\n");
   if (nlhs<1) mexErrMsgTxt("ERROR:median3: not enough output elements\n");
   if (nlhs>1) mexErrMsgTxt("ERROR:median3: too many output elements\n");
 
@@ -72,77 +79,51 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   const int     dL = mxGetNumberOfDimensions(prhs[0]);
   const int     nL = (int) mxGetNumberOfElements(prhs[0]);
 
-  if ( dL != 3 || mxIsSingle(prhs[0])==0)        mexErrMsgTxt("ERROR:median3: first input must be a single 3d matrix\n");
+  if ( dL != 3 || mxIsSingle(prhs[0])==0)        mexErrMsgTxt("ERROR:median3c: first input must be a single 3d matrix\n");
   if ( nrhs>1) {
-    if ( mxGetNumberOfDimensions(prhs[1]) != 3 ) mexErrMsgTxt("ERROR:median3: second input must be 3d - to use a later parameter use ''true(size( input1 ))''\n");
-    if (mxIsLogical(prhs[1])==0)                 mexErrMsgTxt("ERROR:median3: second input must be a logical 3d matrix\n");
+    if ( mxGetNumberOfDimensions(prhs[1]) != 3 ) mexErrMsgTxt("ERROR:median3c: second input must be 3d - to use a later parameter use ''true(size( input1 ))''\n");
+    if (mxIsLogical(prhs[1])==0)                 mexErrMsgTxt("ERROR:median3c: second input must be a logical 3d matrix\n");
   } else if ( nrhs>2) {
-    if ( mxGetNumberOfDimensions(prhs[2]) != 3 ) mexErrMsgTxt("ERROR:median3: third input must be 3d - to use a later parameter use ''true(size( input1 ))'\n");
-    if ( mxIsLogical(prhs[2])==0)                mexErrMsgTxt("ERROR:median3: third input must be a logical 3d matrix\n"); 
+    if ( mxGetNumberOfDimensions(prhs[2]) != 3 ) mexErrMsgTxt("ERROR:median3c: third input must be 3d - to use a later parameter use ''true(size( input1 ))'\n");
+    if ( mxIsLogical(prhs[2])==0)                mexErrMsgTxt("ERROR:median3c: third input must be a logical 3d matrix\n"); 
   }
 
   /* indices of the neighbor Ni (index distance) and euclidean distance NW */
-  float sf, bil, bih, bnl, bnh;
-  int NV[256],i,j,k,ind,ni,x,y,z,n;
+  int NV[256],ind,ni,n;
   bool *Bi, *Bn;
         
   /* in- and output */
   float *D = (float *) mxGetPr(prhs[0]);
   if (nrhs>1)  Bi = (bool *) mxGetPr(prhs[1]); 
   if (nrhs>2)  Bn =  (bool *) mxGetPr(prhs[2]); 
-  if (nrhs<4)  sf = 0; 
-  else         sf = (float) *mxGetPr(prhs[3]);
-
-  if (nrhs<5) bil = -FLT_MAX;   
-  else        bil = (float) *mxGetPr(prhs[4]);
-  if (nrhs<6) bih =  FLT_MAX;   
-  else        bih = (float) *mxGetPr(prhs[5]);
-  if (nrhs<7) bnl = -FLT_MAX;   
-  else        bnl = (float) *mxGetPr(prhs[6]);  
-  if (nrhs<8) bnh =  FLT_MAX;   
-  else        bnh = (float) *mxGetPr(prhs[7]);
 
   plhs[0] = mxCreateNumericArray(dL,sL,mxSINGLE_CLASS,mxREAL);
   float *M = (float *) mxGetPr(plhs[0]);
   
   /* filter process */
-  for (z=0;z<sL[2];z++) for (y=0;y<sL[1];y++) for (x=0;x<sL[0];x++) {
+  for (int z=0;z<sL[2];z++) for (int y=0;y<sL[1];y++) for (int x=0;x<sL[0];x++) {
     ind = index(x,y,z,sL);
-    if ((nrhs==1 || (nrhs>=2 && Bi[ind])) && D[ind]>=0 && D[ind]>=bil && D[ind]<=bih) {
+    if ((nrhs==1 || (nrhs>=2 && Bi[ind])) ) {
       n = 0;
       /* go through all elements in a 3x3x3 box */
-      for (i=0;i<255;i++) NV[i]=0;  
-      for (i=-1;i<=1;i++) for (j=-1;j<=1;j++) for (k=-1;k<=1;k++) {
+      for (int i=0;i<255;i++) NV[i]=0;  
+      for (int i=-1;i<=1;i++) for (int j=-1;j<=1;j++) for (int k=-1;k<=1;k++) {
         /* check borders */ 
         if ( ((x+i)>=0) && ((x+i)<sL[0]) && ((y+j)>=0) && ((y+j)<sL[1]) && ((z+k)>=0) && ((z+k)<sL[2])) {
           ni = index(x+i,y+j,z+k,sL);
           /* check masks and NaN or Infinities */
-          if ((nrhs>=3 && Bn[ni]==0) || D[ni]<0 || D[ni]<bnl || D[ni]>bnh || isnan(D[ni]) ||
+          if ((nrhs>=3 && Bn[ni]==0) || isnan(D[ni]) ||
               D[ni]==FLT_MAX || D[ind]==-FLT_MAX ) ni = ind;
           NV[(int) D[ni]]++;
         }
       }
       /* find maximum occurent value */
-      M[ind]=0; n=0; for (i=0;i<255;i++) {if ((NV[i]>=0) && (NV[i]>n)) {n=NV[i]; M[ind]=i;}}
+      M[ind]=0.0; n=0; for (int i=0;i<255;i++) {if ((NV[i]>=0) && (NV[i]>n)) {n=NV[i]; M[ind]=i;}}
      }
     else {
       M[ind] = D[ind];
     }
   }
-  
-  /* selective filter settings - only big changes (only change extremly noisy data) */
-  if (sf>0) {
-    for (i=0;i<nL;i++) {
-      if ( (nrhs>=2 && Bi[i]) && D[i]>bil && D[i]<bih && (abs2(D[i]-M[i])<sf) ) M[i]=D[i];
-    }
-  }
-  /* selective filter settings - only small changes */
-  if (sf<0) { 
-    for (i=0;i<nL;i++) {
-      if ( (nrhs>=2 && Bi[i]) && D[i]>bil && D[i]<bih && (abs2(D[i]-M[i])>-sf) ) M[i]=D[i];
-    }
-  }
- 
 }
 
 
