40d4e282ab37fc0a81d97ec784abe5f782e1073e
Robert Dahnke <robert.dahnke@uni-jena.de>
2021-11-30 13:40:04 +0000

    Changed paths: M       Amap.c M       CHANGES.txt M       Kmeans.c M       MrfPrior.c M       Pve.c M       cat_amap.c M       cat_amap.m M       cat_amap.mex M       cat_amap.mexmaci64 M       cat_main_amap.m M       cat_main_amap1639.m M       cat_main_kamap.m M       cat_ornlm.mex M       cat_ornlm.mexmaci64 M       cat_sanlm.mex M       cat_sanlm.mexmaci64 M       cat_vbdist.mex M       cat_vbdist.mexmaci64 M       cat_vol_ROIval.c M       cat_vol_ROIval.mexmaci64 M       cat_vol_downcut.mex M       cat_vol_downcut.mexmaci64 M       cat_vol_eidist.mex M       cat_vol_eidist.mexmaci64 D       cat_vol_eikonal3.c D       cat_vol_eikonal3.m M       cat_vol_genus0.mex M       cat_vol_genus0.mexmaci64 M       cat_vol_gradient3.mex M       cat_vol_gradient3.mexmaci64 M       cat_vol_interp3f.mex M       cat_vol_interp3f.mexmaci64 M       cat_vol_laplace3.c M       cat_vol_laplace3.mex M       cat_vol_laplace3.mexmaci64 M       cat_vol_laplace3R.mex M       cat_vol_laplace3R.mexmaci64 M       cat_vol_localstat.m M       cat_vol_localstat.mex M       cat_vol_localstat.mexmaci64 M       cat_vol_median3.c M       cat_vol_median3.mex M       cat_vol_median3.mexmaci64 M       cat_vol_median3c.c M       cat_vol_median3c.mex M       cat_vol_median3c.mexmaci64 M       cat_vol_pbtp.c M       cat_vol_pbtp.mex M       cat_vol_pbtp.mexmaci64 M       cat_vol_pbtp2.c A       cat_vol_pbtp2.m M       cat_vol_pbtp2.mexmaci64 M       cat_vol_simgrow.mex M       cat_vol_simgrow.mexmaci64 M       compile.m M       internal/cat_vol_cMRegularizarNLM3D.mex M       internal/cat_vol_cMRegularizarNLM3D.mexmaci64
    (1) Deleted:   Remove unused function cat_vol_eikonal3.c.
    (2) Fixed:     Corrected cat_vol_ROIval.c, cat_vol_pbtp2.c.
    (3) Fixed:     Corrected bugs in cat_vol_laplace3.c and
                   cat_vol_median3.c.
    (4) Fixed:     Corrected octave bugs in compile.  
    (5) Fixed:     Corrected octave issues in cat_amap.c, 
                   Amap.c, kmeans.c, MrfPrior.c, Pve.c and
                   cat_main_amap[1639].
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1914 9f44b166-36c4-483c-8295-4af200601734

--- Pve.c
+++ Pve.c
@@ -9,10 +9,9 @@
  *
  */
 
-/* This PVE calculation is a modified version from
- * the PVE software bundle:
- * Copyright (C) Jussi Tohka, Institute of Signal Processing, Tampere University of
- * Technology, 2002 - 2004.
+/* This PVE calculation is a modified version from the PVE software bundle:
+ * Copyright (C) Jussi Tohka, Institute of Signal Processing, 
+ * Tampere University of Technology, 2002 - 2004.
  * P.O. Box 553, FIN-33101, Finland
  * E-mail: jussi.tohka@tut.fi
  */
@@ -24,18 +23,18 @@
 
 void Pve5(double *src, unsigned char *prob, unsigned char *label, double *mean, int *dims)
 {
-  int x,y,z,z_area,y_dims,ind;
+  int z_area, y_dims, ind;
   double w;
   unsigned char new_val[MAX_NC];
   
   int area = dims[0]*dims[1];
-  int vol = area*dims[2];
+  int vol  = area*dims[2];
     
-  for (z = 1; z < dims[2]-1; z++) {
+  for (int z = 1; z < dims[2]-1; z++) {
     z_area = z*area;
-    for (y = 1; y < dims[1]-1; y++) {
+    for (int y = 1; y < dims[1]-1; y++) {
       y_dims = y*dims[0];
-      for (x = 1; x < dims[0]-1; x++) {
+      for (int x = 1; x < dims[0]-1; x++) {
         ind = z_area + y_dims + x;
 
         switch(label[ind]) {
@@ -48,27 +47,27 @@ void Pve5(double *src, unsigned char *prob, unsigned char *label, double *mean, 
           new_val[CSFLABEL-1] = 255;
           new_val[GMLABEL-1]  = 0;
           new_val[WMLABEL-1]  = 0;
-          label[ind] = (unsigned char) ROUND(255.0/3.0);
+          label[ind]          = (unsigned char) ROUND(1.0*255.0/3.0);
           break;
         case GMLABEL: /* GM */
           new_val[CSFLABEL-1] = 0;
           new_val[GMLABEL-1]  = 255;
           new_val[WMLABEL-1]  = 0;
-          label[ind] = (unsigned char) ROUND(2.0*255.0/3.0);
+          label[ind]          = (unsigned char) ROUND(2.0*255.0/3.0);
           break;
         case WMLABEL: /* WM */
           new_val[CSFLABEL-1] = 0;
           new_val[GMLABEL-1]  = 0;
           new_val[WMLABEL-1]  = 255;
-          label[ind] = 255;
+          label[ind]          = 255;
           break;
         case GMCSFLABEL: /* GMCSF */
-          w = (src[ind] - mean[CSFLABEL-1])/(mean[GMLABEL-1]-mean[CSFLABEL-1]);
+          w = (src[ind] - mean[CSFLABEL-1]) / ( mean[GMLABEL-1] - mean[CSFLABEL-1] );
           if(w > 1.0) w = 1.0; if(w < 0.0) w = 0.0;
           new_val[CSFLABEL-1] = (unsigned char) ROUND(255.0*(1-w));
           new_val[GMLABEL-1]  = (unsigned char) ROUND(255.0*w);
           new_val[WMLABEL-1]  = 0;
-          label[ind] = ROUND(255.0/3.0*(1.0 + w));
+          label[ind]          = (unsigned char) ROUND(255.0/3.0*(1.0 + w));
           break;
         case WMGMLABEL: /* WMGM */
           w = (src[ind] - mean[GMLABEL-1])/(mean[WMLABEL-1]-mean[GMLABEL-1]);
@@ -76,7 +75,7 @@ void Pve5(double *src, unsigned char *prob, unsigned char *label, double *mean, 
           new_val[CSFLABEL-1] = 0;
           new_val[GMLABEL-1]  = (unsigned char) ROUND(255.0*(1-w));
           new_val[WMLABEL-1]  = (unsigned char) ROUND(255.0*w);
-          label[ind] = ROUND(255.0/3.0*(2.0 + w));
+          label[ind]          = (unsigned char) ROUND(255.0/3.0*(2.0 + w));
           break;
         }
 
@@ -95,18 +94,18 @@ void Pve5(double *src, unsigned char *prob, unsigned char *label, double *mean, 
 
 void Pve6(double *src, unsigned char *prob, unsigned char *label, double *mean, int *dims)
 {
-  int x,y,z,z_area,y_dims,ind;
+  int z_area,y_dims,ind;
   double w;
   unsigned char new_val[MAX_NC];
   
   int area = dims[0]*dims[1];
   int vol = area*dims[2];
     
-  for (z = 1; z < dims[2]-1; z++) {
+  for (int z = 1; z < dims[2]-1; z++) {
     z_area = z*area;
-    for (y = 1; y < dims[1]-1; y++) {
+    for (int y = 1; y < dims[1]-1; y++) {
       y_dims = y*dims[0];
-      for (x = 1; x < dims[0]-1; x++) {
+      for (int x = 1; x < dims[0]-1; x++) {
         ind = z_area + y_dims + x;
 
         switch(label[ind]) {
@@ -119,19 +118,19 @@ void Pve6(double *src, unsigned char *prob, unsigned char *label, double *mean, 
           new_val[CSFLABEL] = 255;
           new_val[GMLABEL]  = 0;
           new_val[WMLABEL]  = 0;
-          label[ind] = (unsigned char) ROUND(255.0/3.0);
+          label[ind]        = (unsigned char) ROUND(1.0*255.0/3.0);
           break;
         case GMLABEL+1: /* GM */
           new_val[CSFLABEL] = 0;
           new_val[GMLABEL]  = 255;
           new_val[WMLABEL]  = 0;
-          label[ind] = (unsigned char) ROUND(2.0*255.0/3.0);
+          label[ind]        = (unsigned char) ROUND(2.0*255.0/3.0);
           break;
         case WMLABEL+1: /* WM */
           new_val[CSFLABEL] = 0;
           new_val[GMLABEL]  = 0;
           new_val[WMLABEL]  = 255;
-          label[ind] = 255;
+          label[ind]        = 255;
           break;
         case BKGCSFLABEL+1: /* BKGCSF */
           w = src[ind]/mean[CSFLABEL];
@@ -139,7 +138,7 @@ void Pve6(double *src, unsigned char *prob, unsigned char *label, double *mean, 
           new_val[CSFLABEL] = (unsigned char) ROUND(255.0*w);
           new_val[GMLABEL]  = 0;
           new_val[WMLABEL]  = 0;
-          label[ind] = ROUND(255.0/3.0*w);
+          label[ind]        = ROUND(255.0/3.0*w);
           break;
         case GMCSFLABEL+1: /* GMCSF */
           w = (src[ind] - mean[CSFLABEL])/(mean[GMLABEL]-mean[CSFLABEL]);
@@ -147,7 +146,7 @@ void Pve6(double *src, unsigned char *prob, unsigned char *label, double *mean, 
           new_val[CSFLABEL] = (unsigned char) ROUND(255.0*(1-w));
           new_val[GMLABEL]  = (unsigned char) ROUND(255.0*w);
           new_val[WMLABEL]  = 0;
-          label[ind] = ROUND(255.0/3.0*(1.0 + w));
+          label[ind]        = (unsigned char) ROUND(255.0/3.0*(1.0 + w));
           break;
         case WMGMLABEL+1: /* WMGM */
           w = (src[ind] - mean[GMLABEL])/(mean[WMLABEL]-mean[GMLABEL]);
@@ -155,7 +154,7 @@ void Pve6(double *src, unsigned char *prob, unsigned char *label, double *mean, 
           new_val[CSFLABEL] = 0;
           new_val[GMLABEL]  = (unsigned char) ROUND(255.0*(1-w));
           new_val[WMLABEL]  = (unsigned char) ROUND(255.0*w);
-          label[ind] = ROUND(255.0/3.0*(2.0 + w));
+          label[ind]        = (unsigned char) ROUND(255.0/3.0*(2.0 + w));
           break;
         }
 
