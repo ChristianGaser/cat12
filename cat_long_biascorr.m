11b9662152be6a6fcc646d9e09172370c8680f92
Robert Dahnke <robert.dahnke@uni-jena.de>
2022-02-02 08:11:40 +0000

    Changed paths: M       CHANGES.txt M       cat_long_biascorr.m M       cat_long_main.m MM      cat_long_report.m M       cat_main_reportfig.m M       cat_main_reportstr.m MM      cat_plot_glassbrain.m M       cat_run_job.m M       cat_run_job1639.m  M      html/cat_methods_catlongreport.html MM      html/images/cat_long_report_plasticity-aging_ADNI011S0003.jpg  M      html/images/cat_long_report_scannerdifferences_ADNI002S0559.jpg  M      html/images/catlongreportj_mwp1r.jpg
    (1) Fixed:   Corrected typos in plasticity-aging figure. 
    (2) Changed: Corrections in cat_long_biascorr to improve the longitudinal 
                 bias correction by replacing the median-based tissue threshold
                 estimation by kmeans and further filtering terms to create a 
                 smoother bias-correction field. 
                 Also added a new further internal option to remove protocol-
                 depending intensity changes by a simplified LAS approach.
                 Although both elements work, recent tests did not show the 
                 hoped for improvements in the ADNI test data. 
    (3) Fixed:   Added further setting to detect XML files and improved handling
                 of some fields in cat_long_report and cat_main_reportstr.             
    (4) Fixes:   Corrected many small display bugs in cat_main_reportfig.               
    (5) Tested:  Checked processing of longitudinal models.            
    (6) Fixed:   Corrected tiny bugs in cat_long_main.
    (7) Fixed:   Corrected settings of resolution warnings in cat_run[1639].
    (8) Fixed:   Corrected bug that avoid printing of the long report if only 
                 the aging model was used. 
                 
    
    
    git-svn-id: svn://141.35.69.214/cat12@1938 9f44b166-36c4-483c-8295-4af200601734

--- cat_long_biascorr.m
+++ cat_long_biascorr.m
@@ -19,8 +19,8 @@ function out = cat_long_biascorr(job)
 
 % ToDo:
 %  * Integration and Test of WMHs.
-%  * Strong correction (str = 0.75) caused GM overestimation and adaptation of 
-%    filter thresholds and smoothin is required. 
+%  * Strong correction (str = 0.75) caused GM overestimation and adaptation  
+%    of filter thresholds and smoothin is required. 
 %  * Iterative correction with low to high filter size
 
 % RD202010: First tests showed clear improvements of the timepoints but the
@@ -31,14 +31,36 @@ function out = cat_long_biascorr(job)
 %           whereas low correction (str = 0.25) seemed to be much better.
 
 
-  def.images  = {};
-  def.segment = {};
-  def.str     = 0.5; 
-  def.prefix  = 'm';
-  job         = cat_io_checkinopt(job,def);
+  def.images    = {};
+  def.segment   = {};
+  def.str       = 0.5; 
+  def.prefix    = 'm';
+  def.hardcore  = 0;                          % direct correction based on the T1 maps > not realy working
+  def.LASstr    = 0.5;                        % use (local) intensity normalization    > 
+  job           = cat_io_checkinopt(job,def);
+  job.str       = max(0.1,min(1,job.str)); 
   
-  if ~isfield(job,'fs') ||  isempty(job.fs),  
+  if ~isfield(job,'fs') ||  isempty(job.fs)  
     job.fs = 2^(4 * (1 - job.str));
+  end
+  
+  fprintf('  Biasstr = %0.2f, FS: %0.2f \n',job.str,job.fs);
+  
+  if job.hardcore
+    %% 202201 intensity based correction > remove if not further needed in 202301
+    %  This is just a temporary test if stronger changes based on the original 
+    %  images intens can improve the preprocessing, especially in case of 
+    %  different protocols. 
+    %  This can be seen as some very strong bias correction that changes
+    %  also contrast differences (that why we cannot smooth stonger),
+    %  whereas zero smooting would simply replace the image by the average.
+    %  Although, there are some visual improvements, the is much to simple
+    %  and an intensity normalization and more smoothing are probably
+    %  necessary to stabilize the effect. 
+    [pp,ff,ee] = spm_fileparts(job.segment{1});
+    if strcmp(pp(end-2:end),'mri'), pp = pp(1:end-3); end
+    Vavg   = spm_vol( fullfile(pp,[ff(3:end) ee]) ); 
+    Yavg   = spm_read_vols(Vavg); 
   end
   
   % load segment and estimate voxel size
@@ -50,20 +72,39 @@ function out = cat_long_biascorr(job)
   for ii = 1:numel(job.images)
     Vo = spm_vol(job.images{ii}); 
     Yo = single(spm_read_vols(Vo)); 
-   
-    % avoid edges (artifacts) in the original image
+  
+    %% avoid edges (artifacts) in the original image
     Yg   = cat_vol_grad(Yo)  ./ (1+Yo);    % normalized gradient map of the original image  
     Ygp0 = cat_vol_grad(Yp0) ./ (1+Yp0);   % normalized gradient map of the average segmentation map
     Ydiv = cat_vol_div(Yo)   ./ Yo; 
 
     
+    %% hard intensity correction (this is too simple)
+    %Ywa = cat_vol_smooth3X(Yavg ./ Yo,0); % correct addtive to adapt avg to tp 
+    if job.hardcore
+      fprintf('Hard intensity correction based on the average:\n'); 
+      Ybg = cat_vol_smooth3X( Yo ./ cat_stat_kmeans(Yo(round(Yp0)==3)) <0.5 & Yp0==0 , 4) ; 
+      Yi  = max(0.7,max(eps,Yo) ./ max(eps,Yavg)).*(1-Ybg) + Ybg;
+      Yi  = cat_vol_median3(Yi); 
+      Yw  = cat_vol_smooth3X(Yi, 2 ); 
+
+      % quantify difference
+      Ygow = cat_vol_grad(Yo./Yw)  ./ (1+(Yo./Yw));
+      fprintf('  Yg overlap after hard corection: %0.4f\n',cat_stat_nanmean(abs(Ygow(Yp0(:)>0) - Yg(Yp0(:)>0)))); 
+
+      Yo = Yo ./ Yw; 
+    end
+    
+    
     %% intensity normalization 
-    T3th  = [ min(Yo(Yp0(:)>0.5 & Yg(:)<0.2)) ...
-              cat_stat_nanmedian(Yo(round(Yp0)==1 & Yg<0.2)) cat_stat_nanmedian(Yo(round(Yp0)==2 & Yg<0.2)) cat_stat_nanmedian(Yo(round(Yp0)==3 & Yg<0.2)) ...
+    T3th  = [ min(Yo(:)) min(Yo(Yp0(:)>0.5 & Yg(:)<0.2))+eps ...
+              cat_stat_kmeans(Yo(round(Yp0)==1 & Yg<0.2)) ...
+              cat_stat_kmeans(Yo(round(Yp0)==2 & Yg<0.2)) ...
+              cat_stat_kmeans(Yo(round(Yp0)==3 & Yg<0.2)) ...
               max(Yo(Yp0>0  & Yg<0.2))];
-    T3thx = [ min(Yo(Yp0(:)>0.5 & Yg(:)<0.2))/cat_stat_nanmedian(Yo(round(Yp0)==1 & Yg<0.2)) ...
+    T3thx = [ 0 min(Yo(Yp0(:)>0.5 & Yg(:)<0.2))/cat_stat_kmeans(Yo(round(Yp0)==1 & Yg<0.2))+eps ...
               1 2 3 ...
-              max(Yo(Yp0(:)>0.5 & Yg(:)<0.2))/cat_stat_nanmedian(Yo(round(Yp0)==3  & Yg<0.2))*3 ];
+              max(Yo(Yp0(:)>0.5 & Yg(:)<0.2))/cat_stat_kmeans(Yo(round(Yp0)==3  & Yg<0.2))*3 ];
 
     % intensity scaling
     Ym = Yo; 
@@ -84,31 +125,79 @@ function out = cat_long_biascorr(job)
 
     %% extract save segment
     gstr = 2^( 2 * (job.str - 0.5) ); 
-    Ywm = abs(Ydiv)<0.2 & Yd<0.2 & Yg<0.3 * gstr & Ygp0<0.3 & Yp0>2.8 & cat_vol_morph(Yp0>2,'e');
-    Ygm = abs(Ydiv)<0.2 & Yd<0.2 & Yg<0.6 * gstr & Ygp0<0.6 & Yp0>1.2 & Yp0<2.8; % & cat_vol_morph(Yp0>1,'e') & cat_vol_morph(Yp0<3,'e'); 
-    Ycm = abs(Ydiv)<0.2 & Yd<0.2 & Yg<1.2 * gstr & Ygp0<1.2 & Yp0>0.8 & Yp0<1.2 & cat_vol_morph(Yp0>0,'e',3,vx_vol) & cat_vol_morph(Yp0<2,'de',3,vx_vol);
+    Ywm = abs(Ydiv)<0.2 & Yd<0.2 & Yg<0.6 * gstr & Ygp0<0.3 & Yp0>2.5 & cat_vol_morph(Yp0>2,'e');
+    Ygm = abs(Ydiv)<0.2 & Yd<0.2 & Yg<0.6 * gstr & Ygp0<0.6 & Yp0>1.5 & Yp0<2.5; % & cat_vol_morph(Yp0>1,'e') & cat_vol_morph(Yp0<3,'e'); 
+    Ycm = abs(Ydiv)<0.2 & Yd<0.2 & Yg<0.6 * gstr & Ygp0<1.2 & Yp0>0.5 & Yp0<1.5 & cat_vol_morph(Yp0>0,'e',3,vx_vol) & cat_vol_morph(Yp0<2,'de',3,vx_vol);
     Ycm(smooth3(Ycm)<0.8) = 0; 
-    Ybg = single(cat_vol_morph(Yp0==0,'de',10,vx_vol)); 
+    Ybg = Ym<0.5 & single(cat_vol_morph(Yp0==0,'de',40,vx_vol)); 
     %clear Yg; 
-
+    
     
     %% get values from segment
-    Yi = Ybg + Yo .* Ywm ./ cat_stat_nanmedian(Yo(Ywm(:)));
-    Yi = Yi  + Yo .* Ygm ./ cat_stat_nanmedian(Yo(Ygm(:)));
-    Yi = Yi  + Yo .* Ycm ./ cat_stat_nanmedian(Yo(Ycm(:)));
+    Yi = Ybg + Yo .* Ywm ./ cat_stat_kmeans(Yo(Ywm(:)));
+    Yi = Yi  + Yo .* Ygm ./ cat_stat_kmeans(Yo(Ygm(:)));
+    Yi = Yi  + Yo .* Ycm ./ cat_stat_kmeans(Yo(Ycm(:)));
+    % remove outlier 
     Yi2 = Yi; Yi2( smooth3(Yi2)<0.8 ) = 0;  
-    Yw = cat_vol_approx(Yi2,'nh',vx_vol,4); 
-    Yi( abs(Yi-Yw)>0.4 * job.str ) = 0; 
-
+    Yi2 = cat_vol_localstat(Yi2,Yi2~=0,1,1,round(2 ./ mean(vx_vol)));
+    Yw  = cat_vol_approx(Yi2,'nh',vx_vol,4); 
+    Yi( abs(Yi-Yw)>0.2 * max(0.5,job.str) ) = 0; 
+    % remove background
+    Yi(Ybg) = 0; 
+    Yi = cat_vol_median3(Yi,Yi~=1 & Yi~=0,Yi~=1 & Yi~=0);
+    Yi = cat_vol_localstat(Yi,Yi~=0,1,1,round(10 ./ mean(vx_vol)));
+    % approximate bias field
+    Yw = cat_vol_approx(Yi,'nh',vx_vol,job.fs * 4); % overcorrection of subcortical structures
     
-    %% approximate bias field
-    Yw = cat_vol_approx(Yi,'nh',vx_vol,job.fs ./ vx_vol); 
-
     
-    % write corrected output
+    
+    %% local intensity correction based on the average segmentation 
+    %  The priciple idea is to do some further corrections in case of
+    %  changed protocols (indicated by the user). The hope is that this
+    %  reduce the bias like local differences related to contrast
+    %  differences. 
+    %  For test the ADNI 1.5 and 3.0 Tesla scans with 100 days differences 
+    %  and normal longitudinal scans with 1 and 2 years differences are used, 
+    %  where the correction should result in a more likely aging pattern in 
+    %  the 100-days rescans and not worse (quite similar) changes within the 
+    %  normal longitudinal images. 
+    if job.LASstr
+      
+      % apply bias correction 
+      Ysrc = Yo ./ Yw; 
+     
+      % estimate threshholds
+      T3th2 = zeros(1,3); 
+      for ti=1:3, T3th2(ti) = cat_stat_kmeans(Ysrc(round(Yp0)==ti & Yg<0.3)); end
+    
+      % the most important segment is the GM 
+      Ygm2    = Ygp0<0.6 & Yp0>1.5 & Yp0<2.5 & Yg<0.6; 
+      Yi      = Ysrc .* Ygm2;
+      Yi      = cat_vol_median3(Yi,Yi~=1 & Yi~=0,Yi~=1 & Yi~=0);
+      Yi      = cat_vol_localstat(Yi,Yi~=0,1,1,round(20 ./ mean(vx_vol)));
+      Ylab{1} = cat_vol_approx(Yi,'nh',vx_vol,job.fs * 4 / job.LASstr); 
+      % for all other segments we just use the global values
+      Ylab{2} = T3th2(3);
+      Ylab{3} = T3th2(1); 
+      Ylab{6} = min(Yo); % ####### inoptimal#
+
+      % intensity normalization 
+      Yml = zeros(size(Ysrc)); 
+      Yml = Yml + ( (Ysrc>=Ylab{2}                ) .* (3 + (Ysrc - Ylab{2}) ./ max(eps,Ylab{2} - Ylab{3})) );
+      Yml = Yml + ( (Ysrc>=Ylab{1} & Ysrc<Ylab{2} ) .* (2 + (Ysrc - Ylab{1}) ./ max(eps,Ylab{2} - Ylab{1})) );
+      Yml = Yml + ( (Ysrc>=Ylab{3} & Ysrc<Ylab{1} ) .* (1 + (Ysrc - Ylab{3}) ./ max(eps,Ylab{1} - Ylab{3})) );
+      Yml = Yml + ( (Ysrc< Ylab{3}                ) .* (    (Ysrc - Ylab{6}) ./ max(eps,Ylab{3} - Ylab{6})) );
+      Yml(isnan(Yml) | Yml<0)=0; Yml(Yml>10)=10;
+    end
+    
+    
+    %% write corrected output
     out.bc{ii} = spm_file(Vo.fname,'prefix',job.prefix); 
     Vw = Vo; Vw.fname = out.bc{ii};
-    spm_write_vol(Vw,Yo ./ Yw); 
-
+    if job.LASstr
+      spm_write_vol(Vw,Yml);
+    else
+      spm_write_vol(Vw,Yo ./ Yw);
+    end
   end
 end
