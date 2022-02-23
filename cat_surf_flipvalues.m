d428a5b64116f9f69c55832fcf780f79801de15f
Christian Gaser <christian.gaser@uni-jena.de>
2022-02-23 15:42:07 +0000

    Changed paths: M       CHANGES.txt A       cat_surf_flipvalues.m A       cat_surf_laterality_index.m M       cat_surf_rename.m M       cat_vol_morph.m
    (1) Changed: Added wrapper to interactively call vat_vol_morph.m.
    (2) Fixed:   Corrected handling of 32k-meshes in cat_surf_rename.m 
    (3) Added:   New function cat_surf_flipvalues to flip values in resampled
                 32k-meshes.
    (4) Added:   New function cat_surf_laterality_index to calculate laterality
                 index in resampled 32k-meshes.
    
    
    
    git-svn-id: svn://141.35.69.214/cat12@1952 9f44b166-36c4-483c-8295-4af200601734

---
+++ cat_surf_flipvalues.m
@@ -0,0 +1,43 @@
+function cat_surf_flipvalues(P)
+% ______________________________________________________________________
+% Mirror values in 32k-meshes to the opposite hemisphere
+%
+% This function allows to flip the resampled data of symmetrical 32k-meshes.
+% The flipped mesh is indicated with a prepended 'flip_' in the dataname
+% of the file.
+% ______________________________________________________________________
+%
+% Christian Gaser, Robert Dahnke
+% Structural Brain Mapping Group (http://www.neuro.uni-jena.de)
+% Departments of Neurology and Psychiatry
+% Jena University Hospital
+% ______________________________________________________________________
+% $Id: cat_surf_flipvalues.m 1901 2021-10-26 10:25:52Z gaser $
+
+if ~nargin
+  P = spm_select(Inf,'mesh','select 32k-meshes to flip',{},pwd,'mesh.*.resampled_32k');
+end
+
+n = size(P,1);
+for i=1:n
+  mesh_name = deblank(P(i,:));
+  M = gifti(mesh_name);
+    
+  % check that values are in the mesh and the data size is that of a 32k mesh
+  if isfield(M,'cdata') && numel(M.cdata) ~= 64984
+    fprintf('ERROR: %s does not contain resampled 32k-mesh values.\n',mesh_name);
+    break
+  end  
+    
+  % flip values
+  cdata1 = M.cdata(1:32492);
+  cdata2 = M.cdata(32493:64984);
+  M.cdata = [cdata2; cdata1];
+  
+  % rename dataname
+  sinfo = cat_surf_info(mesh_name);
+  flipped_name = char(cat_surf_rename(mesh_name,'dataname',['flipped_' sinfo.dataname]));
+  
+  save(M, flipped_name, 'Base64Binary');
+  fprintf('Save flipped file %s\n',flipped_name);
+end
