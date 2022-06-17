4baed504eed6d20b7932ecb5bd840904ad19c7f2
Christian Gaser <christian.gaser@uni-jena.de>
2022-06-17 12:56:48 +0000

    Changed paths: M       CHANGES.txt M       Contents.m M       html/cat.html M       html/cat.txt M       html/cat_references.html
    (1) Changed: Updated CAT12 references.
    
    
    git-svn-id: svn://141.35.69.214/cat12@2000 9f44b166-36c4-483c-8295-4af200601734

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 1992 (CAT12.8.1) 2022-05-13
+% Version 1999 (CAT12.8.1) 2022-06-17
 % ______________________________________________________________________
 %
 % Christian Gaser, Robert Dahnke
