ef612e0b3bda1dda6d6e54b3d9671042e0d08c50
Christian Gaser <christian.gaser@uni-jena.de>
2022-07-13 00:20:22 +0200

    Prepared adding of revision and date to m-files before zipping.

---
+++ update_revision.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# Tool for adding revision and date to m-files that were prepared in cat12/cat12.
+# ______________________________________________________________________
+#
+# Christian Gaser, Robert Dahnke
+# Structural Brain Mapping Group (https://neuro-jena.github.io)
+# Departments of Neurology and Psychiatry
+# Jena University Hospital
+# ______________________________________________________________________
+%
+
+REVISION=`git rev-list --count HEAD`
+DATE=`git log --date short |grep "Date:"|head -1|cut -f2 -d':'|sed -e s'/ //g'`
+
+perl -p -i -e 's/\$Id\$/\$Id: '$REVISION' '$DATE' \$/g' cat12/*.m
+perl -p -i -e 's/\$Id\$/\$Id: '$REVISION' '$DATE' \$/g' cat12/*.sh
