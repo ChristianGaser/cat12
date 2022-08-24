1e30fc40298cdfc423fee2dada3a579075bd252d
Christian Gaser <christian.gaser@uni-jena.de>
2022-08-24 15:19:37 +0200

    Changed: Force to use PET modality in CAT12 to avoid problems of very low          variance in spm_spm.m. Changed: Renamed cat12-html to cat12-help. Changed: Mimimized loading of mat-file in cat_vol_galssbrain.m. Changed: Added check for result of CAT_SeparatePolygon in cat_surf_createCS2.m          because sometimes CAT_SeparatePolygon fails and we have to use the          original raw file. Fixed:   Added cbar-flag to defaults in cat_vol_img2mip.m.
    Changed paths:
     M CHANGES.txt
     M Makefile
     M cat_surf_createCS2.m
     M cat_vol_glassbrain.m
     M cat_vol_img2mip.m
     M spm_cat12.m

--- cat_vol_glassbrain.m
+++ cat_vol_glassbrain.m
@@ -9,7 +9,7 @@ function fig = cat_vol_glassbrain(X,pos,varargin)
 %   S.dark          - dark mode                    - Default: false
 %   S.detail        - glass brain detail level:
 %                     0=LOW, 1=NORMAL, 2=HIGH      - Default: 1
-%   S.grid          - overlay grid                 - Default false
+%   S.grid          - overlay grid                 - Default: false
 % Output:
 %   fig             - Handle for generated figure
 %__________________________________________________________________________
@@ -28,22 +28,22 @@ function fig = cat_vol_glassbrain(X,pos,varargin)
 % $Id$
 
 switch nargin
-    case 1
-        error('need at least two arguments, values, and positions!')
-    case 2
-        S = [];
-    case 3
-        S = varargin{1};
+  case 1
+    error('need at least two arguments, values, and positions!')
+  case 2
+    S = [];
+  case 3
+    S = varargin{1};
 end
 
 assert(length(X) == length(pos), ['number of values do not match '...
-    'number of positions!']);
+  'number of positions!']);
 
-if ~isfield(S, 'dark'),         S.dark = false; end
-if ~isfield(S, 'cmap'),         S.cmap = 'gray'; end
-if ~isfield(S, 'detail'),       S.detail = 1; end
-if ~isfield(S, 'grid'),         S.grid = false; end
-if ~isfield(S, 'colourbar'),    S.colourbar = false; end
+if ~isfield(S, 'dark'),     S.dark = false; end
+if ~isfield(S, 'cmap'),     S.cmap = 'gray'; end
+if ~isfield(S, 'detail'),   S.detail = 1; end
+if ~isfield(S, 'grid'),     S.grid = false; end
+if ~isfield(S, 'colourbar'),S.colourbar = false; end
 
 M = [-2 0 0 92;0 2 0 -128;0 0 2 -74;0 0 0 1];
 dim = [91 109 91];
@@ -52,21 +52,21 @@ pos = ceil(M \ [pos';ones(1,size(pos,1))])';
 % exclude positions that exceed image dimensions
 ind = find(pos(:,1) < 1 | pos(:,1) > dim(1) | pos(:,2) < 1 | pos(:,2) > dim(2) | pos(:,3) < 1 | pos(:,3) > dim(3));
 if ~isempty(ind)
-    pos(ind,:) = [];
-    X(ind) = [];
+  pos(ind,:) = [];
+  X(ind) = [];
 end
 
 if any(X<0) && any(X>0)
-    div = 1;
+  div = 1;
 else
-    div = 0;
+  div = 0;
 end
 
 [~,id] = sort(abs(X),'ascend');
 if div
-    [~,bin] = histc(X,linspace(-max(abs(X)),max(abs(X)),65));
+  [~,bin] = histc(X,linspace(-max(abs(X)),max(abs(X)),65));
 else
-    [~,bin] = histc(X,linspace(min(abs(X)),max(abs(X)),65));
+  [~,bin] = histc(X,linspace(min(abs(X)),max(abs(X)),65));
 end
 
 % saggital plane
@@ -74,14 +74,14 @@ end
 p_sag = NaN(dim(2),dim(3));
 
 for ii = 1:length(id)
-        
-    p1 = pos(id(ii),2);
-    p2 = pos(id(ii),3);
     
-    if p1 > 0 && p1 <= dim(2) && p2 > 0 && p2 <= dim(3)
-      p_sag(p1,p2) = bin(id(ii));
-    end
-    
+  p1 = pos(id(ii),2);
+  p2 = pos(id(ii),3);
+  
+  if p1 > 0 && p1 <= dim(2) && p2 > 0 && p2 <= dim(3)
+    p_sag(p1,p2) = bin(id(ii));
+  end
+  
 end
 
 % coronal plane
@@ -89,13 +89,13 @@ end
 p_cor = NaN(dim(1),dim(3));
 
 for ii = 1:length(id)
-        
-    p1 = pos(id(ii),1);
-    p2 = pos(id(ii),3);
     
-    if p1 > 0 && p1 <= dim(1) && p2 > 0 && p2 <= dim(3)
-      p_cor(p1,p2) = bin(id(ii));
-    end  
+  p1 = pos(id(ii),1);
+  p2 = pos(id(ii),3);
+  
+  if p1 > 0 && p1 <= dim(1) && p2 > 0 && p2 <= dim(3)
+    p_cor(p1,p2) = bin(id(ii));
+  end  
 end
 
 
@@ -104,76 +104,79 @@ end
 p_axi = NaN(dim(2),dim(1));
 
 for ii = 1:length(id)
-        
-    p1 = pos(id(ii),2);
-    p2 = pos(id(ii),1);
     
-    if p1 > 0 && p1 <= dim(2) && p2 > 0 && p2 <= dim(1)
-      p_axi(p1,p2) = bin(id(ii));
-    end
+  p1 = pos(id(ii),2);
+  p2 = pos(id(ii),1);
+  
+  if p1 > 0 && p1 <= dim(2) && p2 > 0 && p2 <= dim(1)
+    p_axi(p1,p2) = bin(id(ii));
+  end
 end
 
 % optional colorbar
 %---------------------------------------------------------------------
 p_col = NaN(dim(1),dim(3));
 if S.colourbar
-    for ii = 42:52
-        p_col(ii,14:78) = linspace(2,65,numel(14:78));
-    end
-    if div
-        rmin = -max(abs(X));
-    else
-        rmin = min(abs(X));
-    end
-    rmax = max(abs(X));
+  for ii = 42:52
+    p_col(ii,14:78) = linspace(2,65,numel(14:78));
+  end
+  if div
+    rmin = -max(abs(X));
+  else
+    rmin = min(abs(X));
+  end
+  rmax = max(abs(X));
 end
 
 
 % combine and plot
 %---------------------------------------------------------------------
 p_all = [rot90(p_sag,1) fliplr(rot90(p_cor,1));...
-    rot90(p_axi,1) rot90(p_col,1)];
+rot90(p_axi,1) rot90(p_col,1)];
 p_all(isnan(p_all)) = 0;
 imagesc(p_all)
 set(gca,'XTickLabel',{},'YTickLabel',{});
 axis image
 
 caxis([0 64])
-overlay_glass_brain('side',S.dark,S.detail);
-overlay_glass_brain('back',S.dark,S.detail);
-overlay_glass_brain('top',S.dark,S.detail);
+load(fullfile(fileparts(mfilename('fullpath')),'glass_brain.mat'));
+overlay_glass_brain(glass,'side',S.dark,S.detail);
+overlay_glass_brain(glass,'back',S.dark,S.detail);
+overlay_glass_brain(glass,'top', S.dark,S.detail);
 
 if ischar(S.cmap)
   c = feval(S.cmap,64);
 else
   c = S.cmap;
 end
+
 if S.dark
-    c(1,:) = [0 0 0];
+  c(1,:) = [0 0 0];
 else
-    c(1,:) = [1 1 1];
+  c(1,:) = [1 1 1];
 end
 colormap(c);
+
 if S.dark
-    set(gcf,'color','k');
+  set(gcf,'color','k');
 else
-    set(gcf,'color','w');
+  set(gcf,'color','w');
 end
 
 if S.colourbar
-    if rmin < 0
-      text(175,170,'-max','color',~c(1,:),'fontsize',12,'horizontalalignment','center');
-    else
-      text(175,170,'min','color',~c(1,:),'fontsize',12,'horizontalalignment','center');
-    end
-      
-    text(175,105,'max','color',~c(1,:),'fontsize',12,'horizontalalignment','center');
+  if rmin < 0
+    text(175,170,'-max','color',~c(1,:),'fontsize',12,'horizontalalignment','center');
+  else
+    text(175,170,'min','color',~c(1,:),'fontsize',12,'horizontalalignment','center');
+  end
+    
+  text(175,105,'max','color',~c(1,:),'fontsize',12,'horizontalalignment','center');
 end
 
 if S.grid
-    grid on
+  grid on
 else
-    axis off
+  axis off
 end
 
 fig = gcf;
@@ -183,46 +186,44 @@ end
 % supporting functions
 %---------------------------------------------------------------------
 
-function overlay_glass_brain(orient,dark,detail)
-
-load(fullfile(fileparts(mfilename('fullpath')),'glass_brain.mat'));
+function overlay_glass_brain(glass,orient,dark,detail)
 
 dat = glass.(orient);
 
 switch orient
-    case 'top'
-        xform = [0 -1 0; 1 0 0; 0 0 1]*[0.185 0 0; 0 0.185 0; 10.5 173 1];
-    case 'back'
-        xform = [0.185 0 0; 0 -0.185 0; 120 89 1];
-    case 'side'
-        xform = [0.185 0 0; 0 -0.185 0; 10.5 89 1];
+  case 'top'
+    xform = [0 -1 0; 1 0 0; 0 0 1]*[0.185 0 0; 0 0.185 0; 10.5 173 1];
+  case 'back'
+    xform = [0.185 0 0; 0 -0.185 0; 120 89 1];
+  case 'side'
+    xform = [0.185 0 0; 0 -0.185 0; 10.5 89 1];
 end
 
 for ii = 1:length(dat.paths)
-    pth = dat.paths(ii);
-    % see if we need to draw based on the complexity option
-    switch detail
-        case 0
-            draw = pth.linewidth > 1 & sum(hex2rgb(pth.edgecolor))==0;
-        case 1
-            draw = sum(hex2rgb(pth.edgecolor))==0;
-        otherwise
-            draw = 1;
+  pth = dat.paths(ii);
+  % see if we need to draw based on the complexity option
+  switch detail
+    case 0
+      draw = pth.linewidth > 1 & sum(hex2rgb(pth.edgecolor))==0;
+    case 1
+      draw = sum(hex2rgb(pth.edgecolor))==0;
+    otherwise
+      draw = 1;
+  end
+  
+  if draw
+    for jj = 1:length(pth.items)
+      pts = pth.items(jj).pts;
+      v = [generate_bezier(pts) ones(10,1)];
+      v2 = v*xform;
+      if dark
+        c = 1 - hex2rgb(pth.edgecolor);
+      else
+        c = hex2rgb(pth.edgecolor);
+      end
+      line(v2(:,1),v2(:,2),'LineWidth',pth.linewidth,'Color',c);
     end
-    
-    if draw
-        for jj = 1:length(pth.items)
-            pts = pth.items(jj).pts;
-            v = [generate_bezier(pts) ones(10,1)];
-            v2 = v*xform;
-            if dark
-                c = 1 - hex2rgb(pth.edgecolor);
-            else
-                c = hex2rgb(pth.edgecolor);
-            end
-            line(v2(:,1),v2(:,2),'LineWidth',pth.linewidth,'Color',c);
-        end
-    end
+  end
 end
 
 end
@@ -235,9 +236,9 @@ function [points, t] = generate_bezier(controlPts, varargin)
 
 % estimate nDrawPoints
 if nargin == 1
-    nCurvePoints = 10;
+  nCurvePoints = 10;
 else
-    nCurvePoints = varargin{1};
+  nCurvePoints = varargin{1};
 end
 
 % curve parametrization variable
@@ -246,26 +247,26 @@ t = linspace(0, 1, nCurvePoints)';
 % detect the type of curve (linear, quadratic, cubic) based on the
 % number of points given in controlPts.
 switch size(controlPts, 1)
-    case 1
-        error('Number of Control Points should be at least 2');
-        
-    case 2
-        % linear formula
-        points = (1 - t) * controlPts(1, :) + ...
-            t * controlPts(2, :);
-        
-    case 3
-        % quadratic formula
-        points = ((1 - t) .^ 2) * controlPts(1, :) + ...
-            (2 * (1 - t) .* t) * controlPts(2, :) + ...
-            (t .^ 2) * controlPts(3, :);
-        
-    case 4
-        % cubic formula
-        points =  ((1 - t) .^ 3) * controlPts(1, :) + ...
-            (3 * (1 - t) .^ 2 .* t) * controlPts(2, :) + ...
-            (3 * (1 - t) .* t .^ 2) * controlPts(3, :) + ...
-            (t .^ 3) * controlPts(4, :);
+  case 1
+    error('Number of Control Points should be at least 2');
+    
+  case 2
+    % linear formula
+    points = (1 - t) * controlPts(1, :) + ...
+      t * controlPts(2, :);
+    
+  case 3
+    % quadratic formula
+    points = ((1 - t) .^ 2) * controlPts(1, :) + ...
+      (2 * (1 - t) .* t) * controlPts(2, :) + ...
+      (t .^ 2) * controlPts(3, :);
+    
+  case 4
+    % cubic formula
+    points =  ((1 - t) .^ 3) * controlPts(1, :) + ...
+      (3 * (1 - t) .^ 2 .* t) * controlPts(2, :) + ...
+      (3 * (1 - t) .* t .^ 2) * controlPts(3, :) + ...
+      (t .^ 3) * controlPts(4, :);
 end
 
 % verify dimensions
@@ -275,7 +276,7 @@ end
 function rgb = hex2rgb(hex)
 % converts hex string to matlab rgb triplet
 if strcmpi(hex(1,1),'#')
-    hex(:,1) = [];
+  hex(:,1) = [];
 end
 rgb = reshape(sscanf(hex.','%2x'),3,[]).'/255;
 end
