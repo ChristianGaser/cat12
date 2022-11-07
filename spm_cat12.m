459a5dbfa3333dd8e44a764f6185ef480cc30d9f
Christian Gaser <christian.gaser@uni-jena.de>
2022-11-07 22:44:40 +0100

    Changed: Many changes to implement the new html help. Changed: Updated Makefile to consider new html help. Changed: Updated and moved cat.txt from html to doc folder. Added:   Added css style file for entry page.
    Changed paths:
     M CHANGES.txt
     M Contents.m
     M Makefile
     M cat12.m
     M cat_install_tfce.m
     M cat_update.m
     M doc/cat.html
    AM doc/cat.txt
     M doc/css/main.css
     M doc/index.html
     M spm_cat12.m

--- spm_cat12.m
+++ spm_cat12.m
@@ -259,12 +259,14 @@ end
 
 spm('FnBanner',mfilename,cat_version);
 [Finter,Fgraph] = spm('FnUIsetup','CAT12.8.1');
-url = fullfile(fileparts(mfilename('fullpath')),'html','cat.html');
-spm_help('!Disp',url,'',Fgraph,'Computational Anatomy Toolbox for SPM12');
+url = fullfile(fileparts(mfilename('fullpath')),'doc','cat.html');
 
 % open interactive help for newer version because display of html pages does not work anymore
 if cat_io_matlabversion > 20212
-  web(fullfile(catdir,'html','cat.html'));
+  pos = Fgraph.Position(3:4);
+  web(url,'-noaddressbox','-new')
+else
+  spm_help('!Disp',url,'',Fgraph,'Computational Anatomy Toolbox for SPM12');
 end
 
 % check that binaries for surface tools are running 
