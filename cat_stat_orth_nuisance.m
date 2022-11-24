96a7873c35f894e559a381dc36a7e860e444474d
Christian Gaser <christian.gaser@uni-jena.de>
2022-11-24 17:13:46 +0100

    Added:   New function cat_stat_orth_nuisance.m to orthogonalize nuisance          parameters w.r.t. covariate of interest. Changed: Added additional trial to change file permissions of cat_system fails.
    Changed paths:
     M CHANGES.txt
    A  cat_stat_orth_nuisance.m
     M cat_system.m

---
+++ cat_stat_orth_nuisance.m
@@ -0,0 +1,49 @@
+function G_orth = cat_stat_orth_nuisance(C,G)
+% Orthogonalization of nuisance parameters G w.r.t C
+% FORMAT G_orth = cat_stat_orth_nuisance(C,G)
+% C      - vector of covariate parameter
+% G      - matrix of nuisance parameter
+% 
+% G_orth - orthogonalized nuisance parameter
+%
+% ______________________________________________________________________
+%
+% Christian Gaser, Robert Dahnke
+% Structural Brain Mapping Group (https://neuro-jena.github.io)
+% Departments of Neurology and Psychiatry
+% Jena University Hospital
+% ______________________________________________________________________
+% $Id$ 
+
+if nargin < 2
+  fprintf('Syntax: G = cat_stat_orth_nuisance(C,G)\n');
+  G = [];
+  return
+end
+
+if size(C,2) > size(C,1)
+  C = C';
+  G = G';
+  trans = 1;
+else
+  trans = 0;
+end
+
+if size(C,1) ~= size(G,1)
+  fprintf('Parameters C and G have different length: %d vs %d\n',size(C,1),size(G,1));
+  G = [];
+  return
+end
+
+if size(C,2) ~= 1
+  fprintf('Parameters C must have only one column: %d \n',size(C,2));
+  G = [];
+  return
+end
+
+C2 = [ones(size(C)) C];
+G_orth = G - C2*(pinv(C2)*G);
+
+if trans
+  G_orth = G_orth';
+end
