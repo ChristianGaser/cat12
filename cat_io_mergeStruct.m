eb98604719836fb90e6f27aea16962c7471d8f81
Robert <52195853+robdahn@users.noreply.github.com>
2023-04-27 16:19:09 +0200

    Fixed:    Reactivated multi_subject_imcalc for developers in cat_conf_tools. Fixed:    Corrections in cat_io_mergeStruct and cat_io_updateStruct. Fixed:    Added CS3 surface pipeline case in cat_main. Fixed:    Added missing variable for SPM-CAT pipeline in cat_main[1639]. Fixed:    Corrected fixed TPM loop number in cat_main_updateSPM1639. Fixed:    Corrected typos in CHANGES.txt Changed:  Be verbose in special longitudinal background model case in           cat_run_job1639 (new_release only). Changed paths:  M CHANGES.txt AD batches/OASIS_bones_CT2MRI.m AD bones_seg.m  M cat_conf_tools.m  M cat_io_mergeStruct.m  M cat_io_updateStruct.m  M cat_main.m  M cat_main1639.m  M cat_main_updateSPM.m  M cat_main_updateSPM1639.m  M cat_run.m  M cat_run_job1639.m AD templates_surfaces/bh.brain.human.TPM.gii AD templates_surfaces/bh.headbrain.human.TPM.gii

--- cat_io_mergeStruct.m
+++ cat_io_mergeStruct.m
@@ -1,8 +1,10 @@
-function S=cat_io_mergeStruct(S,SN,ri)
+function S=cat_io_mergeStruct(S,SN,ri,id)
 % _________________________________________________________________________
-% Merge to structures 'S' and 'SN'.
+% Merge to structures 'S' and 'SN'. 
 %
-%   S = cat_io_mergeStruct(S,SN)
+%   S = cat_io_mergeStruct(S,SN[,[],id])
+% 
+%   where id>0 updates elements S(id)  
 %   
 % WARNING:
 %   This function is still in developent! Be careful by using it, due to
@@ -20,56 +22,72 @@ function S=cat_io_mergeStruct(S,SN,ri)
 
   % check input
   maxri = 20; 
-  if ~exist('ri','var'), ri=0; end
+  if ~exist('id','var') || isempty(id), id=0; end
+  if ~exist('ri','var') || isempty(ri), ri=0; end
   
   SFN  = fieldnames(S);
   SNFN = fieldnames(SN);
 
+  %% add new empty fields in additional element in S
   NSFN = setdiff(SNFN,SFN);
-  for ni = 1:numel(NSFN)
-   if isnumeric(SN(1).(NSFN{ni}))
-     S(1).(NSFN{ni}) = [];
-   elseif ischar(SN(1).(NSFN{ni}))
-     S(1).(NSFN{ni}) = '';
-   elseif isstruct(SN(1).(NSFN{ni}))
-     if ri<maxri
-       Stmp = cat_io_mergeStruct(struct(),SN(1).(NSFN{ni})(1),ri+1);
-     else
-        Stmp = struct(); 
-     end
-     S(1).(NSFN{ni}) = Stmp(1);
-   elseif iscell(SN(1).(NSFN{ni}))
-     S(1).(NSFN{ni}) = {};
-   end
-  end
-
-  NSSFN = setdiff(SFN,SNFN);
-  for ni = 1:numel(NSSFN)
-    if isnumeric(S(1).(NSSFN{ni}))
-      SN(1).(NSSFN{ni}) = [];
-    elseif ischar(S(1).(NSFN{ni}))
-      SN(1).(NSSFN{ni}) = '';
-    elseif isstruct(S(1).(NSSFN{ni}))
-      if ri<maxri
-        Stmp = cat_io_mergeStruct(struct(),S(1).(NSSFN{ni})(1),ri+1);
-      else
-        Stmp = struct(); 
+  if ~isempty(NSFN)
+    ne   = numel(S)+1; 
+    for ni = 1:numel(NSFN)
+      if isnumeric(SN(1).(NSFN{ni})) 
+        S(ne).(NSFN{ni}) = [];
+      elseif islogical(SN(1).(NSFN{ni}))
+        S(ne).(NSFN{ni}) = false; 
+      elseif ischar(SN(1).(NSFN{ni}))
+        S(ne).(NSFN{ni}) = '';
+      elseif isstruct(SN(1).(NSFN{ni}))
+        if ri<maxri
+          Stmp = cat_io_mergeStruct(struct(),SN(1).(NSFN{ni})(1),ri+1);
+        else
+          Stmp = struct(); 
+        end
+        S(ne).(NSFN{ni}) = Stmp(1);
+      elseif iscell(SN(1).(NSFN{ni}))
+        S(ne).(NSFN{ni}) = {};
       end
-      SN(1).(NSSFN{ni}) = Stmp(1);
-    elseif iscell(SN(1).(NSSFN{ni}))
-      SN(1).(NSSFN{ni}) = {};
     end
+    S(ne) = []; 
   end
 
-  S = orderfields(S);
-  SN = orderfields(SN);
+  %%
+  NSSFN = setdiff(SFN,SNFN);
+  if ~isempty(NSSFN)
+    sne   = numel(SN) + 1; 
+    for ni = 1:numel(NSSFN)
+      if isnumeric(S(1).(NSSFN{ni})) 
+        SN(sne).(NSSFN{ni}) = [];
+      elseif islogical(S(1).(NSSFN{ni}))
+        SN(sne).(NSSFN{ni}) = false(0);
+      elseif ischar(S(1).(NSSFN{ni}))
+        SN(sne).(NSSFN{ni}) = '';
+      elseif isstruct(S(1).(NSSFN{ni}))
+        if ri<maxri
+          Stmp = cat_io_mergeStruct(struct(),S(1).(NSSFN{ni})(1),ri+1);
+        else
+          Stmp = struct(); 
+        end
+        SN(sne).(NSSFN{ni}) = Stmp(1);
+      elseif iscell(S(1).(NSSFN{ni}))
+        SN(sne).(NSSFN{ni}) = {};
+      end
+    end
+    SN(sne) = [];
+  end
+
+  %%
+  S   = orderfields(S);
+  SN  = orderfields(SN);
   
-  ns = numel(S);
+  ns  = numel(S); 
   for sni=1:numel(SN)
-    try
+    if id==0
       S(ns + sni) = SN(sni);
-    catch
-      1==1;
+    else
+      S(id + sni - 1) = SN(sni);
     end
   end
 end
