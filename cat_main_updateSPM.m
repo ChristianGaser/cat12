ccb35f6441e244baa03066daeb59682932594d1d
Robert <52195853+robdahn@users.noreply.github.com>
2023-04-27 16:19:09 +0200

    Fixed:    Reactivated multi_subject_imcalc for developers in cat_conf_tools. Fixed:    Corrections in cat_io_mergeStruct and cat_io_updateStruct. Fixed:    Added CS3 surface pipeline case in cat_main. Fixed:    Added missing variable for SPM-CAT pipeline in cat_main[1639]. Fixed:    Corrected fixed TPM loop number in cat_main_updateSPM1639. Fixed:    Corrected typos in CHANGES.txt Changed:  Be verbose in special longitudinal background model case in           cat_run_job1639 (new_release only). Changed paths:  M CHANGES.txt AD batches/OASIS_bones_CT2MRI.m AD bones_seg.m  M cat_conf_tools.m  M cat_io_mergeStruct.m  M cat_io_updateStruct.m  M cat_main.m  M cat_main1639.m  M cat_main_updateSPM.m  M cat_main_updateSPM1639.m  M cat_run.m  M cat_run_job1639.m AD templates_surfaces/bh.brain.human.TPM.gii AD templates_surfaces/bh.headbrain.human.TPM.gii

--- cat_main_updateSPM.m
+++ cat_main_updateSPM.m
@@ -19,6 +19,11 @@ function [Ysrc,Ycls,Yb,Yb0,Yy,job,res,trans,T3th,stime2] = cat_main_updateSPM(Ys
 
   [pth,nam] = spm_fileparts(res.image0(1).fname); %#ok<ASGLU> % original   
 
+  % RD202211: Added SPM based detection of high intensity backgronds of MP2Rage scans
+  %res.isMP2RAGE = min( res.mn(res.lkp==3 & res.mg'>0.2) ) < min( res.mn(res.lkp==max(res.lkp) & res.mg'>0.2) ); 
+  % CG20230227 disabled detection because it was not working properly
+  res.isMP2RAGE = false; 
+  
   % voxel size parameter
   vx_vol  = sqrt(sum(res.image(1).mat(1:3,1:3).^2));    % voxel size of the processed image
   vx_vol0 = sqrt(sum(res.image0(1).mat(1:3,1:3).^2));
@@ -545,6 +550,14 @@ function [Ysrc,Ycls,Yb,Yb0,Yy,job,res,trans,T3th,stime2] = cat_main_updateSPM(Ys
       P(:,:,:,1) = cat_vol_ctype(single(P(:,:,:,1)) + 255*single(Ygm));
       Yp0  = single(P(:,:,:,3))/255/3 + single(P(:,:,:,1))/255*2/3 + single(P(:,:,:,2))/255;
       clear Ywm Ygm;
+
+      %% RD20221108: update CSF to reduce problems in MP2rage
+      if res.isMP2RAGE
+        Ycsf = uint8( (Ysrc < min( res.mn(res.lkp==3)) * 0.7 + 0.3 * min( res.mn(res.lkp==1)) )  &  Yb  & Yg<.3);
+        for ti = setdiff(1:size(P,4),3), P(:,:,:,ti) = P(:,:,:,ti) .* (1-Ycsf); end
+        P(:,:,:,3) = cat_vol_ctype(single(P(:,:,:,3)) + 255*single(Ycsf));
+        Yp0  = single(P(:,:,:,3))/255/3 + single(P(:,:,:,1))/255*2/3 + single(P(:,:,:,2))/255;
+      end   
 
       %% head to CSF
       Ycsf = (Ysrc > T3th(1) -  min( abs( [ diff(T3th(1:2)) diff(T3th(1:2:3)) ] ))/2 ) & ...
