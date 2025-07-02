00ef1e77a610bb694855c3ac7a14ca613acb74f9
Christian Gaser <christian.gaser@uni-jena.de>
2025-07-02 16:32:09 +0200

    Removed debugging code in compile.m

--- compile.m
+++ compile.m
@@ -104,19 +104,6 @@ function varargout = compile(comp,test,verb)
     nce  = cell(1,2);   % number of errors
     ncw  = cell(1,2);   % number of warnings
     
-    mex -O -largeArrayDims  cat_vol_eidist.c
-    mex -O -largeArrayDims  cat_vbdist.c
-    mex -O -largeArrayDims  cat_vol_localstat.c
-    mex -O -largeArrayDims  cat_vol_pbtp.c
-    mex -O -largeArrayDims  cat_sanlm.c sanlm_float.c
-
-    
-    mex -O -largeArrayDims COPTIMFLAGS='-O3 -fwrapv -DNDEBUG' cat_vol_eidist.c
-    mex -O -largeArrayDims COPTIMFLAGS='-O3 -fwrapv -DNDEBUG' cat_vbdist.c
-    mex -O -largeArrayDims COPTIMFLAGS='-O3 -fwrapv -DNDEBUG' cat_vol_localstat.c
-    mex -O -largeArrayDims COPTIMFLAGS='-O3 -fwrapv -DNDEBUG' cat_vol_pbtp.c
-    mex -O -largeArrayDims COPTIMFLAGS='-O3 -fwrapv -DNDEBUG' cat_sanlm.c sanlm_float.c
-    
     %% compile main c-functions
     for nci=1:numel(nc)
       if verb, fprintf('Compiling c-functions:\n'); end
