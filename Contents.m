6e4d135e362aeb939e63eee9f6b773772f13d35a
Christian Gaser <christian.gaser@uni-jena.de>
2026-02-24 16:28:56 +0100

    Bump version to r3082 and add manual link
    Update version string to r3082 in Contents.m and update documentation UI. doc/cat.html and doc/cat.txt add an "Open CAT Manual" link and refactor the quick-start/manual buttons into a button-row layout. doc/css/cat.css adjusts button styling (background color, width, flex layout, new .button-row/.button rules) and hover color to match the new layout.

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Computational Anatomy Toolbox
-% Version 3079 (CAT26.0.rc1) 2026-02-24
+% Version 3082 (CAT26.0.rc1) 2026-02-24
 % ______________________________________________________________________
 %
 % Christian Gaser, Robert Dahnke
